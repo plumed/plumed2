2bbcaf6ada57c42212b88271ce9b6484c2345399
Giovanni Bussi <giovanni.bussi@gmail.com>
2011-12-20 13:15:35 +0100

    Added initial checks for Vector and Tensor
    They will detect a compiler where layout of Vector
    and Tensor could give problems e.g. for MPI functions.
    It's a bit of paranoid...

--- src/Tensor.cpp
+++ src/Tensor.cpp
@@ -1,1 +1,18 @@
 #include "Tensor.h"
+#include <iostream>
+#include <cstdlib>
+
+namespace PLMD{
+
+static class TensorChecks{
+public:
+  TensorChecks(){
+    if(sizeof(Tensor)==9*sizeof(double)) return;
+    std::cerr<<"Severe error: sizeof(Tensor)!=9*sizeof(double)\n";
+    std::cerr<<"PLUMED cannot work properly in these conditions\n";
+    std::abort();
+  }
+} checks;
+
+}
+
--- src/Vector.cpp
+++ src/Vector.cpp
@@ -1,9 +1,25 @@
 #include "Vector.h"
 #include <cmath>
+#include <iostream>
+#include <cstdlib>
 
-using namespace PLMD;
+namespace PLMD{
 
 double Vector::modulo()const{
   return sqrt(modulo2());
 }
+
+static class VectorChecks{
+public:
+  VectorChecks(){
+    if(sizeof(Vector)==3*sizeof(double)) return;
+    std::cerr<<"Severe error: sizeof(Vector)!=3*sizeof(double)\n";
+    std::cerr<<"PLUMED cannot work properly in these conditions\n";
+    std::abort();
+  }
+} checks;
+
+}
+
+
 
