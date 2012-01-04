09b7baa18de583949762631a6cb6dfffad6e4acc
Carlo Camilloni <carlo@vpnclient44.ch.cam.ac.uk>
2012-01-04 16:56:41 +0100

    added the possibility of using linear restraints

--- src/BiasRestraint.cpp
+++ src/BiasRestraint.cpp
@@ -10,16 +10,16 @@ namespace PLMD{
 
 //+PLUMEDOC BIAS RESTRAINT
 /**
-Adds an harmonic restraint on one or more variables
+Adds an harmonic and/or linear restraint on one or more variables
 
 \par Syntax
 \verbatim
-RESTRAINT ARG=x1,x2,... KAPPA=k1,k2,... AT=a1,a2,...
+RESTRAINT ARG=x1,x2,... KAPPA=k1,k2,... SLOPE=m1,m2,... AT=a1,a2,...
 \endverbatim
-KAPPA specifies an array of force constants, one for each variable,
+KAPPA and SLOPE specifies an array of force constants, one for each variable,
 and AT the center of the restraints. Thus, the resulting potential is
 \f$
-  \sum_i \frac{k_i}{2} (x_i-a_i)^2
+  \sum_i \frac{k_i}{2} (x_i-a_i)^2 + m_i*(x_i-a_i)
 \f$.
 
 \par Example
@@ -40,6 +40,7 @@ PRINT ARG=restraint.bias
 class BiasRestraint : public Bias{
   std::vector<double> at;
   std::vector<double> kappa;
+  std::vector<double> slope;
 public:
   BiasRestraint(const ActionOptions&);
   void calculate();
@@ -51,7 +52,10 @@ BiasRestraint::BiasRestraint(const ActionOptions&ao):
 PLUMED_BIAS_INIT(ao),
 at(0),
 kappa(getNumberOfArguments(),0.0)
+slope(getNumberOfArguments(),0.0)
 {
+  parseVector("SLOPE",slope);
+  assert(slope.size()==getNumberOfArguments());
   parseVector("KAPPA",kappa);
   assert(kappa.size()==getNumberOfArguments());
   parseVector("AT",at);
@@ -61,8 +65,11 @@ kappa(getNumberOfArguments(),0.0)
   log.printf("  at");
   for(unsigned i=0;i<at.size();i++) log.printf(" %f",at[i]);
   log.printf("\n");
-  log.printf("  with force constant");
+  log.printf("  with harmonic force constant");
   for(unsigned i=0;i<kappa.size();i++) log.printf(" %f",kappa[i]);
+  log.printf("\n");
+  log.printf("  and linear force constant");
+  for(unsigned i=0;i<slope.size();i++) log.printf(" %f",slope[i]);
   log.printf("\n");
 
   addValue("bias");
@@ -76,8 +83,9 @@ void BiasRestraint::calculate(){
   for(unsigned i=0;i<getNumberOfArguments();++i){
     const double cv=difference(i,at[i],getArgument(i));
     const double k=kappa[i];
-    const double f=-k*cv;
-    ene+=0.5*k*cv*cv;
+    const double m=slope[i];
+    const double f=-(k*cv+m);
+    ene+=0.5*k*cv*cv+m*cv;
     setOutputForces(i,f);
     totf2+=f*f;
   };
