2821435d02b03c2c3da2eaf937102a94ad1febb1
Gareth Tribello <gareth.tribello@gmail.com>
2012-02-13 14:36:50 +0100

    Added documentation in Keywords header file for Doxygen and the developers manual.

--- src/Keywords.h
+++ src/Keywords.h
@@ -30,25 +30,53 @@ public:
 class Keywords{
 friend class Action;
 private:
+/// Whether the keyword is compulsory, optional...
   std::vector<KeyType> types;
+/// The names of the keyword
   std::vector<std::string> keys;
+/// The documentation for the keywords
   std::vector<std::string> documentation;
+/// The default values for the flags (are they on or of)
   std::map<std::string,bool> booldefs; 
+/// The default values (if there are default values) for compulsory keywords
   std::map<std::string,std::string> numdefs;
-public:
-  void add( const std::string, const std::string, const std::string );
-  void add( const std::string t, const std::string k, const std::string def, const std::string d );
-  void addFlag( const std::string k, const bool def, const std::string d );
-  void remove( const std::string );
-  void clear();
-  unsigned size() const;
-  bool exists( const std::string k ) const ;
-  bool style( const std::string k, const std::string t ) const ;
-  void print( Log& log ) const ;
-  void print_html() const ;
+/// Print the documentation for the jth keyword in html
   void print_html_item( const unsigned& j ) const;
+/// Print the documentation to the log file (used by PLMD::Action::error)
+  void print( Log& log ) const ;
+/// find out whether flag key is on or off by default.
   bool getLogicalDefault( std::string key, bool& def ) const ;
+/// Get the value of the default for the keyword named key
   bool getDefaultValue( std::string key, std::string& def ) const ;
+/// Clear all the keywords
+  void clear();
+/// Return the number of defined keywords 
+  unsigned size() const;
+public:
+/// Add a new keyword of type t with name k and description d
+  void add( const std::string t, const std::string k, const std::string d );
+/// Add a new compulsory keyword (t must equal compulsory) with name k, default value def and description d
+  void add( const std::string t, const std::string k, const std::string def, const std::string d );
+/// Add a falg with name k that is by default on if def is true and off if def is false.  d should provide a description of the flag
+  void addFlag( const std::string k, const bool def, const std::string d );
+/// Remove the keyword with name k
+  void remove( const std::string k );
+// /// Clear all the keywords
+//   void clear();
+// /// Return the number of defined keywords 
+//   unsigned size() const;
+/// Check if there is a keyword with name k
+  bool exists( const std::string k ) const ;
+/// Check if the keyword with name k has style t
+  bool style( const std::string k, const std::string t ) const ;
+// /// Print the documentation to the log file (used by PLMD::Action::error)
+//   void print( Log& log ) const ;
+/// Print an html version of the documentation
+  void print_html() const ;
+// /// find out whether flag key is on or off by default.
+//   bool getLogicalDefault( std::string key, bool& def ) const ;
+// /// Get the value of the default for the keyword named key
+//   bool getDefaultValue( std::string key, std::string& def ) const ;
 };
 
 }
