9f9236841da1e04e6b63cd352a7db70395ac66c4
Gareth Tribello <gareth.tribello@gmail.com>
2012-06-26 12:44:11 +0200

    Added functionality to calculate the fraction of colvars within a given using the WITHIN keyword range as well as the number of colvars within a given range.

--- src/DistributionWithin.cpp
+++ src/DistributionWithin.cpp
@@ -4,22 +4,29 @@ namespace PLMD {
 
 std::string within::documentation(){
   std::ostringstream ostr;
-  ostr<<"To make this quantity continuous it is calculated using "<<HistogramBead::documentation(false);
+  ostr<<"To make this quantity continuous it is calculated using "<<HistogramBead::documentation(false)<<". ";
+  ostr<<"Adding the NORM flag allows you to calculate the fraction of colvars in the particular range rather than the total number ";
+  ostr<<"(N.B. this option should probably not used if you are using neighbor lists and the derivatives of the WITHIN value).";
   return ostr.str();
 }
 
 within::within( const std::string& parameters ) :
-DistributionFunction(parameters)
+DistributionFunction(parameters),
+usenorm(false)
 { 
   std::string errormsg;
+  std::vector<std::string> data=Tools::getWords(parameters);
+  Tools::parseFlag(data,"NORM",usenorm);
   hist.set( parameters, "", errormsg );
   if( errormsg.size()!=0 ) error( errormsg );
   addAccumulator( true );
+  addAccumulator( false );
 }
 
 std::string within::message(){
   std::ostringstream ostr;
-  ostr<<"number of values "<<hist.description(); 
+  if(usenorm) ostr<<"fraction of values "<<hist.description();
+  else ostr<<"number of values "<<hist.description(); 
   return ostr.str();
 }
 
@@ -38,11 +45,17 @@ void within::calculate( Value* value_in, std::vector<Value>& aux ){
   copyValue( 0, value_in ); 
   double df, f; f=hist.calculate( value_in->get() , df );
   chainRule(0, df); setValue(0, f);
+  setValue( 1, 1.0 );
 }
 
 void within::finish( Value* value_out ){
   extractDerivatives( 0, value_out );
-  value_out->set( getPntrToAccumulator(0)->get() );
+  if( usenorm ) {
+    double nval=getPntrToAccumulator(1)->get();
+    value_out->chainRule(1.0/nval); value_out->set(getPntrToAccumulator(0)->get()/nval);
+  } else {
+    value_out->set( getPntrToAccumulator(0)->get() );
+  }
 }
 
 }
