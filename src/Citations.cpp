510d2e07f755ef53efc9850b0f707cc31618870c
Giovanni Bussi <giovanni.bussi@gmail.com>
2012-07-05 10:47:05 +0200

    Bibliography now starts from [1] (not [0])

--- src/Citations.cpp
+++ src/Citations.cpp
@@ -12,14 +12,14 @@ std::string Citations::cite(const std::string & item){
   if(i==items.size()) items.push_back(item);
   plumed_assert(i<items.size());
   string ret;
-  Tools::convert(i,ret);
+  Tools::convert(i+1,ret);
   ret="["+ret+"]";
   return ret;
 }
 
 std::ostream & PLMD::operator<<(std::ostream &log,const Citations&cit){
   for(unsigned i=0;i<cit.items.size();++i)
-    log<<"  ["<<i<<"] "<<cit.items[i]<<"\n";
+    log<<"  ["<<i+1<<"] "<<cit.items[i]<<"\n";
   return log;
 }
 
