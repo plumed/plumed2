5982eab180eef8e0dc5a97b12df000b85bf933c9
davidebr <davide.branduardi@gmail.com>
2012-07-14 23:59:02 +0200

    Neighbor list: start
    Added sorting tool and keywords. Still to do the whole call infratrsucture.

--- src/FunctionPathMSD.cpp
+++ src/FunctionPathMSD.cpp
@@ -10,6 +10,7 @@ double (*cmathLog)(double) = log;
 #include <cstring>
 #include <cassert>
 #include "ColvarRMSD.h"
+#include <iostream>
 
 using namespace std;
 
@@ -44,7 +45,9 @@ PRINT ARG=t1,t2,t3,p1.s,p1.z STRIDE=1 FILE=colvar FMT=%8.4f
 class FunctionPathMSD : public Function {
   double lambda;
   bool pbc;
-
+  int neigh_size;
+  int neigh_stride;
+  vector<int> neighlist;
 public:
   FunctionPathMSD(const ActionOptions&);
 // active methods:
@@ -58,19 +61,51 @@ void FunctionPathMSD::registerKeywords(Keywords& keys){
   Function::registerKeywords(keys);
   keys.use("ARG");
   keys.add("compulsory","LAMBDA","all compulsory keywords should be added like this with a description here");
-  keys.add("optional","NEIGHLIST","all optional keywords that have input should be added like a description here");
+  keys.add("optional","NEIGH_SIZE","all optional keywords that have input should be added like a description here");
+  keys.add("optional","NEIGH_STRIDE","all optional keywords that have input should be added like a description here");
 }
+
+// create a custom sorter
+typedef vector<double>::const_iterator myiter;
+struct ordering {
+       bool operator ()(pair<unsigned , myiter> const& a, pair<unsigned , myiter> const& b) {
+           return *(a.second) < *(b.second);
+       };
+};
+// sorting utility
+vector<int> increasingOrder( vector<double> &v){
+   // make a pair
+   vector< pair<unsigned , myiter> > order(v.size());
+   unsigned n = 0;
+   for (myiter it = v.begin(); it != v.end(); ++it, ++n){
+       order[n] = make_pair(n, it); // note: heere i do not put the values but the addresses that point to the value 
+   };
+   // now sort according the second value
+   sort(order.begin(), order.end(), ordering());
+   typedef vector< pair<unsigned , myiter> >::const_iterator pairiter;
+
+   vector<int> vv(v.size());n=0;
+   for ( pairiter it = order.begin(); it != order.end(); ++it ){
+   //    cerr<<(*it).first<<" "<<(*(*it).second)<<endl;
+       vv[n]=(*it).first;n++; 
+   }
+   return vv;
+};
 
 FunctionPathMSD::FunctionPathMSD(const ActionOptions&ao):
 Action(ao),
 Function(ao),
-pbc(true)
+pbc(true),
+neigh_stride(-1),
+neigh_size(-1)
 {
 //  bool nopbc=!pbc;
 //  parseFlag("NOPBC",nopbc);
 //  pbc=!nopbc;
 
   parse("LAMBDA",lambda);
+  parse("NEIGH_SIZE",neigh_size);
+  parse("NEIGH_STRIDE",neigh_stride);
   checkRead();
   log.printf("  lambda is %f\n",lambda);
   // list the action involved and check the type 
@@ -78,8 +113,6 @@ pbc(true)
        // for each value get the name and the label of the corresponding action
        std::string mylabel=getPntrToArgument(i)->getPntrToAction()->getLabel(); 
        std::string myname=getPntrToArgument(i)->getPntrToAction()->getName(); 
-       //log.printf("ARG  %s TYPE %s \n",mylabel.c_str(),myname.c_str());
-       //if(myname!="RMSD")plumed_merror("This argument is not of RMSD type!!!");
        // check of the SQUARED flag is set in all the dependent variables
        ColvarRMSD* ptr=dynamic_cast<ColvarRMSD*>(getPntrToArgument(i)->getPntrToAction());
        if(ptr){
@@ -110,6 +143,15 @@ pbc(true)
        }
   }   
   log.printf("  Consistency check completed! Your path cvs look good!\n"); 
+  // do some neighbor printout
+  if(neigh_stride!=-1 || neigh_size!=-1){
+           log.printf("  Neighbor list enabled: \n");
+           log.printf("                size   :  %d elements\n",neigh_size);
+           log.printf("                stride :  %d steps\n",neigh_stride);
+  }else{
+           log.printf("  Neighbor list NOT enabled \n");
+  }
+
   addComponentWithDerivatives("s"); componentIsNotPeriodic("s");
   addComponentWithDerivatives("z"); componentIsNotPeriodic("z");
 }
@@ -118,6 +160,15 @@ void FunctionPathMSD::calculate(){
   double s_path=0.;
   double partition=0.;
   double tmp;
+  // no neighbor list
+  if(neigh_size<0 ){
+    neighlist.resize(getNumberOfArguments());  
+    for(unsigned i=0;i<getNumberOfArguments();i++)neighlist[i]=i; 
+  }else{
+  // if on step, recalculate
+     
+  // use the neighbor list 
+  }
   Value* val_s_path=getPntrToComponent("s");
   Value* val_z_path=getPntrToComponent("z");
   vector<double> zvec(getNumberOfArguments());
@@ -134,6 +185,10 @@ void FunctionPathMSD::calculate(){
     setDerivative(val_s_path,i,tmp);
     setDerivative(val_z_path,i,zvec[i]/partition);
   }
+  // neighbor list: rank and activate the chain for the next step 
+  vector<int> order=increasingOrder(zvec); // this is the order of the exponential, therefore we should take from the back 
+     
+
 }
 
 }
