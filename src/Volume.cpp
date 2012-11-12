7d57673b6c418d6a5bd3d1a5ab44c258cbb0f0d1
Giovanni Bussi <giovanni.bussi@gmail.com>
2012-11-12 18:22:39 +0100

    removed wrong space at the end of a line

--- src/Volume.cpp
+++ src/Volume.cpp
@@ -18,7 +18,7 @@
 
    You should have received a copy of the GNU Lesser General Public License
    along with plumed.  If not, see <http://www.gnu.org/licenses/>.
-+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++ */ 
++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++ */
 #include "Volume.h"
 
 namespace PLMD {
