bc4c879f6a0e83ab013f87242194fb75a6df3703
Giovanni Bussi <giovanni.bussi@gmail.com>
2012-11-15 10:33:24 +0100

    couple of fix in info files

--- src/header.sh
+++ src/header.sh
@@ -1,3 +1,6 @@
+# This script adds a LGPL header to all the source files
+# If the header is already present, it does not touch the file
+# Please run it whenever you add a new file so as to keep copyright info there!
 for file in *.c *.cpp *.h PlumedConfig.h.in
 do
 
