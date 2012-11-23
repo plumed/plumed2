22abdf90e4e3eef59f80d954b261717da90fe090
Gareth Tribello <gareth.tribello@gmail.com>
2012-11-23 09:36:01 +0100

    Made it so that you can call Action::error outside of Action constructors.

--- src/Action.cpp
+++ src/Action.cpp
@@ -192,7 +192,7 @@ void Action::prepare(){
 
 void Action::error( const std::string & msg ){
   log.printf("ERROR in input to action %s with label %s : %s \n \n", name.c_str(), label.c_str(), msg.c_str() );
-  keywords.print( log );
+  if( !line.empty() ) keywords.print( log );
   plumed_merror("ERROR in input to action " + name + " with label " + label + " : " + msg );
   this->exit(1);
 }
