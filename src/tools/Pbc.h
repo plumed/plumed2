db509d5e37393c1161d92407286c4b74ad900ee7
Giovanni Bussi <giovanni.bussi@gmail.com>
2012-11-28 09:55:38 +0100

    Optimized PBCs
    I took advantage of the fact that we have the reduced cell. This
    allows to avoid the comparison of different shifts for
    many values of the scaled coordinates, as well as using the
    sign of the scaled coordinates to check which shifts should
    be attempted. The average number of shift vector to be evaluated
    is approximately 1 (plus the "null-shift")
    
    There is also a Pbc::test() function which I used during debugging.
    I leave it there for future reference.

--- src/tools/Pbc.h
+++ src/tools/Pbc.h
@@ -24,21 +24,26 @@
 
 #include "Vector.h"
 #include "Tensor.h"
+#include <vector>
+#include <cstddef>
 
 namespace PLMD{
 
 class Pbc{
-  enum {unset,orthorombic,xy,xz,yz,generic} type;
+  enum {unset,orthorombic,generic} type;
   Tensor box;
   Tensor invBox;
   Tensor reduced;
   Tensor invReduced;
+  std::vector<Vector> shifts[2][2][2];
   Vector diag,hdiag,mdiag;
+  void buildShifts(std::vector<Vector> shifts[2][2][2])const;
   void fullSearch(Vector&)const;
 public:
+  static void test();
   Pbc();
   double distance( const bool pbc, const Vector& v1, const Vector& v2 ) const;
-  Vector distance(const Vector&,const Vector&)const;
+  Vector distance(const Vector&,const Vector&,int*nshifts=NULL)const;
   void setBox(const Tensor&);
   const Tensor& getBox()const;
   const Tensor& getInvBox()const;
