db546c439ad64a153e4dce894a3308a55ae6ce06
Giovanni Bussi <giovanni.bussi@gmail.com>
2012-12-07 10:23:41 +0100

    Possibility to directly access to CLTool from cltools dir
    I added a src/cltools/CLTool.h which can be directly included from sources
    in the cltools directory

--- src/cltools/CLToolManual.cpp
+++ src/cltools/CLToolManual.cpp
@@ -19,8 +19,8 @@
    You should have received a copy of the GNU Lesser General Public License
    along with plumed.  If not, see <http://www.gnu.org/licenses/>.
 +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++ */
-#include "core/CLTool.h"
-#include "core/CLToolRegister.h"
+#include "CLTool.h"
+#include "CLToolRegister.h"
 #include "tools/Tools.h"
 #include "config/PlumedConfig.h"
 #include "core/ActionRegister.h"
