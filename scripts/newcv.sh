02b40d2b5e08e876627ed5b098f9d138a709ea7b
Giovanni Bussi <giovanni.bussi@gmail.com>
2012-12-14 14:42:18 +0100

    Fix

--- scripts/newcv.sh
+++ scripts/newcv.sh
@@ -8,14 +8,14 @@ fi
 if [ $# != 2 ] ;
 then
   echo "ERROR"
-  echo "type 'plumed newcv directive cvname'"
+  echo "type 'plumed newcv directive classname'"
   echo "E.g. 'plumed newcv TORSION Torsion'"
   exit 1
 fi
 
 directive=$1
-cvname=$2
+classname=$2
 
 sed "s/TEMPLATE/$directive/g
-     s/Template/$cvname/g" $PLUMED_ROOT/src/basic/ColvarTemplate.cpp > Colvar${cvname}.cpp
+     s/Template/$classname/g" $PLUMED_ROOT/src/colvar/Template.cpp > ${classname}.cpp
 
