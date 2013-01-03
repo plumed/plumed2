6a83e9987b705e2d15fcc1a695c16d253a2da44e
Giovanni Bussi <giovanni.bussi@gmail.com>
2013-01-03 13:41:33 +0100

    Moved MOLINFO documentation
    This avoids complains from make doc

--- src/core/SetupMolInfo.cpp
+++ src/core/SetupMolInfo.cpp
@@ -29,37 +29,6 @@
 
 namespace PLMD {
 
-//+PLUMEDOC TOPOLOGY MOLINFO
-/*
-This command is used to provide information on the molecules that are present in your system.
-
-The information on the molecules in your system can either be provided in the form of a pdb file
-or as a set of lists of atoms that describe the various chains in your system. If a pdb file
-is used plumed the MOLINFO command will endeavor to recognize the various chains and residues that
-make up the molecules in your system using the chainIDs and resnumbers from the pdb file. You can
-then use this information in later commands to specify atom lists in terms residues.  For example
-using this command you can find the backbone atoms in your structure automatically. 
-
-Please be aware that the pdb parser in plumed is far from perfect. You should thus check the log file
-and examine what plumed is actually doing whenenver you use the MOLINFO action.
-
-\bug At the moment all atoms named HA1 are treated as if they are CB atoms.  This makes it possible to deal
-with GLY residues in colvars like \ref ALPHARMSD. 
-
-\par Examples
-
-In the following example the MOLINFO command is used to provide the information on which atoms
-are in the backbone of a protein to the ALPHARMSD CV.
-
-\verbatim
-MOLINFO STRUCTURE=reference.pdb
-ALPHARMSD BACKBONE=all TYPE=DRMSD LESS_THAN={RATIONAL R_0=0.08 NN=8 MM=12} LABEL=a 
-\endverbatim
-(see also \ref ALPHARMSD)
-
-*/
-//+ENDPLUMEDOC
-
 
 /*
 This action is defined in core/ as it is used by other actions.
