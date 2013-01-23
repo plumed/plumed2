43ba7266be2ea282a12a3147a3f1edad91812f34
Giovanni Bussi <giovanni.bussi@gmail.com>
2013-01-23 13:48:12 +0100

    Fixed header

--- src/tools/CubicInterpolation.h
+++ src/tools/CubicInterpolation.h
@@ -19,8 +19,8 @@
    You should have received a copy of the GNU Lesser General Public License
    along with plumed.  If not, see <http://www.gnu.org/licenses/>.
 +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++ */
-#ifndef __PLUMED_core_CubicInterpolation_h
-#define __PLUMED_core_CubicInterpolation_h
+#ifndef __PLUMED_tools_CubicInterpolation_h
+#define __PLUMED_tools_CubicInterpolation_h
 
 #include <vector>
 #include "Matrix.h"
