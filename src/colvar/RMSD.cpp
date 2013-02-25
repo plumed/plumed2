4093cd03f23b2e3e42a9e5b779b57bdb62f6e260
davidebr <davide.branduardi@gmail.com>
2013-02-25 12:14:10 +0100

    improved doc in rmsd

--- src/colvar/RMSD.cpp
+++ src/colvar/RMSD.cpp
@@ -57,40 +57,58 @@ To calculate the root-mean-square deviation between the atoms in two configurati
 you must first superimpose the two structures in some ways.  Obviously, it is the internal vibrational 
 motions of the structure - i.e. not the translations and rotations - that are interesting.  It is 
 possible to align two structures (i.e. remove the translational and rotational motions of the 
-system) using the Kearsley \cite kearsley algorithm.  This algorithm calculates a 
-roto-translation matrix from a set of \e alignment atoms \f${\cal A}\f$.  The amount by which 
-a \e displacement set of atoms, \f${\cal B}\f$, has been moved can then be calculated using:   
+system).
+In general the aim of this colvar is to calculate something like:
 
 \f[
-d(X_j,X_i) = \sum_{a=1}^{N_{\cal B}} (X^{(j)}_a - M_{ij} X^{(i)}_a)^2
+d(X,X_r) = \vert X-X' \vert 
 \f]
 
-where \f$M_{ij}\f$ is the roto-translation matrix translated by the Kearsley algorithm.
+where \f$ X \f$ is the molecular dynamics snapshot and   
+\f$ X' \f$ is a reference structure you provide as input.
+Here the symbols \f$ \vert \dots \vert \f$ represent some sort of norm of choice that is selected through the OPTIMAL switch (see below for examples).
 
-When a pdb input file is required to specify the structure to align to in an RMSD calculation
-the occupancy and beta columns are used to specify which atoms form part of the alignment set
-and which atoms form part of the displacement set.  Values of 1.0 and 0.0 indicate that the 
-atom is to be used in the \e alignment set only, while values of 0.0 and 1.0 indicates that 
-the atom is to be used in the \e displacement set only.  Values of 1.0 and 1.0 indicate that 
-the atom is to be used in both the \e alignment and \e displacement sets. Users can also 
-use fractional values for beta and the occupancy values.  We recommend you only do this when
-you really know what you are doing however as the results can be rather strange.  
-When this form of RMSD is used to calculate the secondary structure variables (\ref ALPHARMSD,
-\ref ANTIBETARMSD and \ref PARABETARMSD) all the atoms in the segment are assumed to be part of
-both the \e alignment and \e displacement sets.
+Here we discuss the switch we implemented so far.
 
-The \e OPTIMAL type of rmsd calculation removes both translational and rotational motions.  
-There may, however, be ocasions where you do not need to remove the rotational motions.  That is
-to say there may be times when you only need to align the centers of mass of the two structures.
-This can be done by performing the \e SIMPLE form of RMSD calculation.
-
-In PDB files the atomic coordinates and box lengths should be in Angstroms unless 
-you are working with natural units.  If you are working with natural units then the coordinates 
-should be in your natural length unit.  For more details on the PDB file format visit http://www.wwpdb.org/docs.html
-
-\attention
-The documentation here needs some work as it is not very clear to me 
-sorry GAT. 
+       - If you use TYPE=SIMPLE you just calculate the root mean square deviation after reset of the geometric center  
+         that reads:
+         \f[
+         d(X,X_r) = \sqrt{ \sum_i \sum_\alpha^{x,y,z}  \frac{w_i}{\sum_j w_j}( X_{i,\alpha}-com_\alpha(X)-{X'}_{i,\alpha}+com_\alpha(X') )^2 } 
+         \f]
+         with 
+         \f[
+         com_\alpha(X)= \sum_i  \frac{w'_{i}}{\sum_j w'_j}X_{i,\alpha}
+         \f]
+         and
+         \f[
+         com_\alpha(X')= \sum_i  \frac{w'_{i}}{\sum_j w'_j}X'_{i,\alpha}
+         \f]
+         where  \f$ com_\alpha(X) \f$ and  \f$ com_\alpha(X') \f$  are the center of mass
+         whenever the weights $w'$ are assigned proportional to the atomic masses, otherwise are the geometric centers of the 
+         running MD snapshot and the reference frame respectively whenever they are all set to one.
+         Note that two set of weights exist:  \f$ w' \f$ and  \f$ w \f$. The first is used to calculate the center of mass (so it determines the \e alignment) 
+         and the second is used to calculate the actual \e displacement.
+         These are assigned in the reference (that you set with e.g.: REFERENCE=whatever.pdb) 
+         frame which is, for this case, a simple pdb file containing the atoms
+         on which you want to calculate the distance. 
+         Note that in this pdb you need to set correctly the index of the atom STARTING FROM 1 (i.e. +1 shift respect 
+         to VMD numbering) so that the program knows which atom needs to be compared with.
+         The OCCUPANCY column (the first after the coordinates) set the \f$ w'  \f$ used for the center of mass calculation and the BETA column (the second
+         after the Cartesian coordinates) set the \f$ w \f$ which is responsible of the calculation of the displacement. 
+         Users can also use fractional values for beta and the occupancy values. We recommend you only do this when you really know what you are doing however as the results can be rather strange. 
+         In PDB files the atomic coordinates and box lengths should be in Angstroms unless 
+         you are working with natural units.  If you are working with natural units then the coordinates 
+         should be in your natural length unit.  For more details on the PDB file format visit http://www.wwpdb.org/docs.html
+         .
+       - If you use TYPE=OPTIMAL you just the root mean square deviation after reset of the geometric center  
+ and performing an optimal alignment that reads:
+        \f[
+         d(X,X_r) = \sqrt{ \sum_i \sum_\alpha^{x,y,z}  \frac{w_i}{\sum_j w_j}[ X_{i,\alpha}-com_\alpha(X)- \sum_\beta M(X,X',w')_{\alpha,\beta}({X'}_{i,\beta}-com_\beta(X')) ]^2 } 
+        \f]
+        where \f$ M(X,X',w') \f$ is the optimal alignment matrix which is calculated through the Kearsley \cite kearsley algorithm and the set of 
+	weights used for the alignment of the center of mass is also used to perform the optimal alignment.
+ 	Note that the flexibility of setting only certain atoms to calculate the center of mass and optimal alignment which may be different from the one used in the \e displacemnt  calculation is useful whenever for example you want to calculate the motion of a ligand in a protein cavity and you want to clearly use the protein as reference system.  
+        When this form of RMSD is used to calculate the secondary structure variables (\ref ALPHARMSD, \ref ANTIBETARMSD and \ref PARABETARMSD) all the atoms in the segment are assumed to be part of both the alignment and displacement sets.
 
 \par Examples
 
