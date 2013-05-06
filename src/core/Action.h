214e5999da65f00a597d06cff5699b3938b5964c
Carlo Camilloni <carlo.camilloni@gmail.com>
2013-05-06 13:02:03 +0200

    typo in an error message

--- src/core/Action.h
+++ src/core/Action.h
@@ -285,7 +285,7 @@ void Action::parse(const std::string&key,T&t){
              this->exit(1);
           }           
        } else if( keywords.style(key,"compulsory") ){
-          error("keyword " + key + " is comulsory for this action");
+          error("keyword " + key + " is compulsory for this action");
        }
   }   
 }
