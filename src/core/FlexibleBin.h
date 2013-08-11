0c2f37a763975420cc58bfb3cddaf3108ea6f8ad
Davide Branduardi <davide.branduardi@gmail.com>
2013-08-11 14:11:30 +0200

    Added SIGMA_MIN and SIGMA_MAX keywords to MetaD
    with this keyword you can limit the range of sigma when using the
    adaptive Gaussian scheme.

--- src/core/FlexibleBin.h
+++ src/core/FlexibleBin.h
@@ -38,9 +38,14 @@ class FlexibleBin{
 		std::vector<double> variance;	
 		// this is only there
 		std::vector<double> average;
+		// minimum and maximum values	
+		std::vector<double> sigmamin;	
+		std::vector<double> sigmamax;	
+		std::vector<bool>  limitmax;	
+		std::vector<bool>  limitmin;	
 	public:
 		/// a constructor that takes the pointer of the action that contains it
-		FlexibleBin(int type,ActionWithArguments *paction, double const &d);
+		FlexibleBin(int type,ActionWithArguments *paction, double const &d, std::vector<double> &sigmamin, std::vector<double> &sigmamax );
 		/// update the average (always for diffusion) or calculate the geom covariance (  only when do_when_zero is zero)
 		void update(bool nowAddAHill );
 		std::vector<double> getMatrix() const;
