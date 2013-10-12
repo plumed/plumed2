fb6f1dffdf749960efafdcb3e35847cee524b76c
Gareth Tribello <gareth.tribello@gmail.com>
2013-10-12 18:19:45 +0100

    Added max vessel

--- src/multicolvar/CoordinationNumbers.cpp
+++ src/multicolvar/CoordinationNumbers.cpp
@@ -89,7 +89,7 @@ void CoordinationNumbers::registerKeywords( Keywords& keys ){
                                "The following provides information on the \\ref switchingfunction that are available. "
                                "When this keyword is present you no longer need the NN, MM, D_0 and R_0 keywords.");
   // Use actionWithDistributionKeywords
-  keys.use("MEAN"); keys.use("MORE_THAN"); keys.use("LESS_THAN");
+  keys.use("MEAN"); keys.use("MORE_THAN"); keys.use("LESS_THAN"); keys.use("MAX");
   keys.use("MIN"); keys.use("BETWEEN"); keys.use("HISTOGRAM"); keys.use("MOMENTS");
 }
 
