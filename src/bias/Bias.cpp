fc3956e1d4873e0867641fc1a7628748e2b01847
Giovanni Bussi <giovanni.bussi@gmail.com>
2013-11-07 13:01:33 +0100

    Added possibility to use MTS in bias
    Just use STRIDE. Experimental feature.... beware!
    
    (cherry picked from commit 9646f975286baa2a8c5c37c60d50983d766eca90)
    
    Conflicts:
    	src/bias/Bias.cpp

--- src/bias/Bias.cpp
+++ src/bias/Bias.cpp
@@ -32,7 +32,11 @@ ActionWithValue(ao),
 ActionWithArguments(ao),
 outputForces(getNumberOfArguments(),0.0)
 {
-  if(getStride()>1) error("Using bias with stride!=1 is not currently supported");
+  if(getStride()>1){
+    log<<"  WARNING: you are turning on multiple timestep here\n";
+    log<<"           this bias will be applied every "<<getStride()<<" steps\n";
+    log<<"           use this feature only if you know what you are doing\n";
+  }
   for(unsigned i=0;i<getNumberOfArguments();++i){
      (getPntrToArgument(i)->getPntrToAction())->turnOnDerivatives();
   }
