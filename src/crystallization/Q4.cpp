6300c927ef15174e6a291d3003b6b396cd8dfddc
Giovanni Bussi <giovanni.bussi@gmail.com>
2013-12-09 20:47:22 +0100

    Fixed headers

--- src/crystallization/Q4.cpp
+++ src/crystallization/Q4.cpp
@@ -1,10 +1,10 @@
 /* +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
-   Copyright (c) 2012 The plumed team
+   Copyright (c) 2013 The plumed team
    (see the PEOPLE file at the root of the distribution for a list of names)
 
    See http://www.plumed-code.org for more information.
 
-   This file is part of plumed, version 2.0.
+   This file is part of plumed, version 2.
 
    plumed is free software: you can redistribute it and/or modify
    it under the terms of the GNU Lesser General Public License as published by
