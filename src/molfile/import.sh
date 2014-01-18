ac4f64a4bdc1bee676eee1eb79ba57bcef1c3c30
Toni G <toni.giorgino@isib.cnr.it>
2014-01-18 15:00:06 +0100

    added pdbplugin

--- src/molfile/import.sh
+++ src/molfile/import.sh
@@ -18,6 +18,7 @@ function mycp {
 	f=$1 
 	t=$2
 	echo "Copying $f into $t  ..."
+	cp $f $t
 }
 
 function mycp_wrap {
@@ -40,12 +41,13 @@ function mycp_wrap {
 PD="$1"
 IFDEF=__PLUMED_INTERNAL_MOLFILE_PLUGINS
 
-# List of files always imported
-for i in endianswap.h fastio.h Gromacs.h largefiles.h; do
+# List of files to import from molfile_plugin/src
+for i in endianswap.h fastio.h Gromacs.h largefiles.h periodic_table.h readpdb.h; do
 	mycp $PD/molfile_plugin/src/$i .
 done
 
-for i in molfile_plugin.h vmdplugin.h; do
+# List of files to import from include/
+for i in molfile_plugin.h vmdplugin.h  ; do
 	mycp $PD/include/$i .
 done
 
@@ -54,15 +56,17 @@ mycp $PD/molfile_plugin/LICENSE COPYRIGHT
 # List of "known-good" plugins. Some renaming is necessary
 mycp_wrap $IFDEF $PD/molfile_plugin/src/dcdplugin.c dcdplugin.cpp
 mycp_wrap $IFDEF $PD/molfile_plugin/src/gromacsplugin.C gromacsplugin.cpp
+mycp_wrap $IFDEF $PD/molfile_plugin/src/pdbplugin.c pdbplugin.cpp
+
 
 # Generate static header
-plugins="dcdplugin gromacsplugin"
+plugins="dcdplugin gromacsplugin pdbplugin"
 echo "Generating libmolfile_plugin.h: $plugins"
 rm libmolfile_plugin.h
 ./create_static_header.sh MOLFILE molfile libmolfile_plugin.h $plugins
 
 # Add copyright notice at top
 echo "Adding COPYRIGHT headers"
-(cd ..; ./header.sh)
+(cd ..; ./header.sh > /dev/null)
 
 
--- src/molfile/libmolfile_plugin.h
+++ src/molfile/libmolfile_plugin.h
@@ -48,18 +48,24 @@ extern int molfile_dcdplugin_fini(void);
 extern int molfile_gromacsplugin_init(void);
 extern int molfile_gromacsplugin_register(void *, vmdplugin_register_cb);
 extern int molfile_gromacsplugin_fini(void);
+extern int molfile_pdbplugin_init(void);
+extern int molfile_pdbplugin_register(void *, vmdplugin_register_cb);
+extern int molfile_pdbplugin_fini(void);
 
 #define MOLFILE_INIT_ALL \
     molfile_dcdplugin_init(); \
     molfile_gromacsplugin_init(); \
+    molfile_pdbplugin_init(); \
 
 #define MOLFILE_REGISTER_ALL(v, cb) \
     molfile_dcdplugin_register(v, cb); \
     molfile_gromacsplugin_register(v, cb); \
+    molfile_pdbplugin_register(v, cb); \
 
 #define MOLFILE_FINI_ALL \
     molfile_dcdplugin_fini(); \
     molfile_gromacsplugin_fini(); \
+    molfile_pdbplugin_fini(); \
 
 #ifdef __cplusplus
 }
