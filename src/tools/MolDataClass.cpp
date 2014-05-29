49f1b92262e5643e192cb27904d04da31b314fa8
Carlo Camilloni <carlo.camilloni@gmail.com>
2014-05-30 00:10:22 +0100

    1. belfast 8 and 9 2. More residues in MOLINFO

--- src/tools/MolDataClass.cpp
+++ src/tools/MolDataClass.cpp
@@ -55,7 +55,16 @@ bool MolDataClass::allowedResidue( const std::string& type, const std::string& r
       else if(residuename=="VAL") return true;     
 // Terminal groups
       else if(residuename=="ACE") return true;
-      else if(residuename=="NME") return true;   
+      else if(residuename=="NME") return true;
+// Alternative residue names in common force fiels   
+      else if(residuename=="GLH") return true; // neutral GLU
+      else if(residuename=="ASH") return true; // neutral ASP
+      else if(residuename=="HID") return true; // neutral HIS-D amber
+      else if(residuename=="HSD") return true; // neutral HIS-D charmm
+      else if(residuename=="HIE") return true; // neutral HIS-E amber
+      else if(residuename=="HSE") return true; // neutral HIS-E charmm
+      else if(residuename=="HIP") return true; // neutral HIS-P amber
+      else if(residuename=="HSP") return true; // neutral HIS-P charmm
       else return false; 
   } 
   return false;
