7a58ea843b1d5ce88d4e57e41091e9e656f0c0d4
Giovanni Bussi <giovanni.bussi@gmail.com>
2014-08-27 17:13:59 +0200

    Added SCALED_COMPONENTS to POSITION

--- src/colvar/Position.cpp
+++ src/colvar/Position.cpp
@@ -21,6 +21,7 @@
 +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++ */
 #include "Colvar.h"
 #include "ActionRegister.h"
+#include "tools/Pbc.h"
 
 #include <string>
 #include <cmath>
@@ -54,7 +55,7 @@ This can be done e.g. using \ref FIT_TO_TEMPLATE.
 //+ENDPLUMEDOC
    
 class Position : public Colvar {
-  bool components;
+  bool scaled_components;
   bool pbc;
 
 public:
@@ -70,19 +71,25 @@ void Position::registerKeywords( Keywords& keys ){
   Colvar::registerKeywords( keys );
   componentsAreNotOptional(keys);
   keys.add("atoms","ATOM","the atom number");
+  keys.addFlag("SCALED_COMPONENTS",false,"calculate the a, b and c scaled components of the position separately and store them as label.a, label.b and label.c");
   keys.addOutputComponent("x","default","the x-component of the atom position");
   keys.addOutputComponent("y","default","the y-component of the atom position");
   keys.addOutputComponent("z","default","the z-component of the atom position");
+  keys.addOutputComponent("a","SCALED_COMPONENTS","the normalized projection on the first lattice vector of the atom position");
+  keys.addOutputComponent("b","SCALED_COMPONENTS","the normalized projection on the second lattice vector of the atom position");
+  keys.addOutputComponent("c","SCALED_COMPONENTS","the normalized projection on the third lattice vector of the atom position");
 }
 
 Position::Position(const ActionOptions&ao):
 PLUMED_COLVAR_INIT(ao),
+scaled_components(false),
 pbc(true)
 {
   vector<AtomNumber> atoms;
   parseAtomList("ATOM",atoms);
   if(atoms.size()!=1)
     error("Number of specified atoms should be 2");
+  parseFlag("SCALED_COMPONENTS",scaled_components);
   bool nopbc=!pbc;
   parseFlag("NOPBC",nopbc);
   pbc=!nopbc;
@@ -92,10 +99,16 @@ pbc(true)
   if(pbc) log.printf("  using periodic boundary conditions\n");
   else    log.printf("  without periodic boundary conditions\n");
 
-  addComponentWithDerivatives("x"); componentIsNotPeriodic("x");
-  addComponentWithDerivatives("y"); componentIsNotPeriodic("y");
-  addComponentWithDerivatives("z"); componentIsNotPeriodic("z");
-  log<<"  WARNING: components will not have the proper periodicity - see manual\n";
+  if(scaled_components){
+    addComponentWithDerivatives("a"); componentIsPeriodic("a","-0.5","+0.5");
+    addComponentWithDerivatives("b"); componentIsPeriodic("b","-0.5","+0.5");
+    addComponentWithDerivatives("c"); componentIsPeriodic("c","-0.5","+0.5");
+  } else {
+    addComponentWithDerivatives("x"); componentIsNotPeriodic("x");
+    addComponentWithDerivatives("y"); componentIsNotPeriodic("y");
+    addComponentWithDerivatives("z"); componentIsNotPeriodic("z");
+    log<<"  WARNING: components will not have the proper periodicity - see manual\n";
+  }
 
   requestAtoms(atoms);
 }
@@ -111,21 +124,34 @@ void Position::calculate(){
     distance=delta(Vector(0.0,0.0,0.0),getPosition(0));
   }
 
-  Value* valuex=getPntrToComponent("x");
-  Value* valuey=getPntrToComponent("y");
-  Value* valuez=getPntrToComponent("z");
+  if(scaled_components){
+    Value* valuea=getPntrToComponent("a");
+    Value* valueb=getPntrToComponent("b");
+    Value* valuec=getPntrToComponent("c");
+    Vector d=getPbc().realToScaled(distance);
+    setAtomsDerivatives (valuea,0,matmul(getPbc().getInvBox(),Vector(+1,0,0)));
+    valuea->set(Tools::pbc(d[0]));
+    setAtomsDerivatives (valueb,0,matmul(getPbc().getInvBox(),Vector(0,+1,0)));
+    valueb->set(Tools::pbc(d[1]));
+    setAtomsDerivatives (valuec,0,matmul(getPbc().getInvBox(),Vector(0,0,+1)));
+    valuec->set(Tools::pbc(d[2]));
+  } else {
+    Value* valuex=getPntrToComponent("x");
+    Value* valuey=getPntrToComponent("y");
+    Value* valuez=getPntrToComponent("z");
 
-  setAtomsDerivatives (valuex,0,Vector(+1,0,0));
-  setBoxDerivatives   (valuex,Tensor(distance,Vector(-1,0,0)));
-  valuex->set(distance[0]);
+    setAtomsDerivatives (valuex,0,Vector(+1,0,0));
+    setBoxDerivatives   (valuex,Tensor(distance,Vector(-1,0,0)));
+    valuex->set(distance[0]);
 
-  setAtomsDerivatives (valuey,0,Vector(0,+1,0));
-  setBoxDerivatives   (valuey,Tensor(distance,Vector(0,-1,0)));
-  valuey->set(distance[1]);
+    setAtomsDerivatives (valuey,0,Vector(0,+1,0));
+    setBoxDerivatives   (valuey,Tensor(distance,Vector(0,-1,0)));
+    valuey->set(distance[1]);
 
-  setAtomsDerivatives (valuez,0,Vector(0,0,+1));
-  setBoxDerivatives   (valuez,Tensor(distance,Vector(0,0,-1)));
-  valuez->set(distance[2]);
+    setAtomsDerivatives (valuez,0,Vector(0,0,+1));
+    setBoxDerivatives   (valuez,Tensor(distance,Vector(0,0,-1)));
+    valuez->set(distance[2]);
+  }
 }
 
 }
