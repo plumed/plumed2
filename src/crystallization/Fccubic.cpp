441bc44a5d901f076170c53d69081ba072b6863a
BingqingCheng <tonicbq@gmail.com>
2014-12-05 13:55:08 +0100

    add alpha to modify the fcc harmonic function

--- src/crystallization/Fccubic.cpp
+++ src/crystallization/Fccubic.cpp
@@ -41,7 +41,7 @@ namespace crystallization{
 class Fccubic : public multicolvar::MultiColvar {
 private:
 //  double nl_cut;
-  double rcut2;
+  double rcut2, alpha;
   SwitchingFunction switchingFunction;
 public:
   static void registerKeywords( Keywords& keys );
@@ -65,6 +65,7 @@ void Fccubic::registerKeywords( Keywords& keys ){
   keys.add("optional","SWITCH","This keyword is used if you want to employ an alternative to the continuous swiching function defined above. "
                                "The following provides information on the \\ref switchingfunction that are available. "
                                "When this keyword is present you no longer need the NN, MM, D_0 and R_0 keywords.");
+  keys.add("optional","ALPHA","The alpha parameter of the angular function");
   // Use actionWithDistributionKeywords
   keys.use("MEAN"); keys.use("MORE_THAN"); keys.use("LESS_THAN"); keys.use("MAX");
   keys.use("MIN"); keys.use("BETWEEN"); keys.use("HISTOGRAM"); keys.use("MOMENTS");
@@ -85,6 +86,13 @@ PLUMED_MULTICOLVAR_INIT(ao)
      if( r_0<0.0 ) error("you must set a value for R_0");
      switchingFunction.set(nn,mm,r_0,d_0);
   }
+  
+  std::string salpha; parse("ALPHA", salpha);
+  if (salpha.length()>0) {
+	  Tools::convert(salpha,alpha);
+	  log.printf("  using ALPHA %f\n", alpha);
+  } else { alpha=3.0; } // defaults to the Angioletti paper
+  
   log.printf("  measure of simple cubicity around central atom.  Includes those atoms within %s\n",( switchingFunction.description() ).c_str() );
   // Set the link cell cutoff
   rcut2 = switchingFunction.get_dmax()*switchingFunction.get_dmax();
@@ -102,7 +110,7 @@ double Fccubic::compute(){
 
    // Calculate the coordination number
    Vector myder, fder;
-   double sw, t0, t1, t2, t3, x2, x4, y2, y4, z2, z4, r8, r12, tmp;
+   double sw, t0, t1, t2, t3, x2, x4, y2, y4, z2, z4, r8, r12, tmp, a1, b1;
    for(unsigned i=1;i<getNAtoms();++i){
       distance=getSeparation( getPosition(0), getPosition(i) );
       double d2 = distance.modulo2();
@@ -123,12 +131,12 @@ double Fccubic::compute(){
          r8 = pow( distance.modulo2(), 4 );
          r12 = pow( distance.modulo2(), 6 );
 
-         tmp = ((x4*y4)+(x4*z4)+(y4*z4))/r8-3.0*x4*y4*z4/r12;
+         tmp = ((x4*y4)+(x4*z4)+(y4*z4))/r8-alpha*x4*y4*z4/r12;
 
-         t0 = (x2*y4+x2*z4)/r8-3.0*x2*y4*z4/r12;
-         t1 = (y2*x4+y2*z4)/r8-3.0*y2*x4*z4/r12;
-         t2 = (z2*x4+z2*y4)/r8-3.0*z2*x4*y4/r12;
-         t3 = (2*tmp-3.0*x4*y4*z4/r12)/distance.modulo2();         
+         t0 = (x2*y4+x2*z4)/r8-alpha*x2*y4*z4/r12;
+         t1 = (y2*x4+y2*z4)/r8-alpha*y2*x4*z4/r12;
+         t2 = (z2*x4+z2*y4)/r8-alpha*z2*x4*y4/r12;
+         t3 = (2*tmp-alpha*x4*y4*z4/r12)/distance.modulo2();         
  
          myder[0]=4*distance[0]*(t0-t3);
          myder[1]=4*distance[1]*(t1-t3);
@@ -136,10 +144,11 @@ double Fccubic::compute(){
          
          // Scaling so that '1' corresponds to fcc lattice
          // and '0' corresponds to liquid
-         tmp = (2288.0*tmp-64.0)/79.0;
-         myder[0] = (2288.0*myder[0]-64.0)/79.0;
-         myder[1] = (2288.0*myder[1]-64.0)/79.0;
-         myder[2] = (2288.0*myder[2]-64.0)/79.0;
+         a1 = 80080. / (2717. + 16*alpha); b1 = 16.*(alpha-143)/(2717+16*alpha);
+         tmp = a1*tmp+b1;
+         myder[0] = a1*myder[0]; 
+         myder[1] = a1*myder[1];
+         myder[2] = a1*myder[2];
           
          value += sw*tmp;
   
