eceb82d0d17cf3a38e637d075db0936ea0e42535
Gareth Tribello <gareth.tribello@gmail.com>
2015-02-07 10:58:51 +0000

    Small optimization to BridgedMultiColvarFunction

--- src/multicolvar/BridgedMultiColvarFunction.cpp
+++ src/multicolvar/BridgedMultiColvarFunction.cpp
@@ -68,7 +68,13 @@ void BridgedMultiColvarFunction::performTask(){
   }
 
   completeTask();
-  atoms_with_derivatives.updateActiveMembers();
+  atoms_with_derivatives.emptyActiveMembers();
+  if( mycolv->isDensity() ){
+     for(unsigned j=0;j<mycolv->atomsWithCatomDer.getNumberActive();++j) atoms_with_derivatives.updateIndex( mycolv->atomsWithCatomDer[j] );
+  } else {
+     for(unsigned j=0;j<mycolv->atoms_with_derivatives.getNumberActive();++j) atoms_with_derivatives.updateIndex( mycolv->atoms_with_derivatives[j] );
+  }
+  atoms_with_derivatives.sortActiveList();
 }
 
 Vector BridgedMultiColvarFunction::retrieveCentralAtomPos(){
@@ -88,7 +94,8 @@ Vector BridgedMultiColvarFunction::retrieveCentralAtomPos(){
              addElementDerivative(nbase + nx + 2, mycolv->getElementDerivative(nbas2 + nx + 2) ); 
          } 
       }
-      atomsWithCatomDer.updateActiveMembers();  // This can perhaps be faster
+      for(unsigned j=0;j<mycolv->atomsWithCatomDer.getNumberActive();++j) atomsWithCatomDer.updateIndex( mycolv->atomsWithCatomDer[j] );
+      atomsWithCatomDer.sortActiveList();
       return cvec;
   }
   Vector cvec;
