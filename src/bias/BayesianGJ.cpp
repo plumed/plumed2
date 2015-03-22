f5341f4e5c0977bf3086e2374412c808a4b7e74b
Massimiliano Bonomi <massimiliano.bonomi@gmail.com>
2015-03-22 16:56:41 +0100

    improvements and clean up

--- src/bias/BayesianGJ.cpp
+++ src/bias/BayesianGJ.cpp
@@ -21,8 +21,11 @@
 +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++ */
 #include "Bias.h"
 #include "ActionRegister.h"
-
+#include "core/PlumedMain.h"
+#include "core/Atoms.h"
 #include <cmath>
+#include <ctime>
+#include "time.h"
 
 using namespace std;
 
@@ -51,7 +54,7 @@ to print the values of the uncertainty parameter, MC acceptance, and Bayesian sc
 \verbatim
 WHOLEMOLECULES ENTITY0=1-174
 cs:  CS2BACKBONE ATOMS=1-174 DATA=data/ FF=a03_gromacs.mdb FLAT=0.0 NRES=13 ENSEMBLE COMPONENTS
-csb: BAYESIANGJ ARG=cs.ha SIGMA0=1.0 SIGMA_MIN=0.00001 SIGMA_MAX=10.0 DSIGMA=0.1 KBT=2.494 NDATA=13 MC_STEPS=1 MC_STRIDE=1 MC_SEED=1234
+csb: BAYESIANGJ ARG=cs.ha SIGMA0=1.0 SIGMA_MIN=0.00001 SIGMA_MAX=10.0 DSIGMA=0.1 NDATA=13
 PRINT ARG=csb.sigma,csb.accept,csb.bias
 \endverbatim
 (See also \ref CS2BACKBONE and \ref PRINT).
@@ -64,26 +67,25 @@ PRINT ARG=csb.sigma,csb.accept,csb.bias
 class BayesianGJ : public Bias
 {
   const double sqrt2pi = 2.506628274631001;
-// sigma is data uncertainty
+  // sigma is data uncertainty
   double sigma_;
   double sigma_min_;
   double sigma_max_;
   double Dsigma_;
-// sigma_mean is uncertainty on the mean estimate
+  // sigma_mean is uncertainty in the mean estimate
   double sigma_mean_;
-// temperature in kbt
-  double temp_;
-// number of data points
+  // temperature in kbt
+  double kbt_;
+  // number of data points
   int ndata_;
-// number of replicas
+  // number of replicas
   int nrep_;
-// Monte Carlo stuff
+  // Monte Carlo stuff
   int MCsteps_;
   int MCstride_;
-  unsigned int MCseed_;
   unsigned int MCaccept_;
   long int MCfirst_;
-// output
+  // output
   Value* valueBias;
   Value* valueSigma;
   Value* valueAccept;
@@ -105,17 +107,14 @@ PLUMED_REGISTER_ACTION(BayesianGJ,"BAYESIANGJ")
 void BayesianGJ::registerKeywords(Keywords& keys){
   Bias::registerKeywords(keys);
   keys.use("ARG");
-  keys.add("compulsory","SIGMA0",   "1.0",    "initial value of the uncertainty parameter");
-  keys.add("compulsory","SIGMA_MIN","0.00001","minimum value of the uncertainty parameter");
-  keys.add("compulsory","SIGMA_MAX","10.0",   "maximum value of the uncertainty parameter");
-  keys.add("compulsory","DSIGMA",   "0.1",    "maximum MC move of the uncertainty parameter");
-  keys.add("compulsory","SIGMA_MEAN","0.1",    "uncertainty in the mean estimate");
-  keys.add("compulsory","KBT",      "2.494",  "temperature of the system in energy units");
-  keys.add("compulsory","NDATA",              "number of experimental data points");
-  keys.add("compulsory","NREP",               "number of replicas");
-  keys.add("compulsory","MC_STEPS", "1",      "number of MC steps");
-  keys.add("compulsory","MC_STRIDE","1",      "MC stride");
-  keys.add("compulsory","MC_SEED",  "1234",   "MC seed");
+  keys.add("compulsory","SIGMA0","initial value of the uncertainty parameter");
+  keys.add("compulsory","SIGMA_MIN","minimum value of the uncertainty parameter");
+  keys.add("compulsory","SIGMA_MAX","maximum value of the uncertainty parameter");
+  keys.add("compulsory","DSIGMA","maximum MC move of the uncertainty parameter");
+  keys.add("compulsory","SIGMA_MEAN","uncertainty in the mean estimate");
+  keys.add("compulsory","NDATA","number of experimental data points");
+  keys.add("optional","MC_STEPS","number of MC steps");
+  keys.add("optional","MC_STRIDE","MC stride");
   componentsAreNotOptional(keys); 
   keys.addOutputComponent("bias","default","the instantaneous value of the bias potential");
   keys.addOutputComponent("sigma", "default","uncertainty parameter");
@@ -124,34 +123,40 @@ void BayesianGJ::registerKeywords(Keywords& keys){
 }
 
 BayesianGJ::BayesianGJ(const ActionOptions&ao):
-PLUMED_BIAS_INIT(ao),
-sigma_(1.0), sigma_min_(0.00001), sigma_max_(10.0), Dsigma_(0.1), sigma_mean_(0.1),
-temp_(2.494), MCsteps_(1), MCstride_(1), MCseed_(1234), MCaccept_(0), MCfirst_(-1)
+PLUMED_BIAS_INIT(ao), MCsteps_(1), MCstride_(1), MCaccept_(0), MCfirst_(-1)
 {
-  parse("SIGMA0",   sigma_);
+  parse("SIGMA0",sigma_);
   parse("SIGMA_MIN",sigma_min_);
   parse("SIGMA_MAX",sigma_max_);
-  parse("DSIGMA",   Dsigma_);
+  parse("DSIGMA",Dsigma_);
   parse("SIGMA_MEAN",sigma_mean_);
-  parse("KBT",      temp_);
-  parse("NDATA",    ndata_);
-  parse("NREP",     nrep_);
-  parse("MC_STEPS", MCsteps_);
+  parse("NDATA",ndata_);
+  parse("MC_STEPS",MCsteps_);
   parse("MC_STRIDE",MCstride_);
-  parse("MC_SEED",  MCseed_);
   checkRead();
 
-  log.printf("  initial value of uncertainty %f\n",sigma_);
-  log.printf("  minimum value of uncertainty %f\n",sigma_min_);
-  log.printf("  maximum value of uncertainty %f\n",sigma_max_);
-  log.printf("  maximum MC move of the uncertainty parameter %f\n",Dsigma_);
+  // get temperature
+  kbt_ = plumed.getAtoms().getKbT();
+
+  // get number of replicas
+  nrep_ = multi_sim_comm.Get_size();
+
+  // divide sigma_mean by the square root of the number of replicas
+  sigma_mean_ /= sqrt(static_cast<double>(nrep_));
+
+  // adjust for multiple-time steps
+  MCstride_ *= getStride();
+
+  log.printf("  initial data uncertainty %f\n",sigma_);
+  log.printf("  minimum data uncertainty %f\n",sigma_min_);
+  log.printf("  maximum data uncertainty %f\n",sigma_max_);
+  log.printf("  maximum MC move of data uncertainty %f\n",Dsigma_);
   log.printf("  uncertainty in the mean estimate %f\n",sigma_mean_);
-  log.printf("  temperature of the system %f\n",temp_);
+  log.printf("  temperature of the system %f\n",kbt_);
   log.printf("  number of experimental data points %d\n",ndata_);
   log.printf("  number of replicas %d\n",nrep_);
-  log.printf("  number of MC steps %d\n",MCsteps_);
-  log.printf("  do MC every %d steps\n", MCstride_);
-  log.printf("  MC seed %d\n", MCseed_);    
+  log.printf("  MC steps %d\n",MCsteps_);
+  log.printf("  MC stride %d\n",MCstride_);
 
   addComponent("bias");   componentIsNotPeriodic("bias");
   addComponent("sigma");  componentIsNotPeriodic("sigma");
@@ -162,14 +167,13 @@ temp_(2.494), MCsteps_(1), MCstride_(1), MCseed_(1234), MCaccept_(0), MCfirst_(-
   valueAccept=getPntrToComponent("accept");
   valueKappa=getPntrToComponent("kappa");
 
+
   // initialize random seed
-  srand (MCseed_);
-
-  // divide sigma_mean by the square root of the number of replicas
-  sigma_mean_ /= sqrt(static_cast<double>(nrep_));
-
-  // adjust for multiple-time steps
-  MCstride_ *= getStride();
+  unsigned int iseed;
+  if(comm.Get_rank()==0) iseed = time(NULL)+multi_sim_comm.Get_rank();
+  else iseed = 0;     
+  comm.Sum(&iseed, 1);
+  srand(iseed);
 
 }
 
@@ -181,9 +185,9 @@ double BayesianGJ::getEnergy(double sigma){
   for(unsigned i=0;i<getNumberOfArguments();++i){
     ene += getArgument(i);
   }
-  // add normalizations and priors
-  ene = 0.5*static_cast<double>(nrep_)*ene/s/s + std::log(sigma) + static_cast<double>(ndata_*nrep_)*std::log(s*sqrt2pi);
-  return temp_ * ene;
+  // add normalization and Jeffrey's prior
+  ene = 0.5*ene/s/s + std::log(s) + static_cast<double>(ndata_)*std::log(s*sqrt2pi);
+  return kbt_ * ene;
 }
 
 void BayesianGJ::doMonteCarlo(){
@@ -201,7 +205,7 @@ void BayesianGJ::doMonteCarlo(){
   // calculate new energy
   double new_energy = getEnergy(new_sigma);
   // accept or reject
-  double delta = ( new_energy - old_energy ) / temp_;
+  double delta = ( new_energy - old_energy ) / kbt_;
   // if delta is negative always accept move
   if( delta < 0.0 ){
    old_energy = new_energy;
@@ -223,9 +227,9 @@ void BayesianGJ::update(){
   // get time step 
   long int step = getStep();
   // do MC stuff at the right time step
-  if(step%MCstride_==0){doMonteCarlo();}
+  if(step%MCstride_==0) doMonteCarlo();
   // this is needed when restarting simulations
-  if(MCfirst_==-1){MCfirst_=step;}
+  if(MCfirst_==-1) MCfirst_=step;
   // calculate acceptance
   double MCtrials = std::floor(static_cast<double>(step-MCfirst_) / static_cast<double>(MCstride_))+1.0;
   double accept = static_cast<double>(MCaccept_) / static_cast<double>(MCsteps_) / MCtrials;
@@ -236,22 +240,36 @@ void BayesianGJ::update(){
 void BayesianGJ::calculate(){
   // calculate effective sigma
   double s = sqrt( sigma_*sigma_ + sigma_mean_*sigma_mean_ );
+
+  // communicate with other replicas
+  double inv_s2;
+  // inter-replicas summation
+  if(comm.Get_rank()==0){
+     inv_s2 = 1.0/s/s;
+     multi_sim_comm.Sum(&inv_s2,1); 
+  } else {
+     inv_s2 = 0.0;
+  }
+  // intra-replica summation
+  comm.Sum(&inv_s2,1);  
+  
   // cycle on arguments 
   double ene = 0.0;
   for(unsigned i=0;i<getNumberOfArguments();++i){
     // increment energy
     ene += getArgument(i); 
     // set derivatives
-    setOutputForce(i, -0.5*static_cast<double>(nrep_)*temp_/s/s);
+    setOutputForce(i, -0.5*kbt_*inv_s2);
   };
+
   // add normalizations and priors
-  ene = 0.5*static_cast<double>(nrep_)*ene/s/s + std::log(sigma_) + static_cast<double>(ndata_*nrep_)*std::log(s*sqrt2pi);
+  ene = 0.5*ene*inv_s2 + std::log(s) + static_cast<double>(ndata_)*std::log(s*sqrt2pi);
   // set value of the bias
-  valueBias->set(temp_*ene);
+  valueBias->set(kbt_*ene);
   // set value of data uncertainty
   valueSigma->set(sigma_);
   // and of the harmonic restraint
-  valueKappa->set(static_cast<double>(nrep_)*temp_/s/s);
+  valueKappa->set(kbt_*inv_s2);
 }
 
 
