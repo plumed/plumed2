206087c26e16578a5c596f1d64b14fc7a56fcc75
Giovanni Bussi <giovanni.bussi@gmail.com>
2015-06-04 08:18:54 +0200

    Replaced PLUMED_PREFIX with prefix
    This makes it compatible with the standard behavior of GNU tools.
    
    PLUMED_PREFIX still works but is now deprecated and dumps a warning.
    I will convert it to an error in a later version (2.3).

--- src/maketools/update-plumedroot.sh
+++ src/maketools/update-plumedroot.sh
@@ -11,15 +11,27 @@ prefix="${prefix:=/usr/local}"
 
 # if environment variable PLUMED_PREFIX is set,
 # override the present prefix
-PLUMED_PREFIX="${PLUMED_PREFIX:=$prefix}"
+if test -n "$PLUMED_PREFIX" ; then
+  echo "++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++"
+  echo "WARNING: using PLUMED_PREFIX variable is deprecated"
+  echo "         please use one of the following choices:"
+  echo "         (1) at configure time:"
+  echo "           ./configure --prefix=$PLUMED_PREFIX"
+  echo "         (2) or later, at install time:"
+  echo "           make install prefix=$PLUMED_PREFIX"
+  echo "++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++"
+# this will result in an error in a later version
+# now we fall back to the previous behavior
+  prefix="${PLUMED_PREFIX}"
+fi
 
 PLUMED_LIBSUFFIX="${PLUMED_LIBSUFFIX:=}"
 test -n "$PLUMED_LIBSUFFIX" && PLUMED_LIBSUFFIX="-${PLUMED_LIBSUFFIX}"
-PLUMED_ROOT="${PLUMED_PREFIX}/lib/plumed${PLUMED_LIBSUFFIX}/"
+PLUMED_ROOT="$prefix/lib/plumed${PLUMED_LIBSUFFIX}/"
 
 {
 echo "PLUMED_INSTALL_ROOT=${PLUMED_ROOT}"
-echo "PLUMED_INSTALL_PREFIX=${PLUMED_PREFIX}"
+echo "PLUMED_INSTALL_PREFIX=$prefix"
 echo "PLUMED_INSTALL_LIBSUFFIX=${PLUMED_LIBSUFFIX}"
 } > install.conf
 
