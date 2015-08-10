82aac7b8207e5cd3a242d10e09b1c9f26bc75a72
Gareth Tribello <gt@eider.phy.qub.ac.uk>
2015-08-10 12:25:56 +0100

    Now possible to use moments with PAMM

--- src/multicolvar/PAMM.cpp
+++ src/multicolvar/PAMM.cpp
@@ -132,7 +132,7 @@ void PAMM::registerKeywords( Keywords& keys ){
   keys.add("compulsory","CLUSTERS","the name of the file that contains the definitions of all the clusters");
   keys.add("compulsory","REGULARISE","0.001","don't allow the denominator to be smaller then this value");
   keys.use("MEAN"); keys.use("MORE_THAN"); keys.use("SUM"); keys.use("LESS_THAN"); keys.use("HISTOGRAM"); keys.use("HISTOGRAM");
-  keys.use("MIN"); keys.use("MAX"); keys.use("LOWEST"); keys.use("HIGHEST"); keys.use("ALT_MIN"); keys.use("BETWEEN");
+  keys.use("MIN"); keys.use("MAX"); keys.use("LOWEST"); keys.use("HIGHEST"); keys.use("ALT_MIN"); keys.use("BETWEEN"); keys.use("MOMENTS");
   keys.setComponentsIntroduction("When the label of this action is used as the input for a second you are not referring to a scalar quantity as you are in "
                                  "regular collective variables.  The label is used to reference the full set of quantities calculated by "
                                  "the action.  This is usual when using \\ref multicolvarfunction. Generally when doing this the set of PAMM variables "
@@ -201,7 +201,6 @@ MultiColvarFunction(ao)
 
    // This builds the lists
    buildSets();
-
 
    // I think we can put this back for anything with not usespecies
    // Now need to pass cutoff information to underlying distance multicolvars
