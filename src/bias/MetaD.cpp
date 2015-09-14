plumed/plumed2

commit 5e3aaf3aa633c894a1579c71333392ab38bc48fe

Added barriers
It seems that sometime on travis a regtest with mw-mpi crashes.

I think this is because when using multiple processors per walker
there is a problem in the opening of the old file (for read) and
its backup (for write). Adding this barrier seems to make it more robust

src/bias/MetaD.cpp
@@ -658,6 +658,9 @@ last_step_warn_grid(0)
    }
   }
 
+  comm.Barrier();
+  if(comm.Get_rank()==0) multi_sim_comm.Barrier();
+
 // open hills file for writing
   hillsOfile_.link(*this);
   hillsOfile_.open(ifilesnames[mw_id_]);

commit 4d7f515a83b899419aca1e5d41c059bd3db94820

Fixed #168
This bug was introduced by me in 5e3aaf3aa633c894a1579c71333392ab38bc48fe
The barrier on multi replicas was due to overcaution and
is only necessary when using WALKERS_MPI

src/bias/MetaD.cpp
@@ -659,7 +659,15 @@ last_step_warn_grid(0)
   }
 
   comm.Barrier();
-  if(comm.Get_rank()==0) multi_sim_comm.Barrier();
+
+// this barrier is needed when using walkers_mpi
+// to be sure that all files have been read before
+// backing them up
+// it should not be used when walkers_mpi is false otherwise
+// it would introduce troubles when using replicas without METAD
+// (e.g. in bias exchange with a neutral replica)
+// see issue #168 on github
+  if(comm.Get_rank()==0 && walkers_mpi) multi_sim_comm.Barrier();
 
 // open hills file for writing
   hillsOfile_.link(*this);