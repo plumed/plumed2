2684a55be3491b801aa2eaeb90cdf3bc2b127284
Carlo Camilloni <carlo.camilloni@gmail.com>
2016-01-05 15:23:04 +0100

    STATS: headers and doc

--- src/function/Stats.cpp
+++ src/function/Stats.cpp
@@ -1,5 +1,5 @@
 /* +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
-   Copyright (c) 2015 The plumed team
+   Copyright (c) 2015-2016 The plumed team
    (see the PEOPLE file at the root of the distribution for a list of names)
 
    See http://www.plumed-code.org for more information.
@@ -22,8 +22,6 @@
 #include "Function.h"
 #include "ActionRegister.h"
 
-#include <cmath>
-
 using namespace std;
 
 namespace PLMD{
@@ -31,7 +29,24 @@ namespace function{
 
 //+PLUMEDOC FUNCTION STATS
 /*
+Calculates statistical properties of a set of collective variables with respect to a set of reference values.
+In particular it calculates and store as components the sum of the squared deviations, the correlation, the
+slope and the intercept of a linear fit.
 
+The reference values can be either provided as values using PARAMETERS or using value without derivatives
+from other actions using PARARG (for example using experimental values from collective variables such as
+\ref CS2BACKBONE, \ref RDC, \ref NOE, ref PRE).
+
+\par Examples
+The following input tells plumed to print the distance between three couple of atoms 
+and compare them with three reference distances.
+
+\verbatim
+d1: DISTANCE ATOMS=10,50
+d2: DISTANCE ATOMS=1,100
+d3: DISTANCE ATOMS=45,75
+st: STATS ARG=d1,d2,d3 PARAMETERS=1.5,4.0,2.0
+PRINT ARG=d1,d2,d3,st.*
 \endverbatim
 
 */
@@ -55,16 +70,7 @@ PLUMED_REGISTER_ACTION(Stats,"STATS")
 void Stats::registerKeywords(Keywords& keys){
   Function::registerKeywords(keys);
   keys.use("ARG");
-  keys.add("optional","PARARG","the input for this action is the scalar output from one or more other actions. The particular scalars that you will use "
-                                  "are referenced using the label of the action. If the label appears on its own then it is assumed that the Action calculates "
-                                  "a single scalar value.  The value of this scalar is thus used as the input to this new action.  If * or *.* appears the "
-                                  "scalars calculated by all the proceding actions in the input file are taken.  Some actions have multi-component outputs and "
-                                  "each component of the output has a specific label.  For example a \\ref DISTANCE action labelled dist may have three componets "
-                                  "x, y and z.  To take just the x component you should use dist.x, if you wish to take all three components then use dist.*."
-                                  "More information on the referencing of Actions can be found in the section of the manual on the PLUMED \\ref Syntax.  "
-                                  "Scalar values can also be "
-                                  "referenced using POSIX regular expressions as detailed in the section on \\ref Regex. To use this feature you you must compile "
-                                  "PLUMED with the appropriate flag."); 
+  keys.add("optional","PARARG","the input for this action is the scalar output from one or more other actions without derivatives."); 
   keys.add("optional","PARAMETERS","the parameters of the arguments in your function");
   keys.addFlag("SQDEVSUM",false,"calculates only SQDEVSUM");
   keys.addOutputComponent("sqdevsum","default","the sum of the squared deviations between arguments and parameters"); 
@@ -93,6 +99,8 @@ sqdonly(false)
       if(arg2[i]->hasDerivatives()==true) error("PARARG can only accept arguments without derivatives");
     }
   }
+
+  if(getNumberOfArguments()<2) error("STATS need at least two arguments to be used");
 
   parseFlag("SQDEVSUM",sqdonly);
 
