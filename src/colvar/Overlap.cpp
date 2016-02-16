19a9d977ea66a6c100a7b964d31018f4ed6a8bc1
Massimiliano Bonomi <massimiliano.bonomi@gmail.com>
2016-02-16 21:53:36 +0000

    initial commit for overlap CV

---
+++ src/colvar/Overlap.cpp
@@ -0,0 +1,217 @@
+/* +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+   Copyright (c) 2016 The plumed team
+   (see the PEOPLE file at the root of the distribution for a list of names)
+
+   See http://www.plumed.org for more information.
+
+   This file is part of plumed, version 2.
+
+   plumed is free software: you can redistribute it and/or modify
+   it under the terms of the GNU Lesser General Public License as published by
+   the Free Software Foundation, either version 3 of the License, or
+   (at your option) any later version.
+
+   plumed is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU Lesser General Public License for more details.
+
+   You should have received a copy of the GNU Lesser General Public License
+   along with plumed.  If not, see <http://www.gnu.org/licenses/>.
++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++ */
+#include "Colvar.h"
+#include "ActionRegister.h"
+#include "core/PlumedMain.h"
+#include "tools/Matrix.h"
+#include "core/SetupMolInfo.h"
+#include "core/ActionSet.h"
+#include "tools/File.h"
+
+#include <string>
+#include <cmath>
+#include <unordered_map>
+
+using namespace std;
+
+namespace PLMD{
+namespace colvar{
+
+//+PLUMEDOC COLVAR OVERLAP
+/*
+Put Documentation here 
+
+*/
+//+ENDPLUMEDOC
+   
+class Overlap : public Colvar {
+private:
+ vector<double> radii_;
+ vector<Vector> gmm_m_;
+ vector<double> gmm_w_;
+ vector< Matrix <double> > gmm_cov_;
+ void get_radii(vector<AtomNumber> &atoms);
+ void read_GMM_datafile(string gmm_file);
+public:
+  static void registerKeywords( Keywords& keys );
+  explicit Overlap(const ActionOptions&);
+// active methods:
+  virtual void calculate();
+};
+
+PLUMED_REGISTER_ACTION(Overlap,"OVERLAP")
+
+void Overlap::registerKeywords( Keywords& keys ){
+  Colvar::registerKeywords( keys );
+  keys.add("atoms","ATOMS","atoms for which we calculate the density map");
+  keys.add("compulsory","GMM_FILE","file with the parameters of the GMM components");
+  componentsAreNotOptional(keys);
+  keys.addOutputComponent("comp", "COMPONENTS","overlap with individual GMM components");
+}
+
+
+Overlap::Overlap(const ActionOptions&ao):
+PLUMED_COLVAR_INIT(ao)
+
+{
+  vector<AtomNumber> atoms;
+  parseAtomList("ATOMS",atoms);
+  
+  string gmm_file;
+  parse("GMM_FILE",gmm_file);
+  
+  checkRead();
+
+  log.printf("  atoms involved : ");
+  for(unsigned i=0;i<atoms.size();++i) log.printf("%d ",atoms[i].serial());
+  log.printf("\n");
+  log.printf("  GMM data file : %s\n", gmm_file.c_str());
+
+  // calculate radii
+  get_radii(atoms);
+
+  // read GMM data file
+  read_GMM_datafile(gmm_file);
+  log.printf("  number of GMM components : %d\n", gmm_m_.size());
+  
+  for(unsigned i=0;i<gmm_m_.size();++i) {
+      string num; Tools::convert(i,num);
+      addComponentWithDerivatives("comp"+num); componentIsNotPeriodic("comp"+num);  
+  }
+  
+  // normalize GMMs
+  // how should we do this?
+  
+  requestAtoms(atoms);
+}
+
+void Overlap::get_radii(vector<AtomNumber> &atoms)
+{
+  vector<SetupMolInfo*> moldat=plumed.getActionSet().select<SetupMolInfo*>();
+
+  // map of atom types to scattering radii - in nm
+  unordered_map<string, double> radii_map;
+  radii_map["C"] = 1.0;
+  radii_map["O"] = 1.0;
+  radii_map["N"] = 1.0;
+  radii_map["H"] = 1.0;
+  radii_map["S"] = 1.0;
+  
+  // check if MOLINFO line is present 
+  if( moldat.size()==1 ){
+    log<<"  MOLINFO DATA found, using proper atom names\n";
+    for(unsigned i=0;i<atoms.size();++i){
+      // get atom name
+      string name = moldat[0]->getAtomName(atoms[i]);
+      char type;
+      // get atom type
+      char first = name.at(0);
+      // GOLDEN RULE: type is first letter, if not a number
+      if (!isdigit(first)){
+         type = first;
+      // otherwise is the second
+      } else {
+         type = name.at(1);
+      }
+      // check if key in map
+      std::string type_s = std::string(1,type);
+      if(radii_map.find(type_s) != radii_map.end()){
+        radii_.push_back(radii_map[type_s]);
+      } else {
+        error("Wrong atom type "+type_s+" from atom name "+name+"\n"); 
+      }
+    }
+  } else {
+    error("MOLINFO DATA not found\n");
+  }
+}
+
+// read GMM data file in IMP or PLUMED format:
+// |Id|Weight|mean|covariance matrix|
+void Overlap::read_GMM_datafile(string gmm_file)
+{
+ int idcomp;
+ double w,m0,m1,m2,c00,c01,c02,c10,c11,c12,c20,c21,c22;
+ 
+ // open file
+ IFile *ifile = new IFile();
+ if(ifile->FileExist(gmm_file)){
+    ifile->open(gmm_file);
+    while(ifile->scanField("Id",idcomp)){
+     ifile->scanField("Weight",w);
+     ifile->scanField("Mean_0",m0);
+     ifile->scanField("Mean_1",m1);
+     ifile->scanField("Mean_2",m2);
+     ifile->scanField("Cov_00",c00);
+     ifile->scanField("Cov_01",c01);
+     ifile->scanField("Cov_02",c02);
+     ifile->scanField("Cov_10",c10);
+     ifile->scanField("Cov_11",c11);
+     ifile->scanField("Cov_12",c12);
+     ifile->scanField("Cov_20",c20);
+     ifile->scanField("Cov_21",c21);
+     ifile->scanField("Cov_22",c22);
+     gmm_w_.push_back(w);
+     gmm_m_.push_back(Vector(m0,m1,m2));
+     Matrix <double> cov(3,3);
+     cov(0,0)=c00; cov(0,1)=c01; cov(0,2)=c02;
+     cov(1,0)=c10; cov(1,1)=c11; cov(1,2)=c12;
+     cov(2,0)=c20; cov(2,1)=c21; cov(2,2)=c22;
+     gmm_cov_.push_back(cov);     
+     ifile->scanField();
+    }
+    ifile->close();
+ } else {
+    error("Cannot find GMM_FILE "+gmm_file+"\n"); 
+ }
+ delete ifile;
+
+}
+
+// normalize GMM
+//Overlap::normalize_GMM()
+//{
+//
+//}
+
+// calculator
+void Overlap::calculate(){
+
+  makeWhole();
+
+  for(unsigned i=0;i<gmm_m_.size();++i) {
+      string num; Tools::convert(i,num);
+      Value* value=getPntrToComponent("comp"+num);
+      value->set(0);
+      for(unsigned j=0;j<getNumberOfAtoms();j++){
+         setAtomsDerivatives (value,j,Vector(0,0,0));
+      }
+  }
+
+
+}
+
+}
+}
+
+
+
