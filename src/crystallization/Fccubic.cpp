ca7caeebd791bcec3e33823887bd5ec60fff84ff
Gareth Tribello <gt@eider.phy.qub.ac.uk>
2016-03-29 13:02:21 +0100

    Moved rotation matrix functionality in Fcccubic into a base class so that it can also be used in SimpleCubic and Tetrahedral

--- src/crystallization/Fccubic.cpp
+++ src/crystallization/Fccubic.cpp
@@ -19,8 +19,7 @@
    You should have received a copy of the GNU Lesser General Public License
    along with plumed.  If not, see <http://www.gnu.org/licenses/>.
 +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++ */
-#include "multicolvar/MultiColvar.h"
-#include "tools/NeighborList.h"
+#include "CubicHarmonicBase.h"
 #include "core/ActionRegister.h"
 #include "tools/SwitchingFunction.h"
 
@@ -41,176 +40,60 @@ namespace crystallization{
 //+ENDPLUMEDOC
 
 
-class Fccubic : public multicolvar::MultiColvar {
+class Fccubic : public CubicHarmonicBase {
 private:
-//  double nl_cut;
-  double rcut2, alpha, a1, b1;
-  double rotationmatrix[3][3]; 
-  
-  SwitchingFunction switchingFunction;
+  double alpha, a1, b1;
 public:
   static void registerKeywords( Keywords& keys );
   explicit Fccubic(const ActionOptions&);
-// active methods:
-  virtual double compute( const unsigned& tindex, multicolvar::AtomValuePack& myatoms ) const ; 
-/// Returns the number of coordinates of the field
-  bool isPeriodic(){ return false; }
+  double calculateCubicHarmonic( const Vector& distance, const double& d2, Vector& myder ) const ;
 };
 
 PLUMED_REGISTER_ACTION(Fccubic,"FCCUBIC")
 
 void Fccubic::registerKeywords( Keywords& keys ){
-  multicolvar::MultiColvar::registerKeywords( keys );
-  keys.use("SPECIES"); keys.use("SPECIESA"); keys.use("SPECIESB");
-  keys.add("compulsory","NN","6","The n parameter of the switching function ");
-  keys.add("compulsory","MM","0","The m parameter of the switching function; 0 implies 2*NN");
-  keys.add("compulsory","D_0","0.0","The d_0 parameter of the switching function");
-  keys.add("compulsory","R_0","The r_0 parameter of the switching function");
-  keys.add("optional","SWITCH","This keyword is used if you want to employ an alternative to the continuous swiching function defined above. "
-                               "The following provides information on the \\ref switchingfunction that are available. "
-                               "When this keyword is present you no longer need the NN, MM, D_0 and R_0 keywords.");
+  CubicHarmonicBase::registerKeywords( keys );
   keys.add("compulsory","ALPHA","3.0","The alpha parameter of the angular function");
-  keys.add("compulsory","PHI","0.0","The Euler rotational angle phi");
-  keys.add("compulsory","THETA","0.0","The Euler rotational angle theta");
-  keys.add("compulsory","PSI","0.0","The Euler rotational angle psi");
-  // Use actionWithDistributionKeywords
-  keys.use("MEAN"); keys.use("MORE_THAN"); keys.use("LESS_THAN"); keys.use("MAX");
-  keys.use("MIN"); keys.use("BETWEEN"); keys.use("HISTOGRAM"); keys.use("MOMENTS");
-  keys.use("ALT_MIN"); keys.use("LOWEST"); keys.use("HIGHEST"); 
 }
 
 Fccubic::Fccubic(const ActionOptions&ao):
-PLUMED_MULTICOLVAR_INIT(ao)
+Action(ao),
+CubicHarmonicBase(ao)
 {
-  // Read in the switching function
-  std::string sw, errors; parse("SWITCH",sw);
-  if(sw.length()>0){
-     switchingFunction.set(sw,errors);
-     if( errors.length()!=0 ) error("problem reading SWITCH keyword : " + errors );
-  } else { 
-     double r_0=-1.0, d_0; int nn, mm;
-     parse("NN",nn); parse("MM",mm);
-     parse("R_0",r_0); parse("D_0",d_0);
-     if( r_0<0.0 ) error("you must set a value for R_0");
-     switchingFunction.set(nn,mm,r_0,d_0);
-  }
-  
-  double phi, theta, psi; parse("PHI",phi); parse("THETA",theta); parse("PSI",psi);
-  log.printf("  creating rotation matrix with Euler angles phi=%f, theta=%f and psi=%f\n",phi,theta,psi);
-  // Calculate the rotation matrix http://mathworld.wolfram.com/EulerAngles.html
-  rotationmatrix[0][0]=cos(psi)*cos(phi)-cos(theta)*sin(phi)*sin(psi);
-  rotationmatrix[0][1]=cos(psi)*sin(phi)+cos(theta)*cos(phi)*sin(psi);
-  rotationmatrix[0][2]=sin(psi)*sin(theta);
-  
-  rotationmatrix[1][0]=-sin(psi)*cos(phi)-cos(theta)*sin(phi)*cos(psi);
-  rotationmatrix[1][1]=-sin(psi)*sin(phi)+cos(theta)*cos(phi)*cos(psi);
-  rotationmatrix[1][2]=cos(psi)*sin(theta);
-  
-  rotationmatrix[2][0]=sin(theta)*sin(phi);
-  rotationmatrix[2][1]=-sin(theta)*cos(phi);
-  rotationmatrix[2][2]=cos(theta);
-  
-  
-  log.printf("  measure of simple cubicity around central atom.  Includes those atoms within %s\n",( switchingFunction.description() ).c_str() );
-  // Set the link cell cutoff
-  rcut2 = switchingFunction.get_dmax()*switchingFunction.get_dmax();
-  setLinkCellCutoff( switchingFunction.get_dmax() );
-
   // Scaling factors such that '1' corresponds to fcc lattice
   // and '0' corresponds to isotropic (liquid)
   parse("ALPHA",alpha);
   a1 = 80080. / (2717. + 16*alpha); b1 = 16.*(alpha-143)/(2717+16*alpha);
   log.printf("  setting alpha paramter equal to %f \n",alpha);
-
-  // Read in the atoms
-  int natoms=2; readAtoms( natoms );
   // And setup the ActionWithVessel
   checkRead();
 }
 
-double Fccubic::compute( const unsigned& tindex, multicolvar::AtomValuePack& myatoms ) const {
-   double value=0, norm=0, dfunc; Vector rotatedis;
+double Fccubic::calculateCubicHarmonic( const Vector& distance, const double& d2, Vector& myder ) const {
+  double x2 = distance[0]*distance[0];
+  double x4 = x2*x2;
 
-   // Calculate the coordination number
-   Vector myder, rotateder, fder; unsigned nat=myatoms.getNumberOfAtoms();
-   double sw, t0, t1, t2, t3, x2, x4, y2, y4, z2, z4, r8, r12, tmp;
-   
-   double d2; 
-   for(unsigned i=1;i<nat;++i){
-      Vector& distance=myatoms.getPosition(i); 
-      
-      if ( (d2=distance[0]*distance[0])<rcut2 && 
-           (d2+=distance[1]*distance[1])<rcut2 &&
-           (d2+=distance[2]*distance[2])<rcut2) {
-           
-         sw = switchingFunction.calculateSqr( d2, dfunc ); 
-   
-         norm += sw;
+  double y2 = distance[1]*distance[1];
+  double y4 = y2*y2;
 
-         rotatedis[0]=rotationmatrix[0][0]*distance[0]
-                  +rotationmatrix[0][1]*distance[1]
-                  +rotationmatrix[0][2]*distance[2];
-         rotatedis[1]=rotationmatrix[1][0]*distance[0]
-                  +rotationmatrix[1][1]*distance[1]
-                  +rotationmatrix[1][2]*distance[2];
-         rotatedis[2]=rotationmatrix[2][0]*distance[0]
-                  +rotationmatrix[2][1]*distance[1]
-                  +rotationmatrix[2][2]*distance[2];
-                  
-         x2 = rotatedis[0]*rotatedis[0];
-         x4 = x2*x2;
-
-         y2 = rotatedis[1]*rotatedis[1];
-         y4 = y2*y2;
-
-         z2 = rotatedis[2]*rotatedis[2];
-         z4 = z2*z2;
-                 
-         r8 = pow( d2, 4 );
-         r12 = pow( d2, 6 );
-
-         tmp = ((x4*y4)+(x4*z4)+(y4*z4))/r8-alpha*x4*y4*z4/r12;
-
-         t0 = (x2*y4+x2*z4)/r8-alpha*x2*y4*z4/r12;
-         t1 = (y2*x4+y2*z4)/r8-alpha*y2*x4*z4/r12;
-         t2 = (z2*x4+z2*y4)/r8-alpha*z2*x4*y4/r12;
-         t3 = (2*tmp-alpha*x4*y4*z4/r12)/d2;         
- 
-         rotateder[0]=4*rotatedis[0]*(t0-t3);
-         rotateder[1]=4*rotatedis[1]*(t1-t3);
-         rotateder[2]=4*rotatedis[2]*(t2-t3);
-         
-         myder[0]=rotationmatrix[0][0]*rotateder[0]
-                  +rotationmatrix[1][0]*rotateder[1]
-                  +rotationmatrix[2][0]*rotateder[2];
-         myder[1]=rotationmatrix[0][1]*rotateder[0]
-                  +rotationmatrix[1][1]*rotateder[1]
-                  +rotationmatrix[2][1]*rotateder[2];
-         myder[2]=rotationmatrix[0][2]*rotateder[0]
-                  +rotationmatrix[1][2]*rotateder[1]
-                  +rotationmatrix[2][2]*rotateder[2];
-         
-         tmp = a1*tmp+b1;
-         myder *= a1;
-          
-         value += sw*tmp;
+  double z2 = distance[2]*distance[2];
+  double z4 = z2*z2;
   
-         fder = (+dfunc)*tmp*distance + sw*myder;
+  double r8 = pow( d2, 4 );
+  double r12 = pow( d2, 6 );
 
-         addAtomDerivatives( 1, 0, -fder, myatoms );
-         addAtomDerivatives( 1, i, +fder, myatoms);
-         myatoms.addBoxDerivatives( 1, Tensor(distance,-fder) );
-         addAtomDerivatives( -1, 0, (-dfunc)*distance, myatoms);
-         addAtomDerivatives( -1, i, (+dfunc)*distance, myatoms);
-         myatoms.addTemporyBoxDerivatives( (-dfunc)*Tensor(distance,distance) );
-      }
-   }
-   
-   myatoms.setValue(1, value);  
-   // values -> der of... value [0], weight[1], x coord [2], y, z... [more magic]
-   updateActiveAtoms( myatoms ); myatoms.getUnderlyingMultiValue().quotientRule( 1, norm, 1 );   
+  double tmp = ((x4*y4)+(x4*z4)+(y4*z4))/r8-alpha*x4*y4*z4/r12;
 
-   return value / norm; // this is equivalent to getting an "atomic" CV
+  double t0 = (x2*y4+x2*z4)/r8-alpha*x2*y4*z4/r12;
+  double t1 = (y2*x4+y2*z4)/r8-alpha*y2*x4*z4/r12;
+  double t2 = (z2*x4+z2*y4)/r8-alpha*z2*x4*y4/r12;
+  double t3 = (2*tmp-alpha*x4*y4*z4/r12)/d2;         
+ 
+  myder[0]=4*a1*distance[0]*(t0-t3);
+  myder[1]=4*a1*distance[1]*(t1-t3);
+  myder[2]=4*a1*distance[2]*(t2-t3);
+         
+  return a1*tmp+b1;
 }
 
 }
--- src/crystallization/SimpleCubic.cpp
+++ src/crystallization/SimpleCubic.cpp
@@ -19,10 +19,8 @@
    You should have received a copy of the GNU Lesser General Public License
    along with plumed.  If not, see <http://www.gnu.org/licenses/>.
 +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++ */
-#include "multicolvar/MultiColvar.h"
-#include "tools/NeighborList.h"
+#include "CubicHarmonicBase.h"
 #include "core/ActionRegister.h"
-#include "tools/SwitchingFunction.h"
 
 #include <string>
 #include <cmath>
@@ -55,116 +53,47 @@ SIMPLECUBIC SPECIESA=101-110 SPECIESB=1-100 R_0=3.0 MORE_THAN={RATIONAL R_0=0.8 
 //+ENDPLUMEDOC
 
 
-class SimpleCubic : public multicolvar::MultiColvar {
-private:
-//  double nl_cut;
-  double rcut2;
-  SwitchingFunction switchingFunction;
+class SimpleCubic : public CubicHarmonicBase {
 public:
   static void registerKeywords( Keywords& keys );
   explicit SimpleCubic(const ActionOptions&);
-// active methods:
-  virtual double compute( const unsigned& tindex, multicolvar::AtomValuePack& myatoms ) const ; 
-/// Returns the number of coordinates of the field
-  bool isPeriodic(){ return false; }
+  double calculateCubicHarmonic( const Vector& distance, const double& d2, Vector& myder ) const ;
 };
 
 PLUMED_REGISTER_ACTION(SimpleCubic,"SIMPLECUBIC")
 
 void SimpleCubic::registerKeywords( Keywords& keys ){
-  multicolvar::MultiColvar::registerKeywords( keys );
-  keys.use("SPECIES"); keys.use("SPECIESA"); keys.use("SPECIESB");
-  keys.add("compulsory","NN","6","The n parameter of the switching function ");
-  keys.add("compulsory","MM","0","The m parameter of the switching function; 0 implies 2*NN");
-  keys.add("compulsory","D_0","0.0","The d_0 parameter of the switching function");
-  keys.add("compulsory","R_0","The r_0 parameter of the switching function");
-  keys.add("optional","SWITCH","This keyword is used if you want to employ an alternative to the continuous swiching function defined above. "
-                               "The following provides information on the \\ref switchingfunction that are available. "
-                               "When this keyword is present you no longer need the NN, MM, D_0 and R_0 keywords.");
-  // Use actionWithDistributionKeywords
-  keys.use("MEAN"); keys.use("MORE_THAN"); keys.use("LESS_THAN"); keys.use("MAX");
-  keys.use("MIN"); keys.use("BETWEEN"); keys.use("HISTOGRAM"); keys.use("MOMENTS");
-  keys.use("ALT_MIN"); keys.use("LOWEST"); keys.use("HIGHEST"); 
+  CubicHarmonicBase::registerKeywords( keys );
 }
 
 SimpleCubic::SimpleCubic(const ActionOptions&ao):
-PLUMED_MULTICOLVAR_INIT(ao)
+Action(ao),
+CubicHarmonicBase(ao)
 {
-  // Read in the switching function
-  std::string sw, errors; parse("SWITCH",sw);
-  if(sw.length()>0){
-     switchingFunction.set(sw,errors);
-     if( errors.length()!=0 ) error("problem reading SWITCH keyword : " + errors );
-  } else { 
-     double r_0=-1.0, d_0; int nn, mm;
-     parse("NN",nn); parse("MM",mm);
-     parse("R_0",r_0); parse("D_0",d_0);
-     if( r_0<0.0 ) error("you must set a value for R_0");
-     switchingFunction.set(nn,mm,r_0,d_0);
-  }
-  log.printf("  measure of simple cubicity around central atom.  Includes those atoms within %s\n",( switchingFunction.description() ).c_str() );
-  // Set the link cell cutoff
-  rcut2 = switchingFunction.get_dmax()*switchingFunction.get_dmax();
-  setLinkCellCutoff( switchingFunction.get_dmax() );
-
-  // Read in the atoms
-  int natoms=2; readAtoms( natoms );
-  // And setup the ActionWithVessel
   checkRead();
 }
 
-double SimpleCubic::compute( const unsigned& tindex, multicolvar::AtomValuePack& myatoms ) const { 
-   double d2, value=0, norm=0, dfunc; 
+double SimpleCubic::calculateCubicHarmonic( const Vector& distance, const double& d2, Vector& myder ) const { 
+  double x2 = distance[0]*distance[0];
+  double x3 = distance[0]*x2;
+  double x4 = distance[0]*x3;
 
-   // Calculate the coordination number
-   Vector myder, fder;
-   double sw, t1, t2, t3, x2, x3, x4, y2, y3, y4, z2, z3, z4, r4, tmp;
-   for(unsigned i=1;i<myatoms.getNumberOfAtoms();++i){
-      Vector& distance=myatoms.getPosition(i);  // getSeparation( myatoms.getPosition(0), myatoms.getPosition(i) );
-      if ( (d2=distance[0]*distance[0])<rcut2 &&
-           (d2+=distance[1]*distance[1])<rcut2 &&
-           (d2+=distance[2]*distance[2])<rcut2) {
+  double y2 = distance[1]*distance[1];
+  double y3 = distance[1]*y2;
+  double y4 = distance[1]*y3;         
+  
+  double z2 = distance[2]*distance[2];
+  double z3 = distance[2]*z2; 
+  double z4 = distance[2]*z3;
 
-         sw = switchingFunction.calculateSqr( d2, dfunc );        
+  double r4 = pow( d2, 2 );
+  double tmp = ( x4 + y4 + z4 ) / r4;
 
-         x2 = distance[0]*distance[0];
-         x3 = distance[0]*x2;
-         x4 = distance[0]*x3;
-
-         y2 = distance[1]*distance[1];
-         y3 = distance[1]*y2;
-         y4 = distance[1]*y3;         
-   
-         z2 = distance[2]*distance[2];
-         z3 = distance[2]*z2; 
-         z4 = distance[2]*z3;
-
-         r4 = pow( distance.modulo2(), 2 );
-         tmp = ( x4 + y4 + z4 ) / r4;
-
-         t1=(x2+y2+z2); t2=t1*t1; t3=(x4+y4+z4)/(t1*t2);
-         myder[0] = 4*x3/t2-4*distance[0]*t3; 
-         myder[1] = 4*y3/t2-4*distance[1]*t3; 
-         myder[2] = 4*z3/t2-4*distance[2]*t3; 
-
-         value += sw*tmp; fder = (+dfunc)*tmp*distance + sw*myder;
-         addAtomDerivatives( 1, 0, -fder, myatoms );
-         addAtomDerivatives( 1, i, +fder, myatoms );
-         // Tens is a constructor that you build by taking the vector product of two vectors (remember the scalars!)
-         myatoms.addBoxDerivatives( 1, Tensor(distance,-fder) );
- 
-         norm += sw;
-         addAtomDerivatives( -1, 0, (-dfunc)*distance, myatoms );
-         addAtomDerivatives( -1, i, (+dfunc)*distance, myatoms );
-         myatoms.addTemporyBoxDerivatives( (-dfunc)*Tensor(distance,distance) );
-      }
-   }
-   
-   myatoms.setValue(1, value);  
-   // values -> der of... value [0], weight[1], x coord [2], y, z... [more magic]
-   updateActiveAtoms( myatoms ); myatoms.getUnderlyingMultiValue().quotientRule( 1, norm, 1 ); 
-
-   return value / norm; // this is equivalent to getting an "atomic" CV
+  double t1=(x2+y2+z2), t2=t1*t1, t3=(x4+y4+z4)/(t1*t2);
+  myder[0] = 4*x3/t2-4*distance[0]*t3; 
+  myder[1] = 4*y3/t2-4*distance[1]*t3; 
+  myder[2] = 4*z3/t2-4*distance[2]*t3; 
+  return tmp; 
 }
 
 }
