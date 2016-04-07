087f2560ac3448b2a268a1e3c4d9c2f7829ff752
Giovanni Bussi <giovanni.bussi@gmail.com>
2016-04-07 15:58:05 +0200

    Fixed hrex with gmx 5.1.2 with NPT
    Volume data were not exchanged properly in hrex.
    Bug was appearing only when not using domain decomposition.

--- patches/gromacs-5.1.2.diff/src/programs/mdrun/repl_ex.h
+++ patches/gromacs-5.1.2.diff/src/programs/mdrun/repl_ex.h
@@ -76,6 +76,7 @@ extern int replica_exchange_get_repl(const gmx_repl_ex_t re);
 extern int replica_exchange_get_nrepl(const gmx_repl_ex_t re);
 extern void pd_collect_state(const t_commrec *cr, t_state *state);
 extern void exchange_state(const gmx_multisim_t *ms, int b, t_state *state);
+extern void copy_state_nonatomdata(t_state *state, t_state *state_local);
 
 #ifdef __cplusplus
 }
