plumed/plumed2

commit e2f37a30d8081e8911e93d6e13d0241f0215f1ac

plumedcheck
Fix namespace name crystallisation -> crystallization

src/crystallization/InterMolecularTorsions.cpp
@@ -37,7 +37,7 @@ Calculate torsions between axis of adjacent molecules
 //+ENDPLUMEDOC
 
 namespace PLMD {
-namespace multicolvar {
+namespace crystallization {
 
 class InterMolecularTorsions : public MultiColvarFunction {
 private:

src/crystallization/Gradient.cpp
@@ -24,7 +24,7 @@
 #include "Gradient.h"
 
 namespace PLMD {
-namespace crystallisation {
+namespace crystallization {
 
 //+PLUMEDOC MCOLVARF GRADIENT
 /*

src/crystallization/Gradient.h
@@ -25,7 +25,7 @@
 #include "multicolvar/VolumeGradientBase.h"
 
 namespace PLMD {
-namespace crystallisation {
+namespace crystallization {
 
 class Gradient : public multicolvar::VolumeGradientBase {
 friend class GradientVessel;

src/crystallization/GradientVessel.cpp
@@ -27,7 +27,7 @@
 #include "Gradient.h"
 
 namespace PLMD {
-namespace crystallisation {
+namespace crystallization {
 
 class GradientVessel : public vesselbase::FunctionVessel {
 private:

commit d96d52d980a26273fb9661b30f6ffc62b585f819

Fix
fixed an error introduced in
e2f37a30d8081e8911e93d6e13d0241f0215f1ac

src/crystallization/InterMolecularTorsions.cpp
@@ -39,7 +39,7 @@ Calculate torsions between axis of adjacent molecules
 namespace PLMD {
 namespace crystallization {
 
-class InterMolecularTorsions : public MultiColvarFunction {
+class InterMolecularTorsions : public multicolvar::MultiColvarFunction {
 private:
 /// The switching function that tells us if atoms are close enough together
   SwitchingFunction switchingFunction;
@@ -47,9 +47,9 @@ public:
   static void registerKeywords( Keywords& keys );
   explicit InterMolecularTorsions(const ActionOptions&);
 /// Do the stuff with the switching functions
-  double calculateWeight( const unsigned& taskCode, const double& weight, AtomValuePack& myatoms ) const ;
+  double calculateWeight( const unsigned& taskCode, const double& weight, multicolvar::AtomValuePack& myatoms ) const ;
 /// Actually do the calculation
-  double compute( const unsigned& tindex, AtomValuePack& myatoms ) const ;
+  double compute( const unsigned& tindex, multicolvar::AtomValuePack& myatoms ) const ;
 /// Is the variable periodic
   bool isPeriodic(){ return true; }
   void retrieveDomain( std::string& min, std::string& max ){ min="-pi"; max="+pi"; }
@@ -112,7 +112,7 @@ MultiColvarFunction(ao)
   readVesselKeywords();
 }
 
-double InterMolecularTorsions::calculateWeight( const unsigned& taskCode, const double& weight, AtomValuePack& myatoms ) const {
+double InterMolecularTorsions::calculateWeight( const unsigned& taskCode, const double& weight, multicolvar::AtomValuePack& myatoms ) const {
   Vector distance = getSeparation( myatoms.getPosition(0), myatoms.getPosition(1) );
   double dfunc, sw = switchingFunction.calculateSqr( distance.modulo2(), dfunc );
 
@@ -124,7 +124,7 @@ double InterMolecularTorsions::calculateWeight( const unsigned& taskCode, const 
   return sw;
 }
 
-double InterMolecularTorsions::compute( const unsigned& tindex, AtomValuePack& myatoms ) const {
+double InterMolecularTorsions::compute( const unsigned& tindex, multicolvar::AtomValuePack& myatoms ) const {
    Vector v1, v2, dv1, dv2, dconn, conn = getSeparation( myatoms.getPosition(0), myatoms.getPosition(1) );
 
    // Retrieve vectors