61daf70a8f1a49c3ef740291f2b6495df742dabe
Carlo Camilloni <carlo.camilloni@gmail.com>
2017-01-17 15:08:55 +0100

    Here we enable Implicit

--- src/colvar/Implicit.cpp
+++ src/colvar/Implicit.cpp
@@ -21,7 +21,6 @@
 +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++ */
 
 /* This class was originally written by Thomas Loehr */
-#ifdef ENABLEC11 
 
 #include "Colvar.h"
 #include "ActionRegister.h"
@@ -1406,4 +1405,3 @@ PRINT ARG=solv FILE=SOLV
         }
     }
 }
-#endif
