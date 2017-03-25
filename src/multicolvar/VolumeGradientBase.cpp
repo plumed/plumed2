6ecd35aa0bc100d27cef69fe6ae4f1273729dce2
Gareth Tribello <gareth.tribello@gmail.com>
2017-03-25 09:58:17 +0000

    Fixed ccpcheck error

--- src/multicolvar/VolumeGradientBase.cpp
+++ src/multicolvar/VolumeGradientBase.cpp
@@ -19,9 +19,9 @@
    You should have received a copy of the GNU Lesser General Public License
    along with plumed.  If not, see <http://www.gnu.org/licenses/>.
 +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++ */
+#include "VolumeGradientBase.h"
 #include "core/PlumedMain.h"
 #include "core/ActionSet.h"
-#include "VolumeGradientBase.h"
 #include "CatomPack.h"
 
 namespace PLMD {
