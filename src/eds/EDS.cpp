plumed/plumed2

commit e2899f5df937729f36842ca03e09f5032ea21331

EDS: Added ability to have other CVs/Actions as centers

regtest/eds/rt-eds-adaptive/eds_restart.reference
@@ -4,7 +4,6 @@
 #! SET seed  0
 #! SET kbt              2.49433863
                       0                      1                      0                      0                      0      7.483015889999999           0.1496603178
-                      0                      1                      0                      0                      0      7.483015889999999           0.1496603178
                       1                      1                      0                      0                      0      7.483015889999999           0.1496603178
                       2                      1     -1.107575371820062                      0                      0      7.483015889999999           0.1496603178
                       3                      1     -1.107575371820062                      0     -1.107575371820062      7.483015889999999           0.1496603178

regtest/eds/rt-eds-append-restart/eds_restart
@@ -4,7 +4,6 @@
 #! SET seed  0
 #! SET kbt              2.49433863
                       0                      1                      0                      0                      0      7.483015889999999           0.1496603178
-                      0                      1                      0                      0                      0      7.483015889999999           0.1496603178
                       1                      1                      0                      0                      0      7.483015889999999           0.1496603178
                       2                      1     -1.107575371820062                      0                      0      7.483015889999999           0.1496603178
                       3                      1     -1.107575371820062                      0     -1.107575371820062      7.483015889999999           0.1496603178

regtest/eds/rt-eds-append-restart/eds_restart.reference
@@ -4,7 +4,6 @@
 #! SET seed  0
 #! SET kbt              2.49433863
                       0                      1                      0                      0                      0      7.483015889999999           0.1496603178
-                      0                      1                      0                      0                      0      7.483015889999999           0.1496603178
                       1                      1                      0                      0                      0      7.483015889999999           0.1496603178
                       2                      1     -1.107575371820062                      0                      0      7.483015889999999           0.1496603178
                       3                      1     -1.107575371820062                      0     -1.107575371820062      7.483015889999999           0.1496603178
@@ -14,7 +13,6 @@
 #! SET update_period  1
 #! SET seed  0
 #! SET kbt              2.49433863
-                      0                      1     -1.107575371820062                      0     -1.107575371820062      7.483015889999999           0.1496603178
                       0                      1     -1.107575371820062                      0     -1.107575371820062      7.483015889999999           0.1496603178
                       1                      1     -1.107575371820062                      0     -1.107575371820062      7.483015889999999           0.1496603178
                       2                      1     -2.215150743640125                      0     -1.107575371820062      7.483015889999999           0.1496603178

regtest/eds/rt-eds-covar/eds_restart.reference
@@ -4,7 +4,6 @@
 #! SET seed  574
 #! SET kbt              2.49433863
                       0                      1                      0                      0                      0      7.483015889999999           0.1496603178                      1                      0                      0                      0      7.483015889999999           0.1496603178                      1                      0                      0                      0      7.483015889999999           0.1496603178
-                      0                      1                      0                      0                      0      7.483015889999999           0.1496603178                      1                      0                      0                      0      7.483015889999999           0.1496603178                      1                      0                      0                      0      7.483015889999999           0.1496603178
                       1                      1                      0                      0                      0      7.483015889999999           0.1496603178                      1                      0                      0                      0      7.483015889999999           0.1496603178                      1                      0                      0                      0      7.483015889999999           0.1496603178
                       2                      1     -1.107575371820062                      0                      0      7.483015889999999           0.1496603178                      1     -1.107575371820062                      0                      0      7.483015889999999           0.1496603178                      1     -1.107575371820062                      0                      0      7.483015889999999           0.1496603178
                       3                      1     -1.107575371820062                      0     -1.107575371820062      7.483015889999999           0.1496603178                      1     -1.107575371820062                      0     -1.107575371820062      7.483015889999999           0.1496603178                      1     -1.107575371820062                      0     -1.107575371820062      7.483015889999999           0.1496603178

regtest/eds/rt-eds-dynamic-center/eds_restart.reference
@@ -1,0 +1,10 @@
+#! FIELDS time d1_center d1_set d1_target d1_coupling d1_maxrange d1_maxgrad
+#! SET adaptive  1
+#! SET update_period  1
+#! SET seed  0
+#! SET kbt              2.49433863
+                      0      1.097204685252765                      0                      0                      0      7.483015889999999           0.1496603178
+                      1      1.058762371055742                      0                      0                      0      7.483015889999999           0.1496603178
+                      2      1.095818674401286     -1.107575371820062                      0                      0      7.483015889999999           0.1496603178
+                      3      1.162848545384402     -1.107575371820062                      0     -1.107575371820062      7.483015889999999           0.1496603178
+                      4       1.21603261267908     -1.107575371820062                      0     -1.107575371820062      7.483015889999999           0.1496603178

regtest/eds/rt-eds-ramp/eds_restart.reference
@@ -4,7 +4,6 @@
 #! SET seed  0
 #! SET kbt              2.49433863
                       0                      1                    0.5                     -3                    0.5      7.483015889999999           0.1496603178
-                      0                      1                    0.5                     -3                    0.5      7.483015889999999           0.1496603178
                       1                      1                    0.5                     -3                  -1.25      7.483015889999999           0.1496603178
                       2                      1                    0.5                     -3                     -3      7.483015889999999           0.1496603178
                       3                      1                    0.5                     -3                     -3      7.483015889999999           0.1496603178

commit d23169a99218404bf52f99fa6593a35d134f6d2b

EDS: Fixed sign error in EDS implementation introduced in commit e2899f5
The means/centers substraction was reversed, inadvertantly introducing
a sign error. This affected adaptive runs with both covariance matrices and
without. Updated regtests and added additional information
in restarts used to debug this (means/std). Will help reproducibility when
restarting too. Accumulation was also too high for certain biases. It
was made to be 0, instead of 1.

regtest/eds/rt-eds-adaptive/eds_restart.reference
@@ -1,10 +1,10 @@
-#! FIELDS time d1_center d1_set d1_target d1_coupling d1_maxrange d1_maxgrad
+#! FIELDS time d1_center d1_set d1_target d1_coupling d1_maxrange d1_maxgrad d1_mean d1_std
 #! SET adaptive  1
 #! SET update_period  1
 #! SET seed  0
 #! SET kbt    2.4943
-   0.0000   1.0000   0.0000   0.0000   0.0000   7.4830   0.1497
-   1.0000   1.0000   0.0000   0.0000   0.0000   7.4830   0.1497
-   2.0000   1.0000  -1.1076   0.0000   0.0000   7.4830   0.1497
-   3.0000   1.0000  -1.1076   0.0000  -1.1076   7.4830   0.1497
-   4.0000   1.0000  -1.1076   0.0000  -1.1076   7.4830   0.1497
+   0.0000   1.0000   0.0000   0.0000   0.0000   7.4830   0.1497   0.0000   0.0000
+   1.0000   1.0000   0.0000   0.0000   0.0000   7.4830   0.1497   0.0000   0.0000
+   2.0000   1.0000  -7.4830   0.0000   0.0000   7.4830   0.1497   0.0000   0.0000
+   3.0000   1.0000  -7.4830   0.0000  -7.4830   7.4830   0.1497   0.0000   0.0000
+   4.0000   1.0000  -7.4830   0.0000  -7.4830   7.4830   0.1497   0.0000   0.0000

regtest/eds/rt-eds-append-restart/eds_restart
@@ -1,10 +1,10 @@
-#! FIELDS time d1_center d1_set d1_target d1_coupling d1_maxrange d1_maxgrad
+#! FIELDS time d1_center d1_set d1_target d1_coupling d1_maxrange d1_maxgrad d1_mean d1_std
 #! SET adaptive  1
 #! SET update_period  1
 #! SET seed  0
 #! SET kbt    2.4943
-   0.0000   1.0000   0.0000   0.0000   0.0000   7.4830   0.1497
-   1.0000   1.0000   0.0000   0.0000   0.0000   7.4830   0.1497
-   2.0000   1.0000  -1.1076   0.0000   0.0000   7.4830   0.1497
-   3.0000   1.0000  -1.1076   0.0000  -1.1076   7.4830   0.1497
-   4.0000   1.0000  -1.1076   0.0000  -1.1076   7.4830   0.1497
+   0.0000   1.0000   0.0000   0.0000   0.0000   7.4830   0.1497   0.0000   0.0000
+   1.0000   1.0000   0.0000   0.0000   0.0000   7.4830   0.1497   0.0000   0.0000
+   2.0000   1.0000  -1.1076   0.0000   0.0000   7.4830   0.1497   0.0000   0.0000
+   3.0000   1.0000  -1.1076   0.0000  -1.1076   7.4830   0.1497   0.0000   0.0000
+   4.0000   1.0000  -1.1076   0.0000  -1.1076   7.4830   0.1497   0.0000   0.0000

regtest/eds/rt-eds-append-restart/eds_restart.reference
@@ -1,10 +1,10 @@
-#! FIELDS time d1_center d1_set d1_target d1_coupling d1_maxrange d1_maxgrad
+#! FIELDS time d1_center d1_set d1_target d1_coupling d1_maxrange d1_maxgrad d1_mean d1_std
 #! SET adaptive  1
 #! SET update_period  1
 #! SET seed  0
 #! SET kbt  2.49
- 0.00 1.00 -1.11 0.00 -1.11 7.48 0.15
- 1.00 1.00 -1.11 0.00 -1.11 7.48 0.15
- 2.00 1.00 -2.22 0.00 -1.11 7.48 0.15
- 3.00 1.00 -2.22 0.00 -2.22 7.48 0.15
- 4.00 1.00 -2.22 0.00 -2.22 7.48 0.15
+ 0.00 1.00 -1.11 0.00 -1.11 7.48 0.15 0.00 0.00
+ 1.00 1.00 -1.11 0.00 -1.11 7.48 0.15 0.00 0.00
+ 2.00 1.00 -8.59 0.00 -1.11 7.48 0.15 0.00 0.00
+ 3.00 1.00 -8.59 0.00 -8.59 9.35 0.19 0.00 0.00
+ 4.00 1.00 -8.59 0.00 -8.59 9.35 0.19 0.00 0.00

regtest/eds/rt-eds-covar/eds_restart.reference
@@ -1,10 +1,10 @@
-#! FIELDS time d1_center d1_set d1_target d1_coupling d1_maxrange d1_maxgrad d2_center d2_set d2_target d2_coupling d2_maxrange d2_maxgrad d3_center d3_set d3_target d3_coupling d3_maxrange d3_maxgrad
+#! FIELDS time d1_center d1_set d1_target d1_coupling d1_maxrange d1_maxgrad d1_mean d1_std d2_center d2_set d2_target d2_coupling d2_maxrange d2_maxgrad d2_mean d2_std d3_center d3_set d3_target d3_coupling d3_maxrange d3_maxgrad d3_mean d3_std
 #! SET adaptive  1
 #! SET update_period  1
 #! SET seed  574
 #! SET kbt  2.49
- 0.00 1.00 0.00 0.00 0.00 7.48 0.15 1.00 0.00 0.00 0.00 7.48 0.15 1.00 0.00 0.00 0.00 7.48 0.15
- 1.00 1.00 0.00 0.00 0.00 7.48 0.15 1.00 0.00 0.00 0.00 7.48 0.15 1.00 0.00 0.00 0.00 7.48 0.15
- 2.00 1.00 -1.11 0.00 0.00 7.48 0.15 1.00 -1.11 0.00 0.00 7.48 0.15 1.00 -1.11 0.00 0.00 7.48 0.15
- 3.00 1.00 -1.11 0.00 -1.11 7.48 0.15 1.00 -1.11 0.00 -1.11 7.48 0.15 1.00 -1.11 0.00 -1.11 7.48 0.15
- 4.00 1.00 -1.11 0.00 -1.11 7.48 0.15 1.00 -1.11 0.00 -1.11 7.48 0.15 1.00 -1.11 0.00 -1.11 7.48 0.15
+ 0.00 1.00 0.00 0.00 0.00 7.48 0.15 0.00 0.00 1.00 0.00 0.00 0.00 7.48 0.15 0.00 0.00 1.00 0.00 0.00 0.00 7.48 0.15 0.00 0.00
+ 1.00 1.00 0.00 0.00 0.00 7.48 0.15 0.00 0.00 1.00 0.00 0.00 0.00 7.48 0.15 0.00 0.00 1.00 0.00 0.00 0.00 7.48 0.15 0.00 0.00
+ 2.00 1.00 -7.48 0.00 0.00 7.48 0.15 0.00 0.00 1.00 -7.48 0.00 0.00 7.48 0.15 0.00 0.00 1.00 -7.48 0.00 0.00 7.48 0.15 0.00 0.00
+ 3.00 1.00 -7.48 0.00 -7.48 7.48 0.15 0.00 0.00 1.00 -7.48 0.00 -7.48 7.48 0.15 0.00 0.00 1.00 -7.48 0.00 -7.48 7.48 0.15 0.00 0.00
+ 4.00 1.00 -7.48 0.00 -7.48 7.48 0.15 0.00 0.00 1.00 -7.48 0.00 -7.48 7.48 0.15 0.00 0.00 1.00 -7.48 0.00 -7.48 7.48 0.15 0.00 0.00

regtest/eds/rt-eds-dynamic-center/eds_restart.reference
@@ -1,10 +1,10 @@
-#! FIELDS time d1_center d1_set d1_target d1_coupling d1_maxrange d1_maxgrad
+#! FIELDS time d1_center d1_set d1_target d1_coupling d1_maxrange d1_maxgrad d1_mean d1_std
 #! SET adaptive  1
 #! SET update_period  1
 #! SET seed  0
 #! SET kbt  2.49
- 0.00 1.10 0.00 0.00 0.00 7.48 0.15
- 1.00 1.06 0.00 0.00 0.00 7.48 0.15
- 2.00 1.10 -1.11 0.00 0.00 7.48 0.15
- 3.00 1.16 -1.11 0.00 -1.11 7.48 0.15
- 4.00 1.22 -1.11 0.00 -1.11 7.48 0.15
+ 0.00 1.10 0.00 0.00 0.00 7.48 0.15 0.00 0.00
+ 1.00 1.06 0.00 0.00 0.00 7.48 0.15 0.00 0.00
+ 2.00 1.10 -7.48 0.00 0.00 7.48 0.15 0.00 0.00
+ 3.00 1.16 -7.48 0.00 -7.48 7.48 0.15 0.00 0.00
+ 4.00 1.22 -7.48 0.00 -7.48 7.48 0.15 0.00 0.00

regtest/eds/rt-eds-ramp/eds_restart.reference
@@ -1,9 +1,9 @@
-#! FIELDS time d1_center d1_set d1_target d1_coupling d1_maxrange d1_maxgrad
+#! FIELDS time d1_center d1_set d1_target d1_coupling d1_maxrange d1_maxgrad d1_mean d1_std
 #! SET adaptive  0
 #! SET update_period  -2
 #! SET seed  0
 #! SET kbt  2.49
- 0.00 1.00 0.50 -3.00 0.50 7.48 0.15
- 1.00 1.00 0.50 -3.00 -1.25 7.48 0.15
- 2.00 1.00 0.50 -3.00 -3.00 7.48 0.15
- 3.00 1.00 0.50 -3.00 -3.00 7.48 0.15
+ 0.00 1.00 0.50 -3.00 0.50 7.48 0.15 0.00 0.00
+ 1.00 1.00 0.50 -3.00 -1.25 7.48 0.15 0.00 0.00
+ 2.00 1.00 0.50 -3.00 -3.00 7.48 0.15 0.00 0.00
+ 3.00 1.00 0.50 -3.00 -3.00 7.48 0.15 0.00 0.00

regtest/eds/rt-eds-read-restart/eds_restart.input
@@ -1,11 +1,10 @@
-#! FIELDS time d1_center d1_set d1_target d1_coupling d1_maxrange d1_maxgrad
+#! FIELDS time d1_center d1_set d1_target d1_coupling d1_maxrange d1_maxgrad d1_mean d1_std
 #! SET adaptive  1
 #! SET update_period  1
 #! SET seed  0
-#! SET kbt              2.49433863
-                      0                      1                      0                      0                      0      7.483015889999999           0.1496603178
-                      0                      1                      0                      0                      0      7.483015889999999           0.1496603178
-                      1                      1                      0                      0                      0      7.483015889999999           0.1496603178
-                      2                      1     -1.107575371820062                      0                      0      7.483015889999999           0.1496603178
-                      3                      1     -1.107575371820062                      0     -1.107575371820062      7.483015889999999           0.1496603178
-                      4                      1     -1.107575371820062                      0     -1.107575371820062      7.483015889999999           0.1496603178
+#! SET kbt    2.4943
+   0.0000   1.0000   0.0000   0.0000   0.0000   7.4830   0.1497   0.0000   0.0000
+   1.0000   1.0000   0.0000   0.0000   0.0000   7.4830   0.1497   0.0000   0.0000
+   2.0000   1.0000  -1.1076   0.0000   0.0000   7.4830   0.1497   0.0000   0.0000
+   3.0000   1.0000  -1.1076   0.0000  -1.1076   7.4830   0.1497   0.0000   0.0000
+   4.0000   1.0000  -1.1076   0.0000  -1.1076   7.4830   0.1497   0.0000   0.0000

regtest/eds/rt-eds-read-restart/eds_restart.output.reference
@@ -1,6 +1,6 @@
-#! FIELDS time d1_center d1_set d1_target d1_coupling d1_maxrange d1_maxgrad
+#! FIELDS time d1_center d1_set d1_target d1_coupling d1_maxrange d1_maxgrad d1_mean d1_std
 #! SET adaptive  0
 #! SET update_period  0
 #! SET seed  0
 #! SET kbt  2.49
- 0.00 1.00 -0.37 0.00 -0.37 7.48 0.15
+ 0.00 1.00 -0.44 0.00 -0.44 7.48 0.15 0.00 0.00

regtest/eds/rt-eds-read-restart/plumed.dat
@@ -1,2 +1,2 @@
 d1: DISTANCE ATOMS=1,2
-EDS ARG=d1 CENTER=1.0 BIAS_SCALE=1.0 PERIOD=2 LABEL=eds IN_RESTART=eds_restart.input OUT_RESTART=eds_restart.output TEMP=300.0 MEAN FREEZE RESTART_FMT=%4.2f
+EDS ARG=d1 CENTER=1.0 BIAS_SCALE=1.0 LABEL=eds IN_RESTART=eds_restart.input OUT_RESTART=eds_restart.output TEMP=300.0 MEAN FREEZE RESTART_FMT=%4.2f

src/eds/EDS.cpp
@@ -51,17 +51,23 @@ The addition to the potential is of the form
   \sum_i \frac{\alpha_i}{s_i} x_i
 \f]
 
-where for CV \f$x_i\f$, a coupling \f${\alpha}_i\f$ is determined
+where for CV \f$x_i\f$, a coupling constant \f${\alpha}_i\f$ is determined
 adaptively or set by the user to match a target value for
 \f$x_i\f$. \f$s_i\f$ is a scale parameter, which by default is set to
 the target value. It may also be set separately.
 
 \warning
-It is not possible to set the target value of the observable to zero with the default value of \f$s_i\f$ as this will cause a divide-by-zero error. Instead, set \f$s_i=1\f$ or modify the CV so the desired target value is no longer zero.
+It is not possible to set the target value of the observable
+to zero with the default value of \f$s_i\f$ as this will cause a
+divide-by-zero error. Instead, set \f$s_i=1\f$ or modify the CV so the
+desired target value is no longer zero.
 
 \par Examples
 
-The following input for a harmonic oscillator of two beads will adaptively find a linear bias to change the mean and variance to the target values. The PRINT line shows how to access the value of the coupling constants.
+The following input for a harmonic oscillator of two beads will
+adaptively find a linear bias to change the mean and variance to the
+target values. The PRINT line shows how to access the value of the
+coupling constants.
 
 \plumedfile
 dist: DISTANCE ATOMS=1,2
@@ -73,7 +79,7 @@ eds: EDS ARG=dist,dist2 CENTER=2.0,1.0 PERIOD=50000 TEMP=1.0
 PRINT ARG=dist,dist2,eds.dist_coupling,eds.dist2_coupling,eds.bias,eds.force2 FILE=colvars.dat STRIDE=100
 \endplumedfile
 
-Rather than trying to find the coupling constants adaptively, can ramp up to a constant value.
+Rather than trying to find the coupling constants adaptively, one can ramp up to a constant value.
 \plumedfile
 #ramp couplings from 0,0 to -1,1 over 50000 steps
 eds: EDS ARG=dist,dist2 CENTER=2.0,1.0 FIXED=-1,1 RAMP PERIOD=50000 TEMP=1.0
@@ -82,21 +88,29 @@ eds: EDS ARG=dist,dist2 CENTER=2.0,1.0 FIXED=-1,1 RAMP PERIOD=50000 TEMP=1.0
 eds: EDS ARG=dist,dist2 CENTER=2.0,1.0 FIXED=-1,1 INIT=-0.5,0.5 RAMP PERIOD=50000 TEMP=1.0
 \endplumedfile
 
-A restart file can be added to dump information needed to restart/continue simulation using these parameters every STRIDE.
+A restart file can be added to dump information needed to restart/continue simulation using these parameters every PERIOD.
 \plumedfile
 #add the option to write to a restart file
 eds: EDS ARG=dist,dist2 CENTER=2.0,1.0 PERIOD=50000 TEMP=1.0 OUT_RESTART=restart.dat
+\endplumedfile
 
-#add the option to read in a previous restart file. Adding RESTART flag makes output append
-eds: EDS ARG=dist,dist2 CENTER=2.0,1.0 PERIOD=50000 TEMP=1.0 IN_RESTART=restart.dat RESTART=yes
+Read in a previous restart file. Adding RESTART flag makes output append
+\plumedfile
+eds: EDS ARG=dist,dist2 CENTER=2.0,1.0 PERIOD=50000 TEMP=1.0 IN_RESTART=restart.dat RESTART
+\endplumedfile
 
-#add the option to read in a previous restart file and freeze the bias at the final level from the previous simulation
-eds: EDS ARG=dist,dist2 CENTER=2.0,1.0 PERIOD=50000 TEMP=1.0 IN_RESTART=restart.dat FREEZE
+Read in a previous restart file and freeze the bias at the final level from the previous simulation
+\plumedfile
+eds: EDS ARG=dist,dist2 CENTER=2.0,1.0 TEMP=1.0 IN_RESTART=restart.dat FREEZE
+\endplumedfile
 
-#add the option to read in a previous restart file and freeze the bias at the mean from the previous simulation
-eds: EDS ARG=dist,dist2 CENTER=2.0,1.0 PERIOD=50000 TEMP=1.0 IN_RESTART=restart.dat FREEZE MEAN
+Read in a previous restart file and freeze the bias at the mean from the previous simulation
+\plumedfile
+eds: EDS ARG=dist,dist2 CENTER=2.0,1.0 TEMP=1.0 IN_RESTART=restart.dat FREEZE MEAN
+\endplumedfile
 
-#add the option to read in a previous restart file and continue the bias, but use the mean from the previous run as the starting point
+Read in a previous restart file and continue the bias, but use the mean from the previous run as the starting point
+\plumedfile
 eds: EDS ARG=dist,dist2 CENTER=2.0,1.0 PERIOD=50000 TEMP=1.0 IN_RESTART=restart.dat MEAN
 \endplumedfile
 
@@ -180,12 +194,12 @@ void EDS::registerKeywords(Keywords& keys) {
   keys.add("optional","CENTER_ARG","The desired centers (equilibrium values) which will be sought during the adaptive linear biasing. "
            "CENTER_ARG is for calculated centers, e.g. from a CV or analysis. ");
 
-  keys.add("compulsory","PERIOD","Steps over which to adjust bias");
+  keys.add("optional","PERIOD","Steps over which to adjust bias for adaptive or ramping");
 
   keys.add("compulsory","RANGE","3.0","The largest magnitude of the force constant which one expects (in kBT) for each CV based");
   keys.add("compulsory","SEED","0","Seed for random order of changing bias");
-  keys.add("compulsory","INIT","0","Starting value for coupling coefficients");
-  keys.add("compulsory","FIXED","0","Fixed target values for bias factors (not adaptive)");
+  keys.add("compulsory","INIT","0","Starting value for coupling constant");
+  keys.add("compulsory","FIXED","0","Fixed target values for coupling constant. Non-adaptive.");
   keys.add("optional","BIAS_SCALE","A divisor to set the units of the bias. "
            "If not set, this will be the experimental value by default (as is done in White and Voth 2014).");
   keys.add("optional","TEMP","The system temperature. If not provided will be taken from MD code (if available)");
@@ -202,7 +216,7 @@ void EDS::registerKeywords(Keywords& keys) {
 
   keys.addFlag("RAMP",false,"Slowly increase bias constant to a fixed value");
   keys.addFlag("COVAR",false,"Utilize the covariance matrix when updating the bias. Default Off, but may be enabled due to other options");
-  keys.addFlag("FREEZE",false,"Fix bias at current level (only used for restarting). Can also set PERIOD=0 if not using RESTART.");
+  keys.addFlag("FREEZE",false,"Fix bias at current level (only used for restarting).");
   keys.addFlag("MEAN",false,"Instead of using final bias level from restart, use average. Can only be used in conjunction with FREEZE");
 
   keys.use("RESTART");
@@ -221,7 +235,7 @@ EDS::EDS(const ActionOptions&ao):
   max_coupling_range_(ncvs_,3.0),
   max_coupling_grad_(ncvs_,0.0),
   coupling_rate_(ncvs_,1.0),
-  coupling_accum_(ncvs_,1.0),
+  coupling_accum_(ncvs_,0.0),
   means_(ncvs_,0.0),
   step_size_(ncvs_,0.0),
   out_coupling_(ncvs_,NULL),
@@ -452,15 +466,6 @@ void EDS::readInRestart(const bool b_mean) {
 
   in_restart_.open(in_restart_name_);
 
-  //some sample code to get the field names:
-  /*
-    std::vector<std::string> fields;
-    in_restart_.scanFieldList(fields);
-    log.printf("field");
-    for(unsigned int i = 0;i<fields.size();i++) log.printf(" %s",fields[i].c_str());
-    log.printf("\n");
-  */
-
   if(in_restart_.FieldExist("kbt")) {
     in_restart_.scanField("kbt",kbt_);
   } else { error("No field 'kbt' in restart file"); }
@@ -485,7 +490,7 @@ void EDS::readInRestart(const bool b_mean) {
     rand_.setSeed(seed_);
   }
 
-  double time;
+  double time, tmp;
   std::vector<double> avg_bias = std::vector<double>(center_.size());
   unsigned int N = 0;
   std::string cv_name;
@@ -500,6 +505,9 @@ void EDS::readInRestart(const bool b_mean) {
       in_restart_.scanField(cv_name + "_coupling",current_coupling_[i]);
       in_restart_.scanField(cv_name + "_maxrange",max_coupling_range_[i]);
       in_restart_.scanField(cv_name + "_maxgrad",max_coupling_grad_[i]);
+      in_restart_.scanField(cv_name + "_mean",means_[i]);
+      //unused due to difference between covar/nocovar
+      in_restart_.scanField(cv_name + "_std",tmp);
 
       avg_bias[i] += current_coupling_[i];
     }
@@ -571,6 +579,12 @@ void EDS::writeOutRestart() {
     out_restart_.printField(cv_name + "_coupling",current_coupling_[i]);
     out_restart_.printField(cv_name + "_maxrange",max_coupling_range_[i]);
     out_restart_.printField(cv_name + "_maxgrad",max_coupling_grad_[i]);
+    out_restart_.printField(cv_name + "_mean",means_[i]);
+    if(!b_covar_)
+      out_restart_.printField(cv_name + "_std",ssds_[i] / (fmax(1, update_calls_ - 1)));
+    else
+      out_restart_.printField(cv_name + "_std",covar_(i,i) / (fmax(1, update_calls_ - 1)));
+
   }
   out_restart_.printField();
 }
@@ -660,6 +674,8 @@ void EDS::calculate() {
   for(unsigned int i = 0; i<ncvs_; ++i) {
     out_coupling_[i]->set(current_coupling_[i]);
   }
+
+
 }
 
 void EDS::apply_bias() {
@@ -686,7 +702,7 @@ void EDS::update_statistics()  {
   //Welford, West, and Hanso online variance method
   for(unsigned int i = 0; i < ncvs_; ++i)  {
     deltas[i] = difference(i,means_[i],getArgument(i));
-    means_[i] += deltas[i]/update_calls_;
+    means_[i] += deltas[i]/fmax(1,update_calls_);
     if(!b_covar_)
       ssds_[i] += deltas[i]*difference(i,means_[i],getArgument(i));
   }
@@ -721,7 +737,7 @@ void EDS::calc_covar_step_size() {
   for(unsigned int i = 0; i< ncvs_; ++i) {
     tmp = 0;
     for(unsigned int j = 0; j < ncvs_; ++j)
-      tmp += difference(i, means_[i], center_[i]) * covar_(i,j);
+      tmp += difference(i, center_[i], means_[i]) * covar_(i,j);
     step_size_[i] = 2 * tmp / kbt_ / scale_[i] * update_calls_ / (update_calls_ - 1);
   }
 
@@ -730,7 +746,7 @@ void EDS::calc_covar_step_size() {
 void EDS::calc_ssd_step_size() {
   double tmp;
   for(unsigned int i = 0; i< ncvs_; ++i) {
-    tmp = 2. * difference(i, means_[i], center_[i]) * ssds_[i] / (update_calls_ - 1);
+    tmp = 2. * difference(i, center_[i], means_[i]) * ssds_[i] / (update_calls_ - 1);
     step_size_[i] = tmp / kbt_/scale_[i];
   }
 }

src/multicolvar/CoordinationNumbers.cpp
@@ -104,10 +104,10 @@ void CoordinationNumbers::registerKeywords( Keywords& keys ) {
   keys.add("compulsory","D_0","0.0","The d_0 parameter of the switching function");
   keys.add("compulsory","R_0","The r_0 parameter of the switching function");
   keys.add("optional","R_POWER","Multiply the coordination number function by a power of r, "
-	   "as done in White and Voth (see note above, default: no)");
+           "as done in White and Voth (see note above, default: no)");
   keys.add("optional","SWITCH","This keyword is used if you want to employ an alternative to the continuous swiching function defined above. "
-	   "The following provides information on the \\ref switchingfunction that are available. "
-	   "When this keyword is present you no longer need the NN, MM, D_0 and R_0 keywords.");
+           "The following provides information on the \\ref switchingfunction that are available. "
+           "When this keyword is present you no longer need the NN, MM, D_0 and R_0 keywords.");
   // Use actionWithDistributionKeywords
   keys.use("MEAN"); keys.use("MORE_THAN"); keys.use("LESS_THAN"); keys.use("MAX");
   keys.use("MIN"); keys.use("BETWEEN"); keys.use("HISTOGRAM"); keys.use("MOMENTS");
@@ -144,7 +144,7 @@ CoordinationNumbers::CoordinationNumbers(const ActionOptions&ao):
     log.printf("  Multiplying switching function by r^%d\n", r_power);
     double offset = switchingFunction.calculate(rcut*0.9999, rcut2) * pow(rcut*0.9999, r_power);
     log.printf("  You will have a discontinuous jump of %f to 0 near the cutoff of your switching function. "
-	       "Consider setting D_MAX or reducing R_POWER if this is large\n", offset);
+               "Consider setting D_MAX or reducing R_POWER if this is large\n", offset);
   }
 
   // Set the link cell cutoff
@@ -161,18 +161,18 @@ double CoordinationNumbers::compute( const unsigned& tindex, AtomValuePack& myat
   for(unsigned i=1; i<myatoms.getNumberOfAtoms(); ++i) {
     Vector& distance=myatoms.getPosition(i);
     if ( (d2=distance[0]*distance[0])<rcut2 &&
-	 (d2+=distance[1]*distance[1])<rcut2 &&
-	 (d2+=distance[2]*distance[2])<rcut2) {
+         (d2+=distance[1]*distance[1])<rcut2 &&
+         (d2+=distance[2]*distance[2])<rcut2) {
 
       sw = switchingFunction.calculateSqr( d2, dfunc );
       if(r_power > 0) {
-	d = sqrt(d2); raised = pow( d, r_power - 1 );
-	accumulateSymmetryFunction( 1, i, sw * raised * d,
-				  (dfunc * d * raised + sw * r_power) * distance,
-				  (-dfunc * d * raised - sw * r_power) * Tensor(distance, distance),
-				  myatoms );
+        d = sqrt(d2); raised = pow( d, r_power - 1 );
+        accumulateSymmetryFunction( 1, i, sw * raised * d,
+                                    (dfunc * d * raised + sw * r_power) * distance,
+                                    (-dfunc * d * raised - sw * r_power) * Tensor(distance, distance),
+                                    myatoms );
       } else {
-	accumulateSymmetryFunction( 1, i, sw, (dfunc)*distance, (-dfunc)*Tensor(distance,distance), myatoms );
+        accumulateSymmetryFunction( 1, i, sw, (dfunc)*distance, (-dfunc)*Tensor(distance,distance), myatoms );
       }
     }
   }