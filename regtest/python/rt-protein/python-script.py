8ba3eb0449f47ee7392ad9f7837a75b6633375ea
Michele Ceriotti <michele.ceriotti@gmail.com>
2017-08-28 09:26:14 +0200

    Fixed crazy MAC input encoding

--- regtest/python/rt-protein/python-script.py
+++ regtest/python/rt-protein/python-script.py
@@ -1,40 +1,40 @@
 # The numbers calculated by the following python script are compared with those output from this PLUMED input
 #
-# MOLINFO STRUCTURE=template.pdb
-# t1: TORSION ATOMS=@phi-2
-# t2: TORSION ATOMS=@psi-2
-# t3: TORSION ATOMS=@phi-3
-# t4: TORSION ATOMS=@psi-3
-# t5: TORSION ATOMS=@phi-4
-# t6: TORSION ATOMS=@psi-4
-# t7: TORSION ATOMS=@phi-5
-# t8: TORSION ATOMS=@psi-5
-# t9: TORSION ATOMS=@phi-6
-# t10: TORSION ATOMS=@psi-6
-# t11: TORSION ATOMS=@phi-7
-# t12: TORSION ATOMS=@psi-7
-# t13: TORSION ATOMS=@phi-8
-# t14: TORSION ATOMS=@psi-8
-# t15: TORSION ATOMS=@phi-9
-# t16: TORSION ATOMS=@psi-9
-# t17: TORSION ATOMS=@phi-10
-# t18: TORSION ATOMS=@psi-10
-# t19: TORSION ATOMS=@phi-11
-# t20: TORSION ATOMS=@psi-11
-# t21: TORSION ATOMS=@phi-12
-# t22: TORSION ATOMS=@psi-12
-# t23: TORSION ATOMS=@phi-13
-# t24: TORSION ATOMS=@psi-13
-# t25: TORSION ATOMS=@phi-14
-# t26: TORSION ATOMS=@psi-14
-# t27: TORSION ATOMS=@phi-15
-# t28: TORSION ATOMS=@psi-15
-# t29: TORSION ATOMS=@phi-16
-# t30: TORSION ATOMS=@psi-16
-# t31: TORSION ATOMS=@phi-17
-# t32: TORSION ATOMS=@psi-17
-#    
-# PRINT ARG=t1,t2,t3,t4,t5,t6,t7,t8,t9,t10,t11,t12,t13,t14,t15,t16,t17,t18,t19,t20,t21,t22,t23,t24,t25,t26,t27,t28,t29,t30,t31,t32 FILE=colvar.ref FMT=%8.4f
+# MOLINFO STRUCTURE=template.pdb
+# t1: TORSION ATOMS=@phi-2
+# t2: TORSION ATOMS=@psi-2
+# t3: TORSION ATOMS=@phi-3
+# t4: TORSION ATOMS=@psi-3
+# t5: TORSION ATOMS=@phi-4
+# t6: TORSION ATOMS=@psi-4
+# t7: TORSION ATOMS=@phi-5
+# t8: TORSION ATOMS=@psi-5
+# t9: TORSION ATOMS=@phi-6
+# t10: TORSION ATOMS=@psi-6
+# t11: TORSION ATOMS=@phi-7
+# t12: TORSION ATOMS=@psi-7
+# t13: TORSION ATOMS=@phi-8
+# t14: TORSION ATOMS=@psi-8
+# t15: TORSION ATOMS=@phi-9
+# t16: TORSION ATOMS=@psi-9
+# t17: TORSION ATOMS=@phi-10
+# t18: TORSION ATOMS=@psi-10
+# t19: TORSION ATOMS=@phi-11
+# t20: TORSION ATOMS=@psi-11
+# t21: TORSION ATOMS=@phi-12
+# t22: TORSION ATOMS=@psi-12
+# t23: TORSION ATOMS=@phi-13
+# t24: TORSION ATOMS=@psi-13
+# t25: TORSION ATOMS=@phi-14
+# t26: TORSION ATOMS=@psi-14
+# t27: TORSION ATOMS=@phi-15
+# t28: TORSION ATOMS=@psi-15
+# t29: TORSION ATOMS=@phi-16
+# t30: TORSION ATOMS=@psi-16
+# t31: TORSION ATOMS=@phi-17
+# t32: TORSION ATOMS=@psi-17
+#    
+# PRINT ARG=t1,t2,t3,t4,t5,t6,t7,t8,t9,t10,t11,t12,t13,t14,t15,t16,t17,t18,t19,t20,t21,t22,t23,t24,t25,t26,t27,t28,t29,t30,t31,t32 FILE=colvar.ref FMT=%8.4f
 
 import numpy as np
 import plumed
@@ -121,14 +121,14 @@ t28 = create_plumed_var( p, "t28", "TORSION ATOMS=@psi-15" )
 t29 = create_plumed_var( p, "t29", "TORSION ATOMS=@phi-16" )
 t30 = create_plumed_var( p, "t30", "TORSION ATOMS=@psi-16" )
 t31 = create_plumed_var( p, "t31", "TORSION ATOMS=@phi-17" )
-t32 = create_plumed_var( p, "t32", "TORSION ATOMS=@psi-17" )
+t32 = create_plumed_var( p, "t32", "TORSION TOMS=@psi-17" )
  
 # Read in the correct answers that were calculated directly using PLUMED
 correct_torsions = np.loadtxt("colvar.ref")
 # Open an output file
 of = open("logfile", "w+")
 
-# Now analyze the trajectory
+# Now analyze the trajectory
 for step in range(0,num_frames) :
     of.write("RUNNING ANALYSIS FOR STEP " + str(step) + "\n" )
     p.cmd("setStep",step )
