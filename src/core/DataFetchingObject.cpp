63720cb8cb91c65be197aae50f4eeb0df11003dc
Gareth Tribello <gareth.tribello@gmail.com>
2017-09-05 08:03:03 +0200

    Added installation of Cython to travis.yml

--- src/core/DataFetchingObject.cpp
+++ src/core/DataFetchingObject.cpp
@@ -19,10 +19,10 @@
    You should have received a copy of the GNU Lesser General Public License
    along with plumed.  If not, see <http://www.gnu.org/licenses/>.
 +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++ */
+#include "DataFetchingObject.h"
 #include "PlumedMain.h"
 #include "ActionSet.h"
 #include "Action.h"
-#include "DataFetchingObject.h"
 #include "ActionWithValue.h"
 #include "Value.h"
 
