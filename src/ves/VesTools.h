e046b2a23661420832f4eb5fd550ce1a7ae158ee
Omar Valsson <omar.valsson@gmail.com>
2017-10-02 10:19:10 +0200

    fixes to copyright/headers

--- src/ves/VesTools.h
+++ src/ves/VesTools.h
@@ -1,23 +1,23 @@
 /* +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
-   Copyright (c) 2016-2017 The ves-code team
+   Copyright (c) 2016-2017 The VES code team
    (see the PEOPLE-VES file at the root of this folder for a list of names)
 
    See http://www.ves-code.org for more information.
 
-   This file is part of ves-code module.
+   This file is part of VES code module.
 
-   The ves-code module is free software: you can redistribute it and/or modify
+   The VES code module is free software: you can redistribute it and/or modify
    it under the terms of the GNU Lesser General Public License as published by
    the Free Software Foundation, either version 3 of the License, or
    (at your option) any later version.
 
-   The ves-code module is distributed in the hope that it will be useful,
+   The VES code module is distributed in the hope that it will be useful,
    but WITHOUT ANY WARRANTY; without even the implied warranty of
    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
    GNU Lesser General Public License for more details.
 
    You should have received a copy of the GNU Lesser General Public License
-   along with the ves-code module.  If not, see <http://www.gnu.org/licenses/>.
+   along with the VES code module.  If not, see <http://www.gnu.org/licenses/>.
 +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++ */
 #ifndef __PLUMED_ves_VesTools_h
 #define __PLUMED_ves_VesTools_h
