plumed/plumed2

commit 3d93bfd29c5227ccd7075f7a0547c6d185444f14

Removed constructor
SwitchingFunction now contains only simple data and is
better initialized with c++11 syntax

src/tools/SwitchingFunction.h
@@ -41,29 +41,35 @@ class Keywords;
 /// performances.
 class SwitchingFunction {
 /// This is to check that switching function has been initialized
-  bool init;
+  bool init=false;
 /// Type of function
-  enum {rational,exponential,gaussian,smap,cubic,tanh,matheval,leptontype,nativeq} type;
+  enum {rational,exponential,gaussian,smap,cubic,tanh,matheval,leptontype,nativeq} type=rational;
 /// Inverse of scaling length.
 /// We store the inverse to avoid a division
-  double invr0;
+  double invr0=0.0;
 /// Minimum distance (before this, function is one)
-  double d0;
+  double d0=0.0;
 /// Maximum distance (after this, function is zero)
-  double dmax;
+  double dmax=0.0;
 /// Exponents for rational function
-  int nn,mm;
+  int nn=6;
+  int mm=0;
 /// Parameters for smap function
-  int a,b;
-  double c,d;
+  int a=0;
+  int b=b;
+  double c=0.0;
+  double d=0.0;
 // nativeq
-  double lambda, beta, ref;
+  double lambda=0.0;
+  double beta=0.0;
+  double ref=0.0;
 /// Square of invr0, useful in calculateSqr()
-  double invr0_2;
+  double invr0_2=0.0;
 /// Square of dmax, useful in calculateSqr()
-  double dmax_2;
+  double dmax_2=0.0;
 /// Parameters for stretching the function to zero at d_max
-  double stretch,shift;
+  double stretch=1.0;
+  double shift=0.0;
 /// Low-level tool to compute rational functions.
 /// It is separated since it is called both by calculate() and calculateSqr()
   double do_rational(double rdist,double&dfunc,int nn,int mm)const;
@@ -77,8 +83,6 @@ class SwitchingFunction {
   std::vector<lepton::CompiledExpression> expression_deriv;
 public:
   static void registerKeywords( Keywords& keys );
-/// Constructor
-  SwitchingFunction();
 /// Set a "rational" switching function.
 /// Notice that a d_max is set automatically to a value such that
 /// f(d_max)=0.00001.

src/tools/SwitchingFunction.cpp
@@ -420,28 +420,6 @@ double SwitchingFunction::calculate(double distance,double&dfunc)const {
   return result;
 }
 
-SwitchingFunction::SwitchingFunction():
-  init(false),
-  type(rational),
-  invr0(0.0),
-  d0(0.0),
-  dmax(0.0),
-  nn(6),
-  mm(0),
-  a(0.0),
-  b(0.0),
-  c(0.0),
-  d(0.0),
-  lambda(0.0),
-  beta(0.0),
-  ref(0.0),
-  invr0_2(0.0),
-  dmax_2(0.0),
-  stretch(1.0),
-  shift(0.0)
-{
-}
-
 void SwitchingFunction::set(int nn,int mm,double r0,double d0) {
   init=true;
   type=rational;

commit d7c341db4c32e5142f53e55da367da82a2c78161

Fixed warning
Harmless error introduced in 3d93bfd29c

src/tools/SwitchingFunction.h
@@ -56,7 +56,7 @@ class SwitchingFunction {
   int mm=0;
 /// Parameters for smap function
   int a=0;
-  int b=b;
+  int b=0;
   double c=0.0;
   double d=0.0;
 // nativeq