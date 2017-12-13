cac65610e5670145b851694eab4597e764324556
carlocamilloni <carlo.camilloni@gmail.com>
2017-12-13 11:27:04 +0100

    astyle

--- src/isdb/FretEfficiency.cpp
+++ src/isdb/FretEfficiency.cpp
@@ -119,7 +119,7 @@ FretEfficiency::FretEfficiency(const ActionOptions&ao):
 
   if(pbc) log.printf("  using periodic boundary conditions\n");
   else    log.printf("  without periodic boundary conditions\n");
-  
+
   log << " Bibliography" << plumed.cite("Bonomi, Camilloni, Bioinformatics, 33, 3999 (2017)") << "\n";
 
   addValueWithDerivatives();
