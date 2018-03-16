2318eedcbab0cc80e9e0269b93e67b23d80ac22a
Glen Hocky <glen842@midway>
2018-03-16 09:59:25 -0500

    eds module - update one of the regtests to use lower precision restart output to make regtest more portable

--- src/eds/EDS.cpp
+++ src/eds/EDS.cpp
@@ -1,5 +1,5 @@
 /* +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
-Copyright (c) 2018 of Glen Hocky and Andrew White
+Copyright (c) 2017 of Glen Hocky and Andrew White
 
 The eds module is free software: you can redistribute it and/or modify
 it under the terms of the GNU Lesser General Public License as published by
