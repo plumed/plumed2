e1b35bd21d00caf784f8d9262cf2b217339f648b
carlocamilloni <carlo.camilloni@gmail.com>
2018-04-06 13:38:07 +0200

    MaxCal - spline curve - time scaling - general reorganisation

--- src/isdb/MaxCal.cpp
+++ src/isdb/MaxCal.cpp
@@ -1,5 +1,5 @@
 /* +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
-   Copyright (c) 2011-2016 The plumed team
+   Copyright (c) 2017,2018 The plumed team
    (see the PEOPLE file at the root of the distribution for a list of names)
 
    See http://www.plumed.org for more information.
@@ -50,7 +50,7 @@ public:
 private:
   vector<double> time;
   vector< vector<double> > var;
-  double   deltat;
+  vector< vector<double> > dvar;
   double   mult;
   bool     master;
   unsigned replica_;
@@ -58,9 +58,13 @@ private:
   unsigned optsigmamean_stride_;
   vector<double> sigma_mean2_;
   vector< vector<double> > sigma_mean2_last_;
+  vector<Value*> x0comp;
+  vector<Value*> kcomp;
+  vector<Value*> mcomp;
 
   void get_sigma_mean(const double fact, const vector<double> &mean);
   void replica_averaging(const double fact, vector<double> &mean);
+  double getSpline(const unsigned iarg);
 };
 
 PLUMED_REGISTER_ACTION(Caliber,"CALIBER")
@@ -68,8 +72,10 @@ PLUMED_REGISTER_ACTION(Caliber,"CALIBER")
 void Caliber::registerKeywords( Keywords& keys ) {
   Bias::registerKeywords(keys);
   keys.use("ARG");
+  keys.addFlag("NOENSEMBLE",false,"don't perform any replica-averaging");
   keys.add("compulsory","FILE","the name of the file containing the time-resolved values");
   keys.add("compulsory","KAPPA","a force constant, this can be use to scale a constant estimanted on-the-fly using AVERAGING");
+  keys.add("compulsory","TSCALE","1.0","Apply a time scaling on the experimental time scale");
   keys.add("optional","AVERAGING", "Stride for calculation of the optimum kappa, if 0 only KAPPA is used.");
   keys.addOutputComponent("x0","default","the instantaneous value of the center of the potential");
   keys.addOutputComponent("mean","default","the current average value of the calculated observable");
@@ -88,6 +94,12 @@ Caliber::Caliber(const ActionOptions&ao):
   unsigned averaging=0;
   parse("AVERAGING", averaging);
   if(averaging>0) optsigmamean_stride_ = averaging;
+  double tscale=1.0;
+  parse("TSCALE", tscale);
+  if(tscale<=0.) error("The time scale factor must be greater than 0.");
+
+  bool noensemble = false;
+  parseFlag("NOENSEMBLE", noensemble);
 
   checkRead();
 
@@ -96,12 +108,14 @@ Caliber::Caliber(const ActionOptions&ao):
   if(master) {
     nrep_    = multi_sim_comm.Get_size();
     replica_ = multi_sim_comm.Get_rank();
+    if(noensemble) nrep_ = 1;
   } else {
     nrep_    = 0;
     replica_ = 0;
   }
   comm.Sum(&nrep_,1);
   comm.Sum(&replica_,1);
+
   const unsigned narg = getNumberOfArguments();
   sigma_mean2_.resize(narg,1);
   sigma_mean2_last_.resize(narg);
@@ -110,10 +124,11 @@ Caliber::Caliber(const ActionOptions&ao):
   log.printf("  Time resolved data from file %s\n",filename.c_str());
   std::ifstream varfile(filename.c_str());
   var.resize(narg);
+  dvar.resize(narg);
   while (!varfile.eof()) {
     double tempT, tempVar;
     varfile >> tempT;
-    time.push_back(tempT);
+    time.push_back(tempT/tscale);
     for(unsigned i=0; i<narg; i++) {
       varfile >> tempVar;
       var[i].push_back(tempVar);
@@ -121,13 +136,20 @@ Caliber::Caliber(const ActionOptions&ao):
   }
   varfile.close();
 
-  deltat = time[1] - time[0];
+  const double deltat = time[1] - time[0];
+  for(unsigned i=0; i<narg; i++) {
+    for(unsigned j=0; j<var[i].size(); j++) {
+      if(j==0) dvar[i].push_back((var[i][j+1] - var[i][j])/(deltat));
+      else if(j==var[i].size()-1) dvar[i].push_back((var[i][j] - var[i][j-1])/(deltat));
+      else dvar[i].push_back((var[i][j+1] - var[i][j-1])/(2.*deltat));
+    }
+  }
 
   for(unsigned i=0; i<narg; i++) {
     std::string num; Tools::convert(i,num);
-    addComponent("x0_"+num); componentIsNotPeriodic("x0_"+num);
-    addComponent("kappa_"+num); componentIsNotPeriodic("kappa_"+num);
-    addComponent("mean_"+num); componentIsNotPeriodic("mean_"+num);
+    addComponent("x0_"+num); componentIsNotPeriodic("x0_"+num); x0comp.push_back(getPntrToComponent("x0_"+num));
+    addComponent("kappa_"+num); componentIsNotPeriodic("kappa_"+num); kcomp.push_back(getPntrToComponent("kappa_"+num));
+    addComponent("mean_"+num); componentIsNotPeriodic("mean_"+num); mcomp.push_back(getPntrToComponent("mean_"+num));
   }
 }
 
@@ -163,13 +185,40 @@ void Caliber::replica_averaging(const double fact, vector<double> &mean)
   comm.Sum(&mean[0], narg);
 }
 
+double Caliber::getSpline(const unsigned iarg)
+{
+  const double deltat = time[1] - time[0];
+  const int tindex = static_cast<int>(getTime()/deltat);
+
+  unsigned start, end;
+  start=tindex;
+  if(tindex+1<var[iarg].size()) end=tindex+2;
+  else end=var[iarg].size();
+
+  double value=0;
+  for(unsigned ipoint=start; ipoint<end; ++ipoint) {
+    double grid=var[iarg][ipoint];
+    double dder=dvar[iarg][ipoint];
+    double yy=0.;
+    if(fabs(grid)>0.0000001) yy=-dder/grid;
+
+    int x0=1;
+    if(ipoint==tindex) x0=0;
+
+    double X=fabs((getTime()-time[tindex])/deltat-(double)x0);
+    double X2=X*X;
+    double X3=X2*X;
+    double C=(1.0-3.0*X2+2.0*X3) - (x0?-1.0:1.0)*yy*(X-2.0*X2+X3)*deltat;
+
+
+    value+=grid*C;
+  }
+  return value;
+}
 
 void Caliber::calculate()
 {
-  const long int now = getStep();
-  const double dnow = static_cast<double>(now);
   const unsigned narg = getNumberOfArguments();
-  const int tindex = now/static_cast<int>(deltat);
   const double dnrep = static_cast<double>(nrep_);
   double fact = 1.0/dnrep;
 
@@ -180,17 +229,15 @@ void Caliber::calculate()
 
   double ene=0;
   for(unsigned i=0; i<narg; ++i) {
-    double x0 = var[i][tindex]   * (1. - (dnow - time[tindex])/(time[tindex+1] - time[tindex]) ) + \
-                var[i][tindex+1] * (1. - (time[tindex+1] - dnow)/(time[tindex+1] - time[tindex]) );
-    double kappa = mult*dnrep/sigma_mean2_[i];
+    const double x0 = getSpline(i);
+    const double kappa = mult*dnrep/sigma_mean2_[i];
     const double cv=difference(i,x0,mean[i]);
     const double f=-kappa*cv*dmean_x[i];
     setOutputForce(i,f);
     ene+=0.5*kappa*cv*cv;
-    std::string num; Tools::convert(i,num);
-    getPntrToComponent("kappa_"+num)->set(kappa);
-    getPntrToComponent("x0_"+num)->set(x0);
-    getPntrToComponent("mean_"+num)->set(mean[i]);
+    x0comp[i]->set(x0);
+    kcomp[i]->set(kappa);
+    mcomp[i]->set(mean[i]);
   }
 
   setBias(ene);
