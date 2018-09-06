09db089e13fca925c9dc33a5198513d8e9a87a7d
Giovanni Bussi <giovanni.bussi@gmail.com>
2018-09-06 16:04:06 +0200

    Regtests

--- regtest/basic/rt-make-wrappers/main.cpp
+++ regtest/basic/rt-make-wrappers/main.cpp
@@ -2,6 +2,7 @@
 #include <fstream>
 #include <iostream>
 #include <vector>
+#include <memory>
 
 extern "C"{
   void plumed_f_installed(int*i);
@@ -11,8 +12,11 @@ extern "C"{
   void plumed_f_gfinalize();
   void plumed_f_global(char*c);
   void plumed_f_create(char*c);
+  void plumed_f_create_dlopen(char*c,char*p);
+  void plumed_f_create_reference(char*p,char*r);
   void plumed_f_cmd(char*c,char* key,void* val);
   void plumed_f_finalize(char*c);
+  void plumed_f_use_count(char*c,int*i);
   void plumed_f_installed_(int*i);
   void plumed_f_ginitialized_(int*i);
   void plumed_f_gcreate_();
@@ -20,8 +24,11 @@ extern "C"{
   void plumed_f_gfinalize_();
   void plumed_f_global_(char*c);
   void plumed_f_create_(char*c);
+  void plumed_f_create_dlopen_(char*c,char*p);
+  void plumed_f_create_reference_(char*p,char*r);
   void plumed_f_cmd_(char*c,char* key,void* val);
   void plumed_f_finalize_(char*c);
+  void plumed_f_use_count_(char*c,int*i);
   void plumed_f_installed__(int*i);
   void plumed_f_ginitialized__(int*i);
   void plumed_f_gcreate__();
@@ -29,8 +36,11 @@ extern "C"{
   void plumed_f_gfinalize__();
   void plumed_f_global__(char*c);
   void plumed_f_create__(char*c);
+  void plumed_f_create_dlopen__(char*c,char*p);
+  void plumed_f_create_reference__(char*p,char*r);
   void plumed_f_cmd__(char*c,char* key,void* val);
   void plumed_f_finalize__(char*c);
+  void plumed_f_use_count__(char*c,int*i);
   void PLUMED_F_INSTALLED(int*);
   void PLUMED_F_GINITIALIZED(int*);
   void PLUMED_F_GCREATE();
@@ -38,8 +48,11 @@ extern "C"{
   void PLUMED_F_GFINALIZE();
   void PLUMED_F_GLOBAL(char*c);
   void PLUMED_F_CREATE(char*c);
+  void PLUMED_F_CREATE_DLOPEN(char*c,char*p);
+  void PLUMED_F_CREATE_REFERENCE(char*p,char*r);
   void PLUMED_F_CMD(char*c,char* key,void* val);
   void PLUMED_F_FINALIZE(char*c);
+  void PLUMED_F_USE_COUNT(char*c,int*i);
   void PLUMED_F_INSTALLED_(int*);
   void PLUMED_F_GINITIALIZED_(int*);
   void PLUMED_F_GCREATE_();
@@ -47,8 +60,11 @@ extern "C"{
   void PLUMED_F_GFINALIZE_();
   void PLUMED_F_GLOBAL_(char*c);
   void PLUMED_F_CREATE_(char*c);
+  void PLUMED_F_CREATE_DLOPEN_(char*c,char*p);
+  void PLUMED_F_CREATE_REFERENCE_(char*p,char*r);
   void PLUMED_F_CMD_(char*c,char* key,void* val);
   void PLUMED_F_FINALIZE_(char*c);
+  void PLUMED_F_USE_COUNT_(char*c,int*i);
   void PLUMED_F_INSTALLED__(int*);
   void PLUMED_F_GINITIALIZED__(int*);
   void PLUMED_F_GCREATE__();
@@ -56,8 +72,11 @@ extern "C"{
   void PLUMED_F_GFINALIZE__();
   void PLUMED_F_GLOBAL__(char*c);
   void PLUMED_F_CREATE__(char*c);
+  void PLUMED_F_CREATE_DLOPEN__(char*c,char*p);
+  void PLUMED_F_CREATE_REFERENCE__(char*p,char*r);
   void PLUMED_F_CMD__(char*c,char* key,void* val);
   void PLUMED_F_FINALIZE__(char*c);
+  void PLUMED_F_USE_COUNT__(char*c,int*i);
 }
 
 template<typename T,typename S>
@@ -104,7 +123,7 @@ void testme(S cmd){
   cmd((char*)"calc",NULL);
 }
 
-void testmecpp(PLMD::Plumed&p){
+void testmecpp(PLMD::Plumed p){
   int natoms=10;
   std::vector<double> positions(3*natoms,0.0);
   std::vector<double> masses(natoms,1.0);
@@ -134,7 +153,15 @@ int main(){
  
     {
       PLMD::Plumed p;
+      if(!p) return 0;
+      if(!p.valid()) return 0;
       testmecpp(p);
+    }
+
+// test valid
+    {
+      PLMD::Plumed p(PLMD::Plumed::invalid);
+      if(p) return 0;
     }
 
     {
@@ -150,13 +177,27 @@ int main(){
       testmecpp(q);
     }
 
+    {
+// test dlopen
+      PLMD::Plumed p(PLMD::Plumed::dlopen(std::getenv("PLUMED_KERNEL")));
+      testmecpp(p);
+    }
+// test use_count
+    {
+      std::unique_ptr<PLMD::Plumed> p(new PLMD::Plumed);
+      if(!*p) return 0;
+      if(p->use_count()!=1) return 0;
+      auto q=*p;
+      if(p->use_count()!=2) return 0;
+      p.reset();
+      if(q.use_count()!=1) return 0;
+      testmecpp(q);
+    }
+
     if(PLMD::Plumed::ginitialized()) return 0;
     PLMD::Plumed::gcreate();
     if(!PLMD::Plumed::ginitialized()) return 0;
-    // this requires move semantics and only works with C++11
     PLMD::Plumed fromglobal(PLMD::Plumed::global());
-    // here's a workaround for plumed 2.3:
-    //PLMD::Plumed fromglobal(plumed_global());
     testmecpp(fromglobal);
     PLMD::Plumed::gfinalize();
     if(PLMD::Plumed::ginitialized()) return 0;
@@ -175,26 +216,66 @@ int main(){
       testme(f,plumed_f_cmd);
     }
 
-    char ff[32];
-    plumed_f_create(ff);
-// convert from fortran
-    PLMD::Plumed fromf(ff); // notice: this won't be deleted by destructor
-    testmecpp(fromf);
-    plumed_f_finalize(ff);
+    {
+      char ff[32];
+      plumed_f_create(ff);
+      PLMD::Plumed fromf(ff);
+      testmecpp(fromf);
+      plumed_f_finalize(ff);
 
-    plumed c=plumed_create();
-    PLMD::Plumed fromc(c); // notice: this won't be deleted by destructor
-    testmecpp(fromc);
-    plumed_finalize(c);
+      plumed c=plumed_create();
+      PLMD::Plumed fromc(c);
+      testmecpp(fromc);
+      plumed_finalize(c);
+    }
+    
+    {
+      char ff[32];
+      plumed_f_create(ff);
+// convert from fortran
+      PLMD::Plumed fromf(ff);
+      plumed_f_finalize(ff);
+      testmecpp(fromf);
+      if(!fromf || fromf.use_count()!=1) return 0;
+
+      plumed c=plumed_create();
+      PLMD::Plumed fromc(c);
+      plumed_finalize(c);
+      testmecpp(fromc);
+    }
     
   }
   {
 // C version
     of<<"C\n";
     if(!plumed_installed()) return 0;
+// test valid
+    {
+      plumed p=plumed_create_invalid();
+      if(plumed_valid(p)) return 0;
+      plumed_finalize(p);
+    }
     plumed p=plumed_create();
     testme(p,plumed_cmd);
     plumed_finalize(p);
+
+    {
+// test dlopen
+      plumed p=plumed_create_dlopen(std::getenv("PLUMED_KERNEL"));
+      testme(p,plumed_cmd);
+      plumed_finalize(p);
+    }
+// test use_count
+    {
+      plumed p=plumed_create();
+      if(plumed_use_count(p)!=1) return 0;
+      plumed q=plumed_create_reference(p);
+      if(plumed_use_count(p)!=2) return 0;
+      plumed_finalize(p);
+      if(plumed_use_count(q)!=1) return 0;
+      testme(q,plumed_cmd);
+      plumed_finalize(q);
+    }
 
     if(plumed_ginitialized()) return 0;
     plumed_gcreate();
@@ -226,6 +307,30 @@ int main(){
     testme(p,plumed_f_cmd);
     plumed_f_finalize(p);
 
+    {
+// test dlopen
+      char p[32];
+      plumed_f_create_dlopen(p,std::getenv("PLUMED_KERNEL"));
+      testme(p,plumed_f_cmd);
+      plumed_f_finalize(p);
+    }
+// test use_count
+    {
+      char p[32],q[32];
+      int count;
+      plumed_f_create(p);
+      plumed_f_use_count(p,&count);
+      if(count!=1) return 0;
+      plumed_f_create_reference(q,p);
+      plumed_f_use_count(p,&count);
+      if(count!=2) return 0;
+      plumed_f_finalize(p);
+      plumed_f_use_count(q,&count);
+      if(count!=1) return 0;
+      testme(q,plumed_f_cmd);
+      plumed_f_finalize(q);
+    }
+
     char p2[32];
     int ini;
     plumed_f_ginitialized(&ini); if(ini) return 0;
@@ -249,6 +354,30 @@ int main(){
     plumed_f_create_(p);
     testme(p,plumed_f_cmd_);
     plumed_f_finalize_(p);
+
+    {
+// test dlopen
+      char p[32];
+      plumed_f_create_dlopen(p,std::getenv("PLUMED_KERNEL"));
+      testme(p,plumed_f_cmd);
+      plumed_f_finalize(p);
+    }
+// test use_count
+    {
+      char p[32],q[32];
+      int count;
+      plumed_f_create_(p);
+      plumed_f_use_count_(p,&count);
+      if(count!=1) return 0;
+      plumed_f_create_reference_(q,p);
+      plumed_f_use_count_(p,&count);
+      if(count!=2) return 0;
+      plumed_f_finalize_(p);
+      plumed_f_use_count_(q,&count);
+      if(count!=1) return 0;
+      testme(q,plumed_f_cmd_);
+      plumed_f_finalize_(q);
+    }
 
     char p2[32];
     int ini;
@@ -274,6 +403,30 @@ int main(){
     testme(p,plumed_f_cmd__);
     plumed_f_finalize__(p);
 
+    {
+// test dlopen
+      char p[32];
+      plumed_f_create_dlopen__(p,std::getenv("PLUMED_KERNEL"));
+      testme(p,plumed_f_cmd__);
+      plumed_f_finalize__(p);
+    }
+// test use_count
+    {
+      char p[32],q[32];
+      int count;
+      plumed_f_create__(p);
+      plumed_f_use_count__(p,&count);
+      if(count!=1) return 0;
+      plumed_f_create_reference__(q,p);
+      plumed_f_use_count__(p,&count);
+      if(count!=2) return 0;
+      plumed_f_finalize__(p);
+      plumed_f_use_count__(q,&count);
+      if(count!=1) return 0;
+      testme(q,plumed_f_cmd__);
+      plumed_f_finalize__(q);
+    }
+
     char p2[32];
     int ini;
     plumed_f_ginitialized__(&ini); if(ini) return 0;
@@ -297,6 +450,30 @@ int main(){
     PLUMED_F_CREATE(p);
     testme(p,PLUMED_F_CMD);
     PLUMED_F_FINALIZE(p);
+
+    {
+// test dlopen
+      char p[32];
+      PLUMED_F_CREATE_DLOPEN(p,std::getenv("PLUMED_KERNEL"));
+      testme(p,PLUMED_F_CMD);
+      PLUMED_F_FINALIZE(p);
+    }
+// test use_count
+    {
+      char p[32],q[32];
+      int count;
+      PLUMED_F_CREATE(p);
+      PLUMED_F_USE_COUNT(p,&count);
+      if(count!=1) return 0;
+      PLUMED_F_CREATE_REFERENCE(q,p);
+      PLUMED_F_USE_COUNT(p,&count);
+      if(count!=2) return 0;
+      PLUMED_F_FINALIZE(p);
+      PLUMED_F_USE_COUNT(q,&count);
+      if(count!=1) return 0;
+      testme(q,PLUMED_F_CMD);
+      PLUMED_F_FINALIZE(q);
+    }
 
     char p2[32];
     int ini;
@@ -322,6 +499,30 @@ int main(){
     testme(p,PLUMED_F_CMD_);
     PLUMED_F_FINALIZE_(p);
 
+    {
+// test dlopen
+      char p[32];
+      PLUMED_F_CREATE_DLOPEN_(p,std::getenv("PLUMED_KERNEL"));
+      testme(p,PLUMED_F_CMD_);
+      PLUMED_F_FINALIZE_(p);
+    }
+// test use_count
+    {
+      char p[32],q[32];
+      int count;
+      PLUMED_F_CREATE_(p);
+      PLUMED_F_USE_COUNT_(p,&count);
+      if(count!=1) return 0;
+      PLUMED_F_CREATE_REFERENCE_(q,p);
+      PLUMED_F_USE_COUNT_(p,&count);
+      if(count!=2) return 0;
+      PLUMED_F_FINALIZE_(p);
+      PLUMED_F_USE_COUNT_(q,&count);
+      if(count!=1) return 0;
+      testme(q,PLUMED_F_CMD_);
+      PLUMED_F_FINALIZE_(q);
+    }
+
     char p2[32];
     int ini;
     PLUMED_F_GINITIALIZED_(&ini); if(ini) return 0;
@@ -345,6 +546,30 @@ int main(){
     PLUMED_F_CREATE__(p);
     testme(p,PLUMED_F_CMD__);
     PLUMED_F_FINALIZE__(p);
+
+    {
+// test dlopen
+      char p[32];
+      PLUMED_F_CREATE_DLOPEN__(p,std::getenv("PLUMED_KERNEL"));
+      testme(p,PLUMED_F_CMD__);
+      PLUMED_F_FINALIZE__(p);
+    }
+// test use_count
+    {
+      char p[32],q[32];
+      int count;
+      PLUMED_F_CREATE__(p);
+      PLUMED_F_USE_COUNT__(p,&count);
+      if(count!=1) return 0;
+      PLUMED_F_CREATE_REFERENCE__(q,p);
+      PLUMED_F_USE_COUNT__(p,&count);
+      if(count!=2) return 0;
+      PLUMED_F_FINALIZE__(p);
+      PLUMED_F_USE_COUNT__(q,&count);
+      if(count!=1) return 0;
+      testme(q,PLUMED_F_CMD__);
+      PLUMED_F_FINALIZE__(q);
+    }
 
     char p2[32];
     int ini;
