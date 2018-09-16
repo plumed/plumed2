50e24e2c60764df460641b61dec8c4883249e5cc
Omar Valsson <omar.valsson@gmail.com>
2018-09-16 15:32:45 +0200

    changed UNITS manual

--- src/setup/Units.cpp
+++ src/setup/Units.cpp
@@ -33,9 +33,10 @@ namespace setup {
 //+PLUMEDOC GENERIC UNITS
 /*
 This command sets the internal units for the code.  A new unit can be set by either
-specifying how to convert from the plumed default unit into that new unit or by using
-the shortcuts described below.  This directive MUST appear at the BEGINNING of the
-plumed.dat file.  The same units must be used througout the plumed.dat file.
+specifying a conversion factor from the plumed default unit or by using a string
+corresponding to one of the defined units given below.  This directive MUST
+appear at the BEGINNING of the plumed.dat file.  The same units must be used
+througout the plumed.dat file.
 
 Notice that all input/output will then be made using the specified units.
 That is: all the input parameters, all the output files, etc. The only
@@ -43,10 +44,19 @@ exceptions are file formats for which there is a specific convention concerning
 the units. For example, trajectories written in .gro format (with \ref DUMPATOMS)
 are going to be always in nm.
 
+The following strings can be used to specify units. Note that the strings are
+case senstive.
+- LENGTH: nm (default), A (for Angstrom), um (for micrometer), Bohr (0.052917721067 nm)
+- ENERGY: kj/mol (default), j/mol, kcal/mol (4.184 kj/mol), eV (96.48530749925792 kj/mol), Ha (for Hartree, 2625.499638 kj/mol)
+- TIME: ps (default), fs, ns, atomic (2.418884326509e-5 ps)
+- MASS: amu (default)
+- CHARGE: e (default)
+
+
 \par Examples
 
 \plumedfile
-# this is using nm - kj/mol - fs
+# this is using Angstrom - kj/mol - fs
 UNITS LENGTH=A TIME=fs
 
 # compute distance between atoms 1 and 4
@@ -89,7 +99,7 @@ PLUMED_REGISTER_ACTION(Units,"UNITS")
 
 void Units::registerKeywords( Keywords& keys ) {
   ActionSetup::registerKeywords(keys);
-  keys.add("optional","LENGTH","the units of lengths.  Either specify a conversion factor from the default, nm, or use one of the defined units, A (for angstroms), um, and Bohr.");
+  keys.add("optional","LENGTH","the units of lengths.  Either specify a conversion factor from the default, nm, or use one of the defined units, A (for angstroms), um (for micrometer), and Bohr.");
   keys.add("optional","ENERGY","the units of energy.  Either specify a conversion factor from the default, kj/mol, or use one of the defined units, j/mol, kcal/mol and Ha (for Hartree)");
   keys.add("optional","TIME","the units of time.  Either specify a conversion factor from the default, ps, or use one of the defined units, ns, fs, and atomic");
   keys.add("optional","MASS","the units of masses.  Specify a conversion factor from the default, amu");
