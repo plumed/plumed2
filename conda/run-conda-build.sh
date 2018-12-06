8c8145a2589c84c9ef9afc965ec378a2a5f290ab
Toni G <toni.giorgino@gmail.com>
2018-12-06 11:57:31 +0100

    nn

--- conda/run-conda-build.sh
+++ conda/run-conda-build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Anywhere but outside of the repository
-CONDA_HOME=/var/tmp/miniconda
+export CONDA_HOME=/var/tmp/miniconda
 
 wget -c https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh -O /tmp/miniconda.sh
 bash /tmp/miniconda.sh -b -f -p $CONDA_HOME
@@ -11,6 +11,12 @@ conda update -q conda
 conda info -a
 conda install conda-build
 
+conda-build recipe
+
+ls -lR $CONDA_HOME/conda-bld
+
+# And now upload if desired
+# conda upload $CONDA_HOME/conda-bld/linux-64/*.tar.bz2
 
 
 # https://conda.io/docs/user-guide/tasks/use-conda-with-travis-ci.html
