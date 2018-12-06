d82abc61cff1998e33ec7d8e797e4c67e1acd1b1
Toni G <toni.giorgino@gmail.com>
2018-12-06 12:30:53 +0100

    tweak osx

--- conda/run-conda-build.sh
+++ conda/run-conda-build.sh
@@ -3,7 +3,16 @@
 # Anywhere but outside of the repository
 export CONDA_HOME=/var/tmp/miniconda
 
-wget -c https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh -O /tmp/miniconda.sh
+if [[ "$TRAVIS_OS_NAME" == "linux" ]]; then
+    csys=Linux
+elif [[ "$TRAVIS_OS_NAME" == "osx" ]]; then
+    csys=MacOSX
+else
+    echo "Unsupported system $TRAVIS_OS_NAME"
+    exit 1
+fi
+    
+wget -c https://repo.continuum.io/miniconda/Miniconda3-latest-$csys-x86_64.sh -O /tmp/miniconda.sh
 bash /tmp/miniconda.sh -b -f -p $CONDA_HOME
 export PATH="$CONDA_HOME/bin:$PATH"
 conda config --set always_yes yes --set changeps1 no
@@ -14,7 +23,7 @@ conda install conda-build
 conda-build recipe
 
 ls -l $CONDA_HOME/conda-bld/
-ls -lR $CONDA_HOME/conda-bld/linux-64
+ls -lR $CONDA_HOME/conda-bld/*-64  $CONDA_HOME/conda-bld/noarch
 
 # And now upload if desired
 # conda upload $CONDA_HOME/conda-bld/linux-64/*.tar.bz2
