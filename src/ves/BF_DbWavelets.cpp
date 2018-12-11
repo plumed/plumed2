90e09d069b949048f77513ffc1c9c044d41ed087
Benjamin Pampel <pampel@mpip-mainz.mpg.de>
2018-12-11 14:05:22 +0100

    Adjusted labels

--- src/ves/BF_DbWavelets.cpp
+++ src/ves/BF_DbWavelets.cpp
@@ -171,7 +171,7 @@ void BF_DbWavelets::getAllValues(const double arg, double& argT, bool& inside_ra
 void BF_DbWavelets::setupLabels() {
   setLabel(0,"const");
   for(unsigned int i=1; i < getNumberOfBasisFunctions(); i++) {
-    double pos = intervalMin() + i/scale_;
+    double pos = intervalMin() + (i-intrinsicIntervalMax())/scale_;
     std::string is; Tools::convert(pos, is);
     setLabel(i,"i="+is);
   }
