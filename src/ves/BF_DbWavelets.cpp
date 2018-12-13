33350c7a5a5d97758ff56e17dab09bf8ed2aa6aa
Benjamin Pampel <pampel@mpip-mainz.mpg.de>
2018-12-13 14:59:54 +0100

    Forgot considering negative values

--- src/ves/BF_DbWavelets.cpp
+++ src/ves/BF_DbWavelets.cpp
@@ -183,14 +183,14 @@ std::vector<double> BF_DbWavelets::getCutoffPoints(const double& threshold) {
   std::vector<double> cutoffpoints;
 
   for (unsigned i = 0; i < waveletGrid_->getSize(); ++i) {
-    if (waveletGrid_->getValue(i) > threshold_value) {
+    if (fabs(waveletGrid_->getValue(i)) > threshold_value) {
       cutoffpoints.push_back(waveletGrid_->getPoint(i)[0]);
       break;
     }
   }
 
   for (unsigned i = waveletGrid_->getSize(); i <= 0; --i) {
-    if (waveletGrid_->getValue(i) > threshold_value) {
+    if (fabs(waveletGrid_->getValue(i)) > threshold_value) {
       cutoffpoints.push_back(waveletGrid_->getPoint(i)[0]);
       break;
     }
