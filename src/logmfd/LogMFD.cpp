2632356f082f665e96c599e5ee5fa8b2d29eb999
carlocamilloni <carlo.camilloni@gmail.com>
2018-12-17 22:06:06 +0100

    astyle

--- src/logmfd/LogMFD.cpp
+++ src/logmfd/LogMFD.cpp
@@ -18,7 +18,7 @@ You should have received a copy of the GNU Lesser General Public License
 along with plumed.  If not, see <http://www.gnu.org/licenses/>.
 +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++ */
 
-//+PLUMEDOC LOGMFDMOD_BIAS LOGMFD 
+//+PLUMEDOC LOGMFDMOD_BIAS LOGMFD
 /*
 Used to perform LogMFD, LogPD, and TAMD/d-AFED.
 
