f4d889bad78510ea0c11ab006b3ac4718528d27f
ali <a.farnudi@physics.sharif.edu>
2019-04-10 11:52:30 +0200

    Gareth is leaving in 10min. THe radius of gyration is fine. There is still a problem with the trace and aspherecity.

--- src/multicolvar/Gyration.cpp
+++ src/multicolvar/Gyration.cpp
@@ -24,6 +24,7 @@
 #include "core/ActionAtomistic.h"
 #include "core/ActionShortcut.h"
 #include "core/PlumedMain.h"
+#include "core/ActionSet.h"
 
 #include <string>
 #include <cmath>
@@ -90,7 +91,11 @@ public ActionWithValue
 {
 private:
   bool nopbc;
+  Value* val_weights;
+  bool weight_mass, weight_charge;
+  std::vector<double> weights;
   std::vector<double> forcesToApply;
+  double getWeight( const unsigned& task_index ) const ;
 public:
   static void registerKeywords(Keywords& keys);
   explicit GyrationTensor(const ActionOptions&);
@@ -110,6 +115,7 @@ void GyrationTensor::registerKeywords(Keywords& keys) {
            "If WEIGHTS=@masses is used the center of mass is computed.  If WEIGHTS=@charges the center of charge is computed.  If "
            "the label of an action is provided PLUMED assumes that that action calculates a list of symmetry functions that can be used "
            "as weights. Lastly, an explicit list of numbers to use as weights can be provided");
+  keys.addFlag("MASS",false,"calculate the center of mass");
   keys.addFlag("NOPBC",false,"ignore the periodic boundary conditions when calculating distances");
 }
 
@@ -126,6 +132,57 @@ GyrationTensor::GyrationTensor(const ActionOptions&ao):
   parseAtomList("CENTER",catom);
   if(catom.size()!=1) error("should be one central atom only");
   parseFlag("NOPBC",nopbc);
+  //copied this bit from ../vatom/Center.cpp  :: Begin 
+  bool usemass = false; parseFlag("MASS",usemass);
+  if(atoms.size()==0) error("at least one atom should be specified");
+  std::vector<std::string> str_weights; parseVector("WEIGHTS",str_weights);
+  if( usemass ) {
+      if( str_weights.size()>0 ) error("USEMASS is incompatible with WEIGHTS");
+      str_weights.resize(1); str_weights[0]="@masses";
+  }
+  if( str_weights.size()==0 ) {
+    log<<"  computing the geometric center of atoms:\n";
+    weights.resize( atoms.size() );
+    for(unsigned i=0; i<atoms.size(); i++) weights[i] = 1.;
+  } else if( str_weights.size()==1 ) {
+    if( str_weights[0]=="@masses" ) {
+      weight_mass=true;
+      log<<"  computing the center of mass of atoms:\n";
+    } else if( str_weights[0]=="@charges" ) {
+      weight_charge=true;
+      log<<"  computing the center of charge of atoms:\n";
+    } else {
+      std::size_t dot=str_weights[0].find_first_of("."); unsigned nargs=0; std::vector<Value*> args;
+      if( dot!=std::string::npos ) {
+        ActionWithValue* action=plumed.getActionSet().selectWithLabel<ActionWithValue*>( str_weights[0].substr(0,dot) );
+        if( !action ) {
+          std::string str=" (hint! the actions in this ActionSet are: ";
+          str+=plumed.getActionSet().getLabelList()+")";
+          error("cannot find action named " + str_weights[0] +str);
+        }
+        action->interpretDataLabel( str_weights[0], this, nargs, args );
+      } else {
+        ActionWithValue* action=plumed.getActionSet().selectWithLabel<ActionWithValue*>( str_weights[0] );
+        if( !action ) {
+          std::string str=" (hint! the actions in this ActionSet are: ";
+          str+=plumed.getActionSet().getLabelList()+")";
+          error("cannot find action named " + str_weights[0] +str);
+        }
+        action->interpretDataLabel( str_weights[0], this, nargs, args );
+      }
+    }   
+  } else {
+    log<<" with weights:";
+    if( str_weights.size()!=atoms.size() ) error("number of elements in weight vector does not match the number of atoms");
+    weights.resize( atoms.size() );
+    for(unsigned i=0; i<weights.size(); ++i) {
+      if(i%25==0) log<<"\n";
+      Tools::convert( str_weights[i], weights[i] ); log.printf(" %f",weights[i]);
+    }
+    log.printf("\n");
+  }
+  //copied this bit from ../vatom/Center.cpp  :: End
+
   checkRead();
 
   log<<"  Bibliography "<<plumed.cite("Jirí Vymetal and Jirí Vondrasek, J. Phys. Chem. A 115, 11455 (2011)");
@@ -150,22 +207,34 @@ GyrationTensor::GyrationTensor(const ActionOptions&ao):
   atoms.push_back(catom[0]); requestAtoms(atoms); 
 }
 
+double GyrationTensor::getWeight( const unsigned& task_index ) const {
+  if( weight_mass ) return getMass(task_index);
+  if( weight_charge ) {
+      if( !plumed.getAtoms().chargesWereSet() ) plumed_merror("cannot calculate center of charge if chrages are unset");
+      return getCharge(task_index);
+  }
+  if( val_weights ) return val_weights->get(task_index);
+  plumed_dbg_assert( task_index<weights.size() );
+  return weights[task_index];
+}
+
 void GyrationTensor::calculate() {
 
   if(!nopbc) makeWhole();
 
-  Tensor3d gyr_tens;
+  Tensor3d gyr_tens; double totmass = 0;
   //calculate gyration tensor
   for(unsigned i=0; i<getNumberOfAtoms()-1; i++) {
+    double w = getWeight(i);
     const Vector diff=delta( getPosition(getNumberOfAtoms()-1), getPosition(i) );
-    gyr_tens[0][0]+=diff[0]*diff[0];
-    gyr_tens[1][1]+=diff[1]*diff[1];
-    gyr_tens[2][2]+=diff[2]*diff[2];
-    gyr_tens[0][1]+=diff[0]*diff[1];
-    gyr_tens[0][2]+=diff[0]*diff[2];
-    gyr_tens[1][2]+=diff[1]*diff[2];
+    gyr_tens[0][0]+=w*diff[0]*diff[0];
+    gyr_tens[1][1]+=w*diff[1]*diff[1];
+    gyr_tens[2][2]+=w*diff[2]*diff[2];
+    gyr_tens[0][1]+=w*diff[0]*diff[1];
+    gyr_tens[0][2]+=w*diff[0]*diff[2];
+    gyr_tens[1][2]+=w*diff[1]*diff[2];
+    totmass += w ;
   }
-  double totmass = static_cast<double>(getNumberOfAtoms()-1);
 
   Value* myval=getPntrToOutput(0); 
   myval->set(0, gyr_tens[0][0] / totmass );
@@ -183,17 +252,18 @@ void GyrationTensor::apply(){
   if( doNotCalculateDerivatives() ) return ;
   
   // Retrieve the forces from the values
-  double totmass = static_cast<double>(getNumberOfAtoms()-1);
+  double totmass=0; for(unsigned i=0; i<getNumberOfAtoms()-1; i++) totmass += getWeight(i);
   for(unsigned i=0;i<9;++i) forcesToApply[i] = getPntrToOutput(0)->getForce( i ) / totmass;
 
   unsigned n=getNumberOfAtoms()-1; Vector ff;
   std::vector<Vector>& f(modifyForces());
   Tensor&              v(modifyVirial());
   for(unsigned i=0; i<getNumberOfAtoms()-1; i++) {
+      double w = getWeight(i);
       const Vector diff=delta( getPosition(getNumberOfAtoms()-1), getPosition(i) );
-      ff[0] = 2*forcesToApply[0]*diff[0] + forcesToApply[1]*diff[1] + forcesToApply[2]*diff[2]; 
-      ff[1] = forcesToApply[3]*diff[0] + 2*forcesToApply[4]*diff[1] + forcesToApply[5]*diff[2]; 
-      ff[2] = forcesToApply[6]*diff[0] + forcesToApply[7]*diff[1] + 2*forcesToApply[8]*diff[2];
+      ff[0] = w*(2*forcesToApply[0]*diff[0] + forcesToApply[1]*diff[1] + forcesToApply[2]*diff[2]); 
+      ff[1] = w*(forcesToApply[3]*diff[0] + 2*forcesToApply[4]*diff[1] + forcesToApply[5]*diff[2]); 
+      ff[2] = w*(forcesToApply[6]*diff[0] + forcesToApply[7]*diff[1] + 2*forcesToApply[8]*diff[2]);
       f[i] += ff; f[n] -= ff;
       v -= Tensor(getPosition(i),ff);
   }
@@ -219,7 +289,7 @@ Action(ao),
 ActionShortcut(ao)
 {
     std::string atoms; parse("ATOMS",atoms); bool nopbc; parseFlag("NOPBC",nopbc); 
-    std::string pbcstr; if(nopbc) pbcstr = " NOPBC"; 
+    std::string pbcstr; if(nopbc) pbcstr = " NOPBC";
     // Create the geometric center of the molecule
     readInputLine( getShortcutLabel() + "_cent: CENTER ATOMS=" + atoms + pbcstr );
     // Now compute the gyration tensor
@@ -238,7 +308,9 @@ ActionShortcut(ao)
     } else {
 	// Diagonalize the gyration tensor
 	readInputLine( getShortcutLabel() + "_diag: DIAGONALIZE ARG=" + getShortcutLabel() + "_tensor VECTORS=all" );    
-        if() {
+        if( gtype=="ASPHERICITY" ) {
+            readInputLine( getShortcutLabel() + ": MATHEVAL ARG1=" + getShortcutLabel() + "_diag.vals-1" +
+			   " ARG2=" + getShortcutLabel() + "_diag.vals-2 ARG3=" + getShortcutLabel() + "_diag.vals-3 FUNC=x-0.5*(y+z) PERIODIC=NO");
 	} else error( gtype + " is not a valid type for gyration radius");
     }	
 }
