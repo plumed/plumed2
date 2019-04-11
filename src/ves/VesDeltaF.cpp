08130d00b892db566e2d35ec7a9f85fe36dcc994
Michele Invernizzi <inve.michele@gmail.com>
2019-04-11 10:21:59 +0200

    VesDeltaF: updated bibliography and other minor improvements (#455)
    * first public version of VesDeltaF
    
    * minor doc changes
    
    * labor limae
    
    * added doc warning about being an isolated VES component
    
    * better doc
    
    * added an update() function, instead of doing everything in calculate()
    
    * removed uncecessary include
    
    * removed pointers to values
    
    * added regtest for VesDeltaF
    
    * added reference to preprint on arxiv
    
    * added ves namespace to try to make travis happy
    
    * removed unnecessary namespaces
    
    * expanded regtests to improve code coverage. and fixed bug with default BIASFACTOR
    
    * last minute minor doc improvement
    
    * added RESTART key
    
    * improved doc
    
    * minor code style modifications
    
    * added lepton (to sync with master)
    
    * updated bibliography, exept for page number
    
    * updated reference with page numbers
    
    * change default filename to ALPHA, to be more allinged to METAD names conventions
    
    * switched from NO_MULTIPLE_WALKERS to MULTIPLE_WALKERS flag, to be consistent with the rest of VES module
    
    * removed the accidentaly added old folder src/lepton/asmjit

--- src/ves/VesDeltaF.cpp
+++ src/ves/VesDeltaF.cpp
@@ -65,7 +65,7 @@ Using the keyword `NORMALIZE`, you can also decide to normalize the local free e
 In this case the parameters will represent not the difference in height (which depends on the chosen CVs),
 but the actual free energy difference, \f$\alpha_i=\Delta F_i\f$.
 
-However, as discussed in \cite Invernizzi2019vesdeltaf, a better estimate of \f$\Delta F_i\f$ should be obtained through the reweighting procedure.
+However, as discussed in Ref. \cite Invernizzi2019vesdeltaf, a better estimate of \f$\Delta F_i\f$ should be obtained through the reweighting procedure.
 
 \par Examples
 
@@ -87,7 +87,13 @@ VES_DELTA_F ...
 PRINT FMT=%g STRIDE=500 FILE=Colvar.data ARG=cv,ves.bias,ves.rct
 \endplumedfile
 
-The local FES files were obtained via \ref sum_hills and their header should be similar to the following:
+The local FES files can be obtained as described in Sec. 4.2 of Ref. \cite Invernizzi2019vesdeltaf, i.e. for example:
+- run 4 indipendent MetaD runs, all starting from basin A, and kill them as soon as they make the first transition (see e.g. \ref COMMITTOR)
+- \verbatim cat HILLS* > all_HILLS \endverbatim
+- \verbatim plumed sum_hills --hills all_HILLS --oufile all_fesA.dat --mintozero --min -1 --max 1 --bin 100 \endverbatim
+- \verbatim awk -v n_rep=4 '{if($1!="#!" && $1!="") {for(i=1+(NF-1)/2; i<=NF; i++) $i/=n_rep;} print $0}' all_fesA.dat > fesA.data \endverbatim
+
+The header of the file should be similar to the following:
 
 \verbatim
 #! FIELDS cv file.free der_cv
@@ -106,7 +112,7 @@ private:
   double beta_;
   unsigned NumParallel_;
   unsigned rank_;
-  unsigned walkers_num_;
+  unsigned NumWalkers_;
   bool isFirstStep_;
 
 //local basins
@@ -180,12 +186,13 @@ void VesDeltaF::registerKeywords(Keywords& keys) {
   keys.add("optional","INITIAL_ALPHA","( default=0 ) an initial guess for the bias potential parameter alpha");
   keys.addFlag("DAMPING_OFF",false,"do not use an AdaGrad-like term to rescale M_STEP");
 //output parameters file
-  keys.add("compulsory","ALPHA_FILE","Alpha.data","file name for output minimization parameters");
+  keys.add("compulsory","ALPHA_FILE","ALPHA","file name for output minimization parameters");
   keys.add("optional","PRINT_STRIDE","( default=10 ) stride for printing to ALPHA_FILE");
   keys.add("optional","FMT","specify format for ALPHA_FILE");
 //debug flags
   keys.addFlag("SERIAL",false,"perform the calculation in serial even if multiple tasks are available");
-  keys.addFlag("NO_MULTIPLE_WALKERS",false,"do not use multiple walkers, even if there are multiple simulations running");
+  keys.addFlag("MULTIPLE_WALKERS",false,"use multiple walkers connected via MPI for the optimization");
+  keys.use("RESTART");
 
 //output components
   componentsAreNotOptional(keys);
@@ -205,7 +212,7 @@ VesDeltaF::VesDeltaF(const ActionOptions&ao)
   double temp=0;
   parse("TEMP",temp);
   double KbT=Kb*temp;
-  if (KbT==0)
+  if(KbT==0)
   {
     KbT=plumed.getAtoms().getKbT();
     plumed_massert(KbT>0,"your MD engine does not pass the temperature to plumed, you must specify it using TEMP");
@@ -221,7 +228,7 @@ VesDeltaF::VesDeltaF(const ActionOptions&ao)
   for(unsigned n=0;; n++)//NB: here we start from FILE_F0 not from FILE_F1
   {
     std::string filename;
-    if (!parseNumbered("FILE_F",n,filename))
+    if(!parseNumbered("FILE_F",n,filename))
       break;
     fes_names.push_back(filename);
     IFile gridfile;
@@ -243,7 +250,7 @@ VesDeltaF::VesDeltaF(const ActionOptions&ao)
 
   bool no_mintozero=false;
   parseFlag("NO_MINTOZERO",no_mintozero);
-  if (!no_mintozero)
+  if(!no_mintozero)
   {
     for(unsigned n=0; n<grid_p_.size(); n++)
       grid_p_[n]->setMinToZero();
@@ -257,17 +264,17 @@ VesDeltaF::VesDeltaF(const ActionOptions&ao)
   const unsigned ncv=getNumberOfArguments(); //just for ease
   for(unsigned n=0; n<grid_p_.size(); n++)
   {
-    for (Grid::index_t t=0; t<grid_p_[n]->getSize(); t++)
+    for(Grid::index_t t=0; t<grid_p_[n]->getSize(); t++)
     {
       std::vector<double> der(ncv);
       const double val=std::exp(-beta_*grid_p_[n]->getValueAndDerivatives(t,der));
-      for (unsigned s=0; s<ncv; s++)
+      for(unsigned s=0; s<ncv; s++)
         der[s]*=-beta_*val;
       grid_p_[n]->setValueAndDerivatives(t,val,der);
       norm_[n]+=val;
     }
     c_norm[n]=1./beta_*std::log(norm_[n]);
-    if (normalize)
+    if(normalize)
     {
       grid_p_[n]->scaleAllValuesAndDerivatives(1./norm_[n]);
       norm_[n]=1;
@@ -278,7 +285,7 @@ VesDeltaF::VesDeltaF(const ActionOptions&ao)
   double biasfactor=0;
   parse("BIASFACTOR",biasfactor);
   plumed_massert(biasfactor==0 || biasfactor>1,"BIASFACTOR must be zero (for uniform target) or greater than one");
-  if (biasfactor==0)
+  if(biasfactor==0)
     inv_gamma_=0;
   else
     inv_gamma_=1./biasfactor;
@@ -300,14 +307,14 @@ VesDeltaF::VesDeltaF(const ActionOptions&ao)
 
   mean_weight_tau_=0;
   parse("TAU_MEAN",mean_weight_tau_);
-  if (mean_weight_tau_!=1) //set it to 1 for basic SGD
+  if(mean_weight_tau_!=1) //set it to 1 for basic SGD
   {
     plumed_massert((mean_weight_tau_==0 || mean_weight_tau_>av_stride_),"TAU_MEAN is rescaled with AV_STRIDE, so it has to be greater");
     mean_weight_tau_/=av_stride_; //this way you can look at the number of simulation steps to choose TAU_MEAN
   }
 
   parseVector("INITIAL_ALPHA",mean_alpha_);
-  if (mean_alpha_.size()>0)
+  if(mean_alpha_.size()>0)
   {
     plumed_massert(mean_alpha_.size()==alpha_size_,"provide one INITIAL_ALPHA for each basin beyond the first one");
   }
@@ -321,13 +328,13 @@ VesDeltaF::VesDeltaF(const ActionOptions&ao)
 
   damping_off_=false;
   parseFlag("DAMPING_OFF",damping_off_);
-  if (damping_off_)
+  if(damping_off_)
     past_increment2_.resize(alpha_size_,1);
   else
     past_increment2_.resize(alpha_size_,0);
 
 //file printing options
-  std::string alphaFileName("Alpha.data");
+  std::string alphaFileName("ALPHA");
   parse("ALPHA_FILE",alphaFileName);
   print_stride_=10;
   parse("PRINT_STRIDE",print_stride_);
@@ -339,35 +346,35 @@ VesDeltaF::VesDeltaF(const ActionOptions&ao)
   rank_=comm.Get_rank();
   bool serial=false;
   parseFlag("SERIAL",serial);
-  if (serial)
+  if(serial)
   {
     log.printf(" -- SERIAL: running without loop parallelization\n");
     NumParallel_=1;
     rank_=0;
   }
 
-  bool no_multiple_walkers=false;
-  parseFlag("NO_MULTIPLE_WALKERS",no_multiple_walkers);
-  if (no_multiple_walkers)
-    walkers_num_=1;
+  bool multiple_walkers=false;
+  parseFlag("MULTIPLE_WALKERS",multiple_walkers);
+  if(!multiple_walkers)
+    NumWalkers_=1;
   else
   {
-    if (comm.Get_rank()==0)//multi_sim_comm works well on first rank only
-      walkers_num_=multi_sim_comm.Get_size();
-    if (comm.Get_size()>1) //if each walker has more than one processor update them all
-      comm.Bcast(walkers_num_,0);
+    if(comm.Get_rank()==0)//multi_sim_comm works well on first rank only
+      NumWalkers_=multi_sim_comm.Get_size();
+    if(comm.Get_size()>1) //if each walker has more than one processor update them all
+      comm.Bcast(NumWalkers_,0);
   }
 
   checkRead();
 
 //restart if needed
-  if (getRestart())
+  if(getRestart())
   {
     IFile ifile;
     ifile.link(*this);
-    if (walkers_num_>1)
+    if(NumWalkers_>1)
       ifile.enforceSuffix("");
-    if (ifile.FileExist(alphaFileName))
+    if(ifile.FileExist(alphaFileName))
     {
       log.printf("  Restarting from: %s\n",alphaFileName.c_str());
       log.printf("    all options (also PRINT_STRIDE) must be consistent!\n");
@@ -395,7 +402,7 @@ VesDeltaF::VesDeltaF(const ActionOptions&ao)
       }
       //sync all walkers and treads. Not sure is mandatory but is no harm
       comm.Barrier();
-      if (comm.Get_rank()==0)
+      if(comm.Get_rank()==0)
         multi_sim_comm.Barrier();
     }
     else
@@ -404,14 +411,14 @@ VesDeltaF::VesDeltaF(const ActionOptions&ao)
 
 //setup output file with Alpha values
   alphaOfile_.link(*this);
-  if (walkers_num_>1)
+  if(NumWalkers_>1)
   {
-    if (comm.Get_rank()==0 && multi_sim_comm.Get_rank()>0)
+    if(comm.Get_rank()==0 && multi_sim_comm.Get_rank()>0)
       alphaFileName="/dev/null"; //only first walker writes on file
     alphaOfile_.enforceSuffix("");
   }
   alphaOfile_.open(alphaFileName);
-  if (fmt.length()>0)
+  if(fmt.length()>0)
     alphaOfile_.fmtField(" "+fmt);
 
 //add other output components
@@ -424,36 +431,44 @@ VesDeltaF::VesDeltaF(const ActionOptions&ao)
   log.printf("  Local free energy basins files and normalization constants:\n");
   for(unsigned n=0; n<grid_p_.size(); n++)
     log.printf("    F_%d filename: %s  c_%d=%g\n",n,fes_names[n].c_str(),n,c_norm[n]);
-  if (no_mintozero)
-    log.printf("  -- NO_MINTOZERO: local free energies are not shifted to be zero at minimum\n");
-  if (normalize)
-    log.printf("  -- NORMALIZE: F_n+=c_n, alpha=DeltaF\n");
+  if(no_mintozero)
+    log.printf(" -- NO_MINTOZERO: local free energies are not shifted to be zero at minimum\n");
+  if(normalize)
+    log.printf(" -- NORMALIZE: F_n+=c_n, alpha=DeltaF\n");
   log.printf("  Using target distribution with 1/gamma = %g\n",inv_gamma_);
   log.printf("    and updated with stride %d\n",tg_stride_);
   log.printf("  Step for the minimization algorithm: %g\n",minimization_step_);
   log.printf("  Stride for the ensemble average: %d\n",av_stride_);
-  if (mean_weight_tau_>1)
+  if(mean_weight_tau_>1)
     log.printf("  Exponentially decaying average with weight=tau/av_stride=%d\n",mean_weight_tau_);
-  if (mean_weight_tau_==1)
-    log.printf(" -- WARNING: setting TAU_MEAN=1 is equivalent to use simple SGD, without mean alpha nor hessian contribution\n");
+  if(mean_weight_tau_==1)
+    log.printf(" +++ WARNING +++ setting TAU_MEAN=1 is equivalent to use simple SGD, without mean alpha nor hessian contribution\n");
   log.printf("  Initial guess for alpha:\n");
   for(unsigned i=0; i<alpha_size_; i++)
     log.printf("    alpha_%d = %g\n",i+1,mean_alpha_[i]);
-  if (damping_off_)
-    log.printf("  -- DAMPING_OFF: the minimization step will NOT become smaller as the simulation goes on\n");
+  if(damping_off_)
+    log.printf(" -- DAMPING_OFF: the minimization step will NOT become smaller as the simulation goes on\n");
   log.printf("  Printing on file %s with stride %d\n",alphaFileName.c_str(),print_stride_);
-  if (serial)
+  if(serial)
     log.printf(" -- SERIAL: running without loop parallelization\n");
-  if (NumParallel_>1)
+  if(NumParallel_>1)
     log.printf("  Using multiple threads per simulation: %d\n",NumParallel_);
-  if (no_multiple_walkers)
-    log.printf("  -- NO_MULTIPLE_WALKERS: multiple simulations will not communicate\n");
-  if (walkers_num_>1)
+  if(multiple_walkers)
   {
-    log.printf("  Using multiple walkers\n");
-    log.printf("    number of walkers: %d\n",walkers_num_);
-    log.printf("    walker rank: %d\n",multi_sim_comm.Get_rank()); //only comm.Get_rank()=0 prints, so this is fine
+    log.printf(" -- MULTIPLE_WALKERS: multiple simulations will combine statistics for the optimization\n");
+    if(NumWalkers_>1)
+    {
+      log.printf("    number of walkers: %d\n",NumWalkers_);
+      log.printf("    walker rank: %d\n",multi_sim_comm.Get_rank()); //only comm.Get_rank()=0 prints, so this is fine
+    }
+    else
+      log.printf(" +++ WARNING +++ only one replica found: are you sure you are running MPI-connected simulations?\n");
   }
+  log.printf(" Bibliography ");
+  log<<plumed.cite("Invernizzi and Parrinello, J. Chem. Theory Comput. 15, 2187-2194 (2019)");
+  log<<plumed.cite("Valsson and Parrinello, Phys. Rev. Lett. 113, 090601 (2014)");
+  if(inv_gamma_>0)
+    log<<plumed.cite("Valsson and Parrinello, J. Chem. Theory Comput. 11, 1996-2002 (2015)");
 
 //set initial value for tg averages and rct
   update_tg_and_rct();
@@ -485,7 +500,7 @@ void VesDeltaF::calculate()
     setOutputForce(s,-(1-inv_gamma_)/beta_/tot_prob*dProb_dCV_s);
   }
 //skip first step to sync getTime() and av_counter_, as in METAD
-  if (isFirstStep_)
+  if(isFirstStep_)
   {
     isFirstStep_=false;
     return;
@@ -523,11 +538,11 @@ void VesDeltaF::calculate()
 
 void VesDeltaF::update()
 {
-  if (av_counter_==av_stride_)
+  if(av_counter_==av_stride_)
   {
     update_alpha();
     tg_counter_++;
-    if (tg_counter_==tg_stride_)
+    if(tg_counter_==tg_stride_)
     {
       update_tg_and_rct();
       tg_counter_=0;
@@ -549,7 +564,7 @@ void VesDeltaF::update_tg_and_rct()
   double Z_tg=0;
   std::fill(tg_dV_dAlpha_.begin(),tg_dV_dAlpha_.end(),0);
   std::fill(tg_d2V_dAlpha2_.begin(),tg_d2V_dAlpha2_.end(),0);
-  for (Grid::index_t t=rank_; t<grid_p_[0]->getSize(); t+=NumParallel_)
+  for(Grid::index_t t=rank_; t<grid_p_[0]->getSize(); t+=NumParallel_)
   { //FIXME can we recycle some code?
     std::vector<double> prob(grid_p_.size());
     for(unsigned n=0; n<grid_p_.size(); n++)
@@ -574,7 +589,7 @@ void VesDeltaF::update_tg_and_rct()
     for(unsigned ij=0; ij<sym_alpha_size_; ij++)
       tg_d2V_dAlpha2_[ij]+=unnorm_tg_p*d2V_dAlpha2[ij];
   }
-  if (NumParallel_>1)
+  if(NumParallel_>1)
   {
     comm.Sum(Z_tg);
     comm.Sum(tg_dV_dAlpha_);
@@ -590,22 +605,22 @@ void VesDeltaF::update_tg_and_rct()
 void VesDeltaF::update_alpha()
 {
 //combining the averages of multiple walkers
-  if (walkers_num_>1)
+  if(NumWalkers_>1)
   {
-    if (comm.Get_rank()==0) //sum only once: in the first rank of each walker
+    if(comm.Get_rank()==0) //sum only once: in the first rank of each walker
     {
       multi_sim_comm.Sum(av_dV_dAlpha_);
       multi_sim_comm.Sum(av_dV_dAlpha_prod_);
       multi_sim_comm.Sum(av_d2V_dAlpha2_);
       for(unsigned i=0; i<alpha_size_; i++)
-        av_dV_dAlpha_[i]/=walkers_num_;
+        av_dV_dAlpha_[i]/=NumWalkers_;
       for(unsigned ij=0; ij<sym_alpha_size_; ij++)
       {
-        av_dV_dAlpha_prod_[ij]/=walkers_num_;
-        av_d2V_dAlpha2_[ij]/=walkers_num_;
+        av_dV_dAlpha_prod_[ij]/=NumWalkers_;
+        av_d2V_dAlpha2_[ij]/=NumWalkers_;
       }
     }
-    if (comm.Get_size()>1)//if there are more ranks for each walker, everybody has to know
+    if(comm.Get_size()>1)//if there are more ranks for each walker, everybody has to know
     {
       comm.Bcast(av_dV_dAlpha_,0);
       comm.Bcast(av_dV_dAlpha_prod_,0);
@@ -631,7 +646,7 @@ void VesDeltaF::update_alpha()
 //calculate the increment and update alpha
   mean_counter_++;
   long unsigned mean_weight=mean_counter_;
-  if (mean_weight_tau_>0 && mean_weight_tau_<mean_counter_)
+  if(mean_weight_tau_>0 && mean_weight_tau_<mean_counter_)
     mean_weight=mean_weight_tau_;
   std::vector<double> damping(alpha_size_);
   for(unsigned i=0; i<alpha_size_; i++)
@@ -639,7 +654,7 @@ void VesDeltaF::update_alpha()
     double increment_i=grad_omega[i];
     for(unsigned j=0; j<alpha_size_; j++)
       increment_i+=hess_omega[get_index(i,j)]*(inst_alpha_[j]-mean_alpha_[j]);
-    if (!damping_off_)
+    if(!damping_off_)
       past_increment2_[i]+=increment_i*increment_i;
     damping[i]=std::sqrt(past_increment2_[i]);
     prev_exp_alpha_[i]=std::exp(-beta_*mean_alpha_[i]);
@@ -649,7 +664,7 @@ void VesDeltaF::update_alpha()
   }
 
 //update the Alpha file
-  if (mean_counter_%print_stride_==0)
+  if(mean_counter_%print_stride_==0)
   {
     alphaOfile_.printField("time",getTime());
     for(unsigned i=0; i<alpha_size_; i++)
@@ -666,7 +681,7 @@ void VesDeltaF::update_alpha()
 //mapping of a [alpha_size_]x[alpha_size_] symmetric matrix into a vector of size sym_alpha_size_, useful for the communicator
 inline unsigned VesDeltaF::get_index(const unsigned i, const unsigned j) const
 {
-  if (i<=j)
+  if(i<=j)
     return j+i*(alpha_size_-1)-i*(i-1)/2;
   else
     return get_index(j,i);
