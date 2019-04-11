054ad691f54fba8636fa917593122a2dc7360d98
Travis CI User <travis@example.org>
2019-04-11 12:44:16 +0000

    Plumedbot commit: headers
    This commit contains changes automatically generated by plumedbot on Travis-ci.

--- src/maze/Random_MT.h
+++ src/maze/Random_MT.h
@@ -6,8 +6,8 @@ See http://www.maze-code.github.io for more information.
 This file is part of maze.
 
 maze is free software: you can redistribute it and/or modify it under the
-terms of the GNU Lesser General Public License as published by the Free
-Software Foundation, either version 3 of the License, or (at your option)
+terms of the GNU Lesser General Public License as published by the Free 
+Software Foundation, either version 3 of the License, or (at your option) 
 any later version.
 
 maze is distributed in the hope that it will be useful, but WITHOUT ANY
@@ -16,10 +16,11 @@ FOR A PARTICULAR PURPOSE.
 
 See the GNU Lesser General Public License for more details.
 
-You should have received a copy of the GNU Lesser General Public License
+You should have received a copy of the GNU Lesser General Public License 
 along with maze. If not, see <https://www.gnu.org/licenses/>.
 +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++ */
-
+#ifndef __PLUMED_maze_Random_MT_h
+#define __PLUMED_maze_Random_MT_h
 /**
  * @file Random_MT.h
  *
@@ -199,3 +200,4 @@ inline void rnd::randomize() {
 } // namespace PLMD
 
 #endif // __PLUMED_maze_Random_MT_h
+#endif
