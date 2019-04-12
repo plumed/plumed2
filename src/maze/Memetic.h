246f295e91968349765db4d2a14ef0ab9bdef02a
Giovanni Bussi <giovanni.bussi@gmail.com>
2019-04-12 09:09:30 +0200

    astyle

--- src/maze/Memetic.h
+++ src/maze/Memetic.h
@@ -6,8 +6,8 @@ See http://www.maze-code.github.io for more information.
 This file is part of maze.
 
 maze is free software: you can redistribute it and/or modify it under the
-terms of the GNU Lesser General Public License as published by the Free 
-Software Foundation, either version 3 of the License, or (at your option) 
+terms of the GNU Lesser General Public License as published by the Free
+Software Foundation, either version 3 of the License, or (at your option)
 any later version.
 
 maze is distributed in the hope that it will be useful, but WITHOUT ANY
@@ -16,7 +16,7 @@ FOR A PARTICULAR PURPOSE.
 
 See the GNU Lesser General Public License for more details.
 
-You should have received a copy of the GNU Lesser General Public License 
+You should have received a copy of the GNU Lesser General Public License
 along with maze. If not, see <https://www.gnu.org/licenses/>.
 +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++ */
 #ifndef __PLUMED_maze_Memetic_h
