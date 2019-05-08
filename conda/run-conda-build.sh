338e01c75d058952af5e941b9b4279e14f31a676
Giovanni Bussi <giovanni.bussi@gmail.com>
2019-05-08 09:14:03 +0200

    test upload
    @tonigi

--- conda/run-conda-build.sh
+++ conda/run-conda-build.sh
@@ -26,24 +26,3 @@ conda-build recipe
 ls -l $CONDA_HOME/conda-bld/
 ls -l $CONDA_HOME/conda-bld/$TRAVIS_OS_NAME-64
 
-
-# And now upload if desired
-# conda upload $CONDA_HOME/conda-bld/linux-64/*.tar.bz2
-
-# https://gist.github.com/zshaheen/fe76d1507839ed6fbfbccef6b9c13ed9
-
-# https://conda.io/docs/user-guide/tasks/use-conda-with-travis-ci.html
-
-
-# One could play with this, but perhaps best not to spam the channel repository
-export VERSION=`date +%Y.%m.%d`
-
-# Disabled because it fails
-if [[ -n "$CONDA_UPLOAD_TOKEN" ]]; then
-    USER=plumed  # the conda channel
-    anaconda -t $CONDA_UPLOAD_TOKEN upload -u $USER -l testing \
-	     $CONDA_HOME/conda-bld/$TRAVIS_OS_NAME-64/plumed*.tar.bz2 --force
-fi
-
-
-
---
+++ conda/run-conda-upload.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+# Anywhere but outside of the repository
+export CONDA_HOME=/var/tmp/miniconda
+
+if [[ "$TRAVIS_OS_NAME" == "linux" ]]; then
+    csys=Linux
+elif [[ "$TRAVIS_OS_NAME" == "osx" ]]; then
+    csys=MacOSX
+else
+    echo "Unsupported system $TRAVIS_OS_NAME"
+    exit 1
+fi
+    
+export PATH="$CONDA_HOME/bin:$PATH"
+export VERSION=$TRAVIS_TAG
+
+# Disabled because it fails
+if [[ -n "$CONDA_UPLOAD_TOKEN" ]]; then
+    USER=plumed  # the conda channel
+    anaconda -t $CONDA_UPLOAD_TOKEN upload -u $USER -l testing \
+	     $CONDA_HOME/conda-bld/$TRAVIS_OS_NAME-64/plumed*.tar.bz2 --force
+fi
+
+
+
