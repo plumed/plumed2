6de1334595ea60cfac9cab2be08e176bce1b1ac4
Toni <toni.giorgino@gmail.com>
2019-05-08 12:24:15 +0200

    more xdrfile attempts

--- conda/recipe/build.sh
+++ conda/recipe/build.sh
@@ -2,26 +2,31 @@
 
 env | sort
 
-# GB: install xdrfile library
-if false; then
-    # TG - disabling because it fails to link for no rational cause
-    wget http://ftp.gromacs.org/pub/contrib/xdrfile-1.1.4.tar.gz
-    tar xzf xdrfile-1.1.4.tar.gz
-    cd xdrfile-1.1.4
-    ./configure --prefix=$PREFIX --enable-shared
-    make
-    make install
-    cd ../
+if [[ $target_platform == linux* ]]; then
+    condaldflags="$BUILD_PREFIX/lib/libz.a $BUILD_PREFIX/lib/libz.so.1 $BUILD_PREFIX/lib/libxdrfile.a $BUILD_PREFIX/lib/libxdrfile.so"
+elif [[ $target_platform == osx* ]]; then
+    # Possibly not needed.
+    condaldflags="$BUILD_PREFIX/lib/libz.a $BUILD_PREFIX/lib/libz.dylib $BUILD_PREFIX/lib/libxdrfile.a $BUILD_PREFIX/lib/libxdrfile.dylib"
+else
+    echo "Unexpected platform $target_platform"
 fi
 
-# TG: The "disabled" features are workaround for possible
-#     conda+configure bugs in library search: building is ok but
-#     linking with the .so doesn't find them (in
-#     conda-forge). Possibly the LD path needs tweaks.
 
-# TODO: re-enable them and see. Also to do: install docs?
 
-./configure --prefix=$PREFIX --enable-shared --disable-python --disable-zlib --disable-external-lapack --disable-external-blas 
+# GB: install xdrfile library
+wget http://ftp.gromacs.org/pub/contrib/xdrfile-1.1.4.tar.gz
+tar xzf xdrfile-1.1.4.tar.gz
+cd xdrfile-1.1.4
+./configure --prefix=$PREFIX --enable-shared
+make
+make install
+cd ../
+
+
+
+# Thanks to https://github.com/intbio/plumed-conda/blob/master/plumed2_v2.5.0/build.sh
+
+./configure --prefix=$PREFIX --enable-shared --disable-python --disable-external-lapack --disable-external-blas LDFLAGS="$condaldflags"
 make -j4
 make install
 
