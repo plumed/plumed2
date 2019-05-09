61355f3687047fbc78679c2bf883c86afc58195d
Giovanni Bussi <giovanni.bussi@gmail.com>
2019-05-09 16:30:06 +0200

    Enabled asmjit in conda for master branch

--- conda/recipe/build.sh
+++ conda/recipe/build.sh
@@ -17,18 +17,21 @@ fi
 
 # python wrapper is installed with pip
 # we temporarily use internal lapack/blas (should probably be fixed)
-# STATIC_LIBS is required on Linux for the following reason:
-# When using env modules the dependent libraries can be found through the
-# LD_LIBRARY_PATH or encoded configuring with -rpath.
-# Conda does not use LD_LIBRARY_PATH and it is thus necessary to suggest where libraries are.
 
 if test -n "$MACOSX_DEPLOYMENT_TARGET" ; then
   opt=""
 else
+# STATIC_LIBS is required on Linux for the following reason:
+# When using env modules the dependent libraries can be found through the
+# LD_LIBRARY_PATH or encoded configuring with -rpath.
+# Conda does not use LD_LIBRARY_PATH and it is thus necessary to suggest where libraries are.
   opt=STATIC_LIBS=-Wl,-rpath-link,$PREFIX/lib
+# Needed due to asmjit. See
+# See https://github.com/Bumblebee-Project/Bumblebee/issues/76
+  export LIBS="$LIBS -lrt"
 fi
 
-./configure --prefix=$PREFIX --enable-shared --disable-python --disable-external-lapack --disable-external-blas $opt
+./configure --prefix=$PREFIX --enable-shared --disable-python --disable-external-lapack --disable-external-blas --enable-asmjit $opt
 
 make -j4
 make install
