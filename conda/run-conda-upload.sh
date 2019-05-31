53fa3ca80dd8afe55eb5ace245699387b413398b
Giovanni Bussi <giovanni.bussi@gmail.com>
2019-05-31 20:01:09 +0200

    test

--- conda/run-conda-upload.sh
+++ conda/run-conda-upload.sh
@@ -5,12 +5,11 @@ export CONDA_HOME=/var/tmp/miniconda
 
 export PATH="$CONDA_HOME/bin:$PATH"
 
-# Disabled because it fails
 if [[ -n "$CONDA_UPLOAD_TOKEN" ]]; then
     USER=plumed  # the conda channel
-    anaconda -t $CONDA_UPLOAD_TOKEN upload -u $USER -l testing \
+    anaconda -t $CONDA_UPLOAD_TOKEN upload -u $USER -l $CONDA_LABEL \
 	     $CONDA_HOME/conda-bld/$TRAVIS_OS_NAME-64/plumed*.tar.bz2 --force
-    anaconda -t $CONDA_UPLOAD_TOKEN upload -u $USER -l testing \
+    anaconda -t $CONDA_UPLOAD_TOKEN upload -u $USER -l $CONDA_LABEL \
 	     $CONDA_HOME/conda-bld/$TRAVIS_OS_NAME-64/py-plumed*.tar.bz2 --force
 fi
 
