6ffc34c27da2b8b32a2edf27f6bcb36d01c69f79
Toni <toni.giorgino@gmail.com>
2019-08-07 14:47:48 +0200

    nn

--- src/pycv/scratch/mybind.cpp
+++ src/pycv/scratch/mybind.cpp
@@ -1,13 +1,17 @@
 #include <iostream>
 #include <pybind11/embed.h> // everything needed for embedding
+#include <pybind11/numpy.h>
+
 namespace py = pybind11;
 using namespace py::literals;
 
-int main() {
-    py::scoped_interpreter guard{}; // start the interpreter and keep it alive
+// https://pybind11.readthedocs.io/en/stable/advanced/embedding.html
+// https://pybind11.readthedocs.io/en/stable/advanced/pycpp/numpy.html
 
+py::scoped_interpreter guard{}; // start the interpreter and keep it alive
+
+int main1() {
     py::print("Hello, World!"); // use the Python API
-
 
     auto locals = py::dict("name"_a="World", "number"_a=42);
     py::exec(R"(
@@ -19,3 +23,22 @@ int main() {
 
 }
 
+int main2() {
+  const int N=3, M=5;
+  
+  py::array_t<float, py::array::c_style> arr({ N, M });
+  float * ptr= (float*) arr.request().ptr;
+  for(int i=0; i<N; i++) {
+    for(int j=0; j<M; j++) {
+      // arr.data(i,j) = i*100+j;
+      ptr[i*N+j]  = i*100+j;
+    }
+  }
+
+  
+}
+
+
+int main() {
+  main2();
+}
