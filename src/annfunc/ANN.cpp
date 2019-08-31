2cc09f6e5ff9475b85abc70dee7a15866582642e
weiHelloWorld <wei.herbert.chen@gmail.com>
2019-08-31 11:44:40 -0500

    fix include paths

--- src/annfunc/ANN.cpp
+++ src/annfunc/ANN.cpp
@@ -21,14 +21,14 @@ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.
 +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++ */
-#include "../function/Function.h"
-#include "../function/ActionRegister.h"
+#include "function/Function.h"
+#include "function/ActionRegister.h"
 #include "cassert"
 
 #include <string>
 #include <cmath>
 #include <iostream>
-#include <stdio.h>
+// #include <stdio.h>
 
 using namespace std;
 
