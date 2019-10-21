01abe0842ff1ce19682167e12f4a8700241052ec
Benjamin Pampel <pampel@mpip-mainz.mpg.de>
2019-10-21 16:44:45 +0200

    Implementation of optimizer
    Needed new vector multiplication operator for CoeffsVector

--- src/ves/Opt_Adam.cpp
+++ src/ves/Opt_Adam.cpp
@@ -24,6 +24,8 @@
 #include "CoeffsVector.h"
 
 #include "core/ActionRegister.h"
+#include "core/ActionSet.h"
+#include <memory>
 
 
 namespace PLMD {
@@ -44,11 +46,18 @@ private:
   double beta_1_;
   double beta_2_;
   double epsilon_;
+  // 1st moment uses the "AuxCoeffs", so only 2nd moment needs new coeff vectors
+  std::vector<std::unique_ptr<CoeffsVector>> var_coeffs_pntrs_;
+protected:
+  CoeffsVector& VarCoeffs(const unsigned int coeffs_id = 0) const;
 public:
   static void registerKeywords(Keywords&);
   explicit Opt_Adam(const ActionOptions&);
   void coeffsUpdate(const unsigned int c_id = 0);
 };
+
+inline
+CoeffsVector& Opt_Adam::VarCoeffs(const unsigned int coeffs_id) const {return *var_coeffs_pntrs_[coeffs_id];}
 
 
 PLUMED_REGISTER_ACTION(Opt_Adam,"OPT_ADAM")
@@ -76,20 +85,49 @@ Opt_Adam::Opt_Adam(const ActionOptions&ao):
   log.printf("  Adam type stochastic gradient decent\n");
 
   parse("BETA_1",beta_1_);
-  //if (beta_1_ != 0.9) {addKeywordToList("BETA_1",beta_1_);}
   parse("BETA_2",beta_1_);
   parse("EPSILON",epsilon_);
+
+  // set up the coeff vector for the 2nd moment (variance)
+  for (unsigned i = 0; i < numberOfCoeffsSets(); ++i)
+  {
+      std::unique_ptr<CoeffsVector> var_coeffs_tmp(new CoeffsVector(Coeffs(i)));
+      std::string var_label = Coeffs(i).getLabel();
+      if(var_label.find("coeffs")!=std::string::npos) {
+        var_label.replace(var_label.find("coeffs"), std::string("coeffs").length(), "var_coeffs");
+      }
+      else {
+        var_label += "_var";
+      }
+      var_coeffs_tmp->setLabels(var_label);
+      var_coeffs_pntrs_.push_back(var_coeffs_tmp);
+      AuxCoeffs(i).setValues(0.0);
+  }
 
   checkRead();
 }
 
 
 void Opt_Adam::coeffsUpdate(const unsigned int c_id) {
-  Coeffs(c_id) += - StepSize(c_id) * CoeffsMask(c_id) * Gradient(c_id);
-  //
-  double aver_decay = 1.0 / ( getIterationCounterDbl() + 1.0 );
-  AuxCoeffs(c_id) += aver_decay * ( Coeffs(c_id)-AuxCoeffs(c_id) );
+  time_++;
+  // AuxCoeffs is used for first moment (mean)
+  AuxCoeffs(c_id) *= beta_1_;
+  AuxCoeffs(c_id) += (1 - beta_1_ ) * Gradient(c_id) * CoeffsMask(c_id);
+  VarCoeffs(c_id) *= beta_2_;
+  VarCoeffs(c_id) += (1 - beta_2_ ) * Gradient(c_id) * Gradient(c_id) * CoeffsMask(c_id);
 
+  // store sqrt of VarCoeffs in vector, easier than writing a CoeffsVector::sqrt() function
+  // also can directly add epsilon and invert to multiply with the Coeffs in last step
+  std::vector<double> var_coeffs_sqrt;
+  for (size_t i = 0; i< VarCoeffs(c_id).getSize(); ++i) {
+    var_coeffs_sqrt.push_back(1 / (sqrt(VarCoeffs(c_id).getValue(i)) + epsilon));
+  }
+
+  // bias correction
+  double scalefactor = StepSize(c_id) * sqrt(1 - pow(beta_2_, time_));
+
+  // coeff update
+  Coeffs(c_id) -= scalefactor * AuxCoeffs(c_id) * var_coeffs_sqrt;
 }
 
 
