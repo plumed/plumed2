a7c020d583300dda9f2a886d1e118824b67ebbf7
Andrew White <white.d.andrew@gmail.com>
2020-07-16 09:03:58 -0400

    Fixed issue-601, segfault on histogram weights when file reading in driver

--- src/vesselbase/ActionWithAveraging.cpp
+++ src/vesselbase/ActionWithAveraging.cpp
@@ -82,7 +82,7 @@ ActionWithAveraging::ActionWithAveraging( const ActionOptions& ao ):
       ActionWithValue* val = plumed.getActionSet().selectWithLabel<ActionWithValue*>(wwstr[i]);
       if( !val ) error("could not find value named");
       bias::ReweightBase* iswham=dynamic_cast<bias::ReweightBase*>( val );
-      if( iswham->buildsWeightStore() ) error("to use wham you must gather data using COLLECT_FRAMES");
+      if( iswham && iswham->buildsWeightStore() ) error("to use wham you must gather data using COLLECT_FRAMES");
       weights.push_back( val->copyOutput(val->getLabel()) );
       arg.push_back( val->copyOutput(val->getLabel()) );
       log.printf("%s ",wwstr[i].c_str() );
