39627430b8565f80c4656799dfa7dbfeb014b298
Jan Domanski <jandom@gmail.com>
2020-10-22 10:01:41 +0100

    Making rt-make-angle more elaborate

--- regtest/tools/rt-make-angle/main.cpp
+++ regtest/tools/rt-make-angle/main.cpp
@@ -7,7 +7,7 @@
 using namespace PLMD;
 
 
-int testCompute90Degrees(std::ofstream& ofs)
+int testCompute2Vectors90Degrees(std::ofstream& ofs)
 {
   Vector vector1(1.0,0.0,0.0);
   Vector vector2(0.0,1.0,0.0);
@@ -17,7 +17,7 @@ int testCompute90Degrees(std::ofstream& ofs)
   return 0;
 }
 
-int testCompute45Degrees(std::ofstream& ofs)
+int testCompute2Vectors45Degrees(std::ofstream& ofs)
 {
   Vector vector1(1.0,0.0,0.0);
   Vector vector2(1.0,1.0,0.0);
@@ -27,13 +27,29 @@ int testCompute45Degrees(std::ofstream& ofs)
   return 0;
 }
 
+int testCompute4Vectors45Degrees(std::ofstream& ofs)
+{
+  Vector vector1(1.0,0.0,0.0);
+  Vector vector2(1.0,1.0,0.0);
+  Vector d1,d2; Angle angle;
+  double result = angle.compute(vector1, vector2, d1, d2);
+  ofs<<result<<"\n";
+  ofs<< d1[0] << " " << d1[1] << " " << d1[2] << "\n";
+  ofs<< d2[0] << " " << d2[1] << " " << d2[2] << "\n";
+  return 0;
+}
+
+
+
 int main(){
   Stopwatch sw;
   sw.start();
   std::ofstream ofs("output");
 
-  testCompute90Degrees(ofs);
-  testCompute45Degrees(ofs);
+  testCompute2Vectors90Degrees(ofs);
+  testCompute2Vectors45Degrees(ofs);
+
+  testCompute4Vectors45Degrees(ofs);
 
   sw.stop();
   std::cout<<sw;
