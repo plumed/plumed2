7efa12d7a6bcfe3f13f0b516e25b4be02433720b
Jan Domanski <jandom@gmail.com>
2020-10-25 21:42:36 +0000

    More tests for Grid.cpp

--- regtest/tools/rt-make-grid/main.cpp
+++ regtest/tools/rt-make-grid/main.cpp
@@ -6,8 +6,7 @@
 
 using namespace PLMD;
 
-int testCreate(std::ofstream &ofs) {
-  ofs << "testCreate" << std::endl;
+std::unique_ptr<GridBase> createGrid() {
   IFile gridfile;
   gridfile.open("./input-grid.data");
 
@@ -15,13 +14,43 @@ int testCreate(std::ofstream &ofs) {
   value->setNotPeriodic();
   const std::vector<Value *> grid_args_ = {value};
   std::unique_ptr<GridBase> gridBase = GridBase::create(
-      "external.bias", grid_args_, gridfile, false, false, false);
+      "external.bias", grid_args_, gridfile, false, false, false); 
+  return gridBase;
+}
+
+int testCreate(std::ofstream &ofs) {
+  ofs << "testCreate" << std::endl;
+  std::unique_ptr<GridBase> gridBase = createGrid();
   ofs << "dimensions= " << gridBase->getDimension() << std::endl;
+  return 0;
+}
+
+int testGetMax(std::ofstream &ofs) {
+  ofs << "testGetMax" << std::endl;
+  std::unique_ptr<GridBase> gridBase = createGrid();
+  std::vector<std::string> property = gridBase->getMax();
+  for (int i = 0; i < property.size(); ++i) {
+    ofs << i << " " << property[i] << ' ';
+  }
+  ofs << std::endl;
+  return 0;
+}
+
+int testGetMin(std::ofstream &ofs) {
+  ofs << "testGetMin" << std::endl;
+  std::unique_ptr<GridBase> gridBase = createGrid();
+  std::vector<std::string> property = gridBase->getMin();
+  for (int i = 0; i < property.size(); ++i) {
+    ofs << i << " " << property[i] << ' ';
+  }
+  ofs << std::endl;
   return 0;
 }
 
 int main() {
   std::ofstream ofs("output");
   testCreate(ofs);
+  testGetMax(ofs);
+  testGetMin(ofs);
   return 0;
 }
