b9cc303bcea45f668e24989239f558094711d06c
Giovanni Bussi <giovanni.bussi@gmail.com>
2020-10-28 20:24:16 +0100

    Added Tools::make_unique

--- src/tools/Tools.h
+++ src/tools/Tools.h
@@ -190,6 +190,12 @@ public:
     explicit DirectoryChanger(const char*path);
     ~DirectoryChanger();
   };
+/// Mimic C++14 std::make_unique
+  template<typename T, typename... Args>
+  static std::unique_ptr<T> make_unique(Args&&... args)
+  {
+    return std::unique_ptr<T>(new T(std::forward<Args>(args)...));
+  }
 };
 
 template <class T>
