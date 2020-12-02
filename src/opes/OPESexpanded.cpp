b9be16792d43d9c1445e45e2114a194e075d12a0
Michele Invernizzi <inve.michele@gmail.com>
2020-12-02 18:51:55 +0100

    more OpenMP from @PabloPiaggi

--- src/opes/OPESexpanded.cpp
+++ src/opes/OPESexpanded.cpp
@@ -679,10 +679,14 @@ inline void OPESexpanded::update_deltaF(double bias)
   plumed_dbg_massert(counter_>0,"deltaF_ must be initialized");
   counter_++;
   const double increment=kbt_*std::log1p(std::exp(static_cast<long double>((bias-rct_)/kbt_))/(counter_-1.));
-  for(unsigned i=0; i<deltaF_.size(); i++)
+  #pragma omp parallel num_threads(NumOMP_)
   {
-    const long double diff_i=static_cast<long double>(-getExpansion(i)+(bias-rct_+deltaF_[i])/kbt_);
-    deltaF_[i]+=increment-kbt_*std::log1p(std::exp(diff_i)/(counter_-1.));
+    #pragma omp for
+    for(unsigned i=0; i<deltaF_.size(); i++)
+    {
+      const long double diff_i=static_cast<long double>(-getExpansion(i)+(bias-rct_+deltaF_[i])/kbt_);
+      deltaF_[i]+=increment-kbt_*std::log1p(std::exp(diff_i)/(counter_-1.));
+    }
   }
   rct_+=increment+kbt_*std::log1p(-1./counter_);
 }
