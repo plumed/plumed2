74760cd0d921c99fd3620adc99e4dcdc8be05249
gibertif <federico.giberti@gmail.com>
2021-04-27 10:53:02 +0200

    Hack the tree (#648)
    * added smearing for the PMI in reco
    
    * Reco now works in orthonormalspace for eigv!=0
    
    * Modified version of ATLAS with WALL and correct weighing for the histogram
    
    * Created automatic output for Atlas
    
    * Fixed the evaluation of the von-mises distribution so that it doesn't blow up and evaluate the GMI correctly
    
    * updated kernel and product so that it works for gaussian and von mises
    
    * Added periodic boundary conditions for projections of CVs in ATLAS PCA space
    
    * Delete a comment
    
    * Fixed residual
    
    * Modified the pmis normalization for the bias.Still need changes for the adaptive wall
    
    * Final version of ATLAS
    
    * Fixed det label problem
    
    * removed unnecessary comments
    
    * Added doc for ATLAS
    
    Co-authored-by: Gareth Tribello <gareth.tribello@gmail.com>

--- src/function/Bessel.cpp
+++ src/function/Bessel.cpp
@@ -22,7 +22,7 @@
 #include "Function.h"
 #include "ActionRegister.h"
 #include "tools/SwitchingFunction.h"
-
+#include <array>
 #include <cmath>
 
 using namespace std;
@@ -44,6 +44,13 @@ class Bessel :
   public Function
 {
   unsigned order;
+private:
+  // Cheb coefficient for range [0,8]
+  static std::vector<double> A;
+  // Cheb coefficient for range (8,inf)
+  static std::vector<double> B;
+  double chbevl(double x,std::vector<double>& array) const; // sub copied from scipy in C
+  void fill_coefficients();
 public:
   explicit Bessel(const ActionOptions&);
   void calculateFunction( const std::vector<double>& args, MultiValue& myvals ) const ;
@@ -68,22 +75,107 @@ Bessel::Bessel(const ActionOptions&ao):
   }
   parse("ORDER",order); log.printf("  computing %dth order bessel function \n", order );
   if( order!=0 ) error("only zero order bessel function is implemented");
+  fill_coefficients();
   addValueWithDerivatives();
   checkRead();
 }
 
+double Bessel::chbevl(double x,std::vector<double>& array) const {
+    double b0, b1, b2;
+    int n = array.size();
+
+    b0 = array[0];
+    b1 = 0.0;
+    b2 = b1 ;
+
+    for(int index = 1 ; index < n ; index++){
+	    b2 = b1;
+	    b1 = b0;
+	    b0 = x * b1 - b2 + array[index];
+    }
+    return (0.5 * (b0 - b2));
+}
+
+
 void Bessel::calculateFunction( const std::vector<double>& args, MultiValue& myvals ) const {
   plumed_dbg_assert( args.size()==1 ); 
   if( order==0 ) {
-      if (fabs(args[0])<3.75) {
-        double y = Tools::fastpow( args[0]/3.75, 2 );
-        addValue( 0, 1 + y*(3.5156229 +y*(3.0899424 + y*(1.2067492+y*(0.2659732+y*(0.0360768+y*0.0045813))))), myvals );
+      double y;
+      double x = fabs(args[0]);
+      if (x <= 8.0) {
+      	y = (x / 2.0) - 2.0;
+	double res =  chbevl(y, A) ;
+        addValue( 0, res ,  myvals );
         return;
       }
-      double ax=fabs(args[0]), y=3.75/ax, bx=std::exp(ax)/sqrt(ax);
-      ax=0.39894228+y*(0.01328592+y*(0.00225319+y*(-0.00157565+y*(0.00916281+y*(-0.02057706+y*(0.02635537+y*(-0.01647633+y*0.00392377)))))));
-      addValue( 0, ax*bx, myvals ); 
+      double res = chbevl(32.0 / x - 2.0, B) / sqrt(x) ;
+      addValue( 0, res , myvals ); 
   } else plumed_error();
+}
+
+std::vector<double> Bessel::A;
+std::vector<double> Bessel::B;
+
+void Bessel::fill_coefficients(){
+	A.resize(30);
+  A = {-4.41534164647933937950E-18,
+             3.33079451882223809783E-17,
+             -2.43127984654795469359E-16,
+             1.71539128555513303061E-15,
+             -1.16853328779934516808E-14,
+             7.67618549860493561688E-14,
+             -4.85644678311192946090E-13,
+             2.95505266312963983461E-12,
+             -1.72682629144155570723E-11,
+             9.67580903537323691224E-11,
+             -5.18979560163526290666E-10,
+             2.65982372468238665035E-9,
+             -1.30002500998624804212E-8,
+             6.04699502254191894932E-8,
+             -2.67079385394061173391E-7,
+             1.11738753912010371815E-6,
+             -4.41673835845875056359E-6,
+             1.64484480707288970893E-5,
+             -5.75419501008210370398E-5,
+             1.88502885095841655729E-4,
+             -5.76375574538582365885E-4,
+             1.63947561694133579842E-3,
+             -4.32430999505057594430E-3,
+             1.05464603945949983183E-2,
+             -2.37374148058994688156E-2,
+             4.93052842396707084878E-2,
+             -9.49010970480476444210E-2,
+             1.71620901522208775349E-1,
+             -3.04682672343198398683E-1,
+             6.76795274409476084995E-1};
+  B.resize(25);
+  B = {-7.23318048787475395456E-18,
+              -4.83050448594418207126E-18,
+              4.46562142029675999901E-17,
+              3.46122286769746109310E-17,
+              -2.82762398051658348494E-16,
+              -3.42548561967721913462E-16,
+              1.77256013305652638360E-15,
+              3.81168066935262242075E-15,
+              -9.55484669882830764870E-15,
+              -4.15056934728722208663E-14,
+              1.54008621752140982691E-14,
+              3.85277838274214270114E-13,
+              7.18012445138366623367E-13,
+              -1.79417853150680611778E-12,
+              -1.32158118404477131188E-11,
+              -3.14991652796324136454E-11,
+              1.18891471078464383424E-11,
+              4.94060238822496958910E-10,
+              3.39623202570838634515E-9,
+              2.26666899049817806459E-8,
+              2.04891858946906374183E-7,
+              2.89137052083475648297E-6,
+              6.88975834691682398426E-5,
+              3.36911647825569408990E-3,
+              8.04490411014108831608E-1};
+
+
 }
 
 }
---
+++ src/reference/core/ExchangePatterns.h
@@ -0,0 +1,1 @@
+#include "../../core/ExchangePatterns.h"
