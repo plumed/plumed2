66c59245d7096cfe44bb39164cdd81efe0e4ea1b
Giovanni Bussi <giovanni.bussi@gmail.com>
2021-06-30 18:51:19 +0200

    Added plumed_installed

--- regtest/basic/rt-make-fortran08/main.f90
+++ regtest/basic/rt-make-fortran08/main.f90
@@ -2,6 +2,9 @@ PROGRAM main
   USE PLUMED_MODULE_F08
   IMPLICIT NONE
   TYPE(PLUMED) :: p
+  if(.not. plumed_installed()) then
+    stop "plumed not installed"
+  endif
   CALL TEST3()
   CALL TEST4()
   call p%cmd("init")
