5c1ae8daa6334f8e6b633c92421799e0bd4e4dc8
Gareth Tribello <gareth.tribello@gmail.com>
2021-10-19 18:30:10 +0100

    Made dipole a multicolvar so we can do water structure coordinates

--- src/multicolvar/Dipole.cpp
+++ src/multicolvar/Dipole.cpp
@@ -1,5 +1,5 @@
 /* +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
-   Copyright (c) 2012-2020 The plumed team
+   Copyright (c) 2011-2020 The plumed team
    (see the PEOPLE file at the root of the distribution for a list of names)
 
    See http://www.plumed.org for more information.
@@ -19,15 +19,16 @@
    You should have received a copy of the GNU Lesser General Public License
    along with plumed.  If not, see <http://www.gnu.org/licenses/>.
 +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++ */
-#include "Colvar.h"
-#include "ActionRegister.h"
+#include "MultiColvarBase.h"
+#include "core/ActionRegister.h"
+#include "tools/Pbc.h"
 
 namespace PLMD {
-namespace colvar {
+namespace multicolvar {
 
-//+PLUMEDOC COLVAR DIPOLE
+//+PLUMEDOC COLVAR DIPOLE 
 /*
-Calculate the dipole moment for a group of atoms.
+Calculate the dipole moments for groups of atoms.
 
 When running with periodic boundary conditions, the atoms should be
 in the proper periodic image. This is done automatically since PLUMED 2.5,
@@ -48,7 +49,7 @@ the atoms from 1-10 and print it every 5 steps
 d: DIPOLE GROUP=1-10
 PRINT FILE=output STRIDE=5 ARG=d
 \endplumedfile
-
+  
 \attention
 If the total charge Q of the group in non zero, then a charge Q/N will be subtracted to every atom,
 where N is the number of atoms. This implies that the dipole (which for a charged system depends
@@ -58,101 +59,79 @@ on the position) is computed on the geometric center of the group.
 */
 //+ENDPLUMEDOC
 
-class Dipole : public Colvar {
-  std::vector<AtomNumber> ga_lista;
+class Dipole : public MultiColvarBase {
+private:
   bool components;
-  bool nopbc;
 public:
+  static void registerKeywords( Keywords& keys );
   explicit Dipole(const ActionOptions&);
-  void calculate() override;
-  static void registerKeywords(Keywords& keys);
+// active methods:
+  void compute( const std::vector<Vector>& pos, MultiValue& myvals ) const override;
 };
 
 PLUMED_REGISTER_ACTION(Dipole,"DIPOLE")
 
-void Dipole::registerKeywords(Keywords& keys) {
-  Colvar::registerKeywords(keys);
-  keys.add("atoms","GROUP","the group of atoms we are calculating the dipole moment for");
-  keys.addFlag("COMPONENTS",false,"calculate the x, y and z components of the dipole separately and store them as label.x, label.y and label.z");
-  keys.addOutputComponent("x","COMPONENTS","the x-component of the dipole");
-  keys.addOutputComponent("y","COMPONENTS","the y-component of the dipole");
-  keys.addOutputComponent("z","COMPONENTS","the z-component of the dipole");
+void Dipole::registerKeywords( Keywords& keys ) {
+  MultiColvarBase::registerKeywords( keys );
+  keys.addFlag("COMPONENTS",false,"calculate the x, y and z components of the distance separately and store them as label.x, label.y and label.z");
+  keys.addOutputComponent("x","COMPONENTS","the x-components of the dipoles");
+  keys.addOutputComponent("y","COMPONENTS","the y-components of the dipoles");
+  keys.addOutputComponent("z","COMPONENTS","the z-components of the dipoles");
 }
 
 Dipole::Dipole(const ActionOptions&ao):
-  PLUMED_COLVAR_INIT(ao),
+  Action(ao),
+  MultiColvarBase(ao),
   components(false)
 {
-  parseAtomList("GROUP",ga_lista);
   parseFlag("COMPONENTS",components);
-  parseFlag("NOPBC",nopbc);
   checkRead();
+
   if(components) {
     addComponentWithDerivatives("x"); componentIsNotPeriodic("x");
     addComponentWithDerivatives("y"); componentIsNotPeriodic("y");
     addComponentWithDerivatives("z"); componentIsNotPeriodic("z");
+    log<<"  WARNING: components will not have the proper periodicity - see manual\n";
   } else {
     addValueWithDerivatives(); setNotPeriodic();
   }
-
-  log.printf("  of %u atoms\n",static_cast<unsigned>(ga_lista.size()));
-  for(unsigned int i=0; i<ga_lista.size(); ++i) {
-    log.printf("  %d", ga_lista[i].serial());
-  }
-  log.printf("  \n");
-  if(nopbc) log.printf("  without periodic boundary conditions\n");
-  else      log.printf("  using periodic boundary conditions\n");
-
-  requestAtoms(ga_lista);
 }
 
+
 // calculator
-void Dipole::calculate()
-{
-  if(!nopbc) makeWhole();
-  double ctot=0.;
-  unsigned N=getNumberOfAtoms();
+void Dipole::compute( const std::vector<Vector>& pos, MultiValue& myvals ) const {
+  double ctot=0.; unsigned N=pos.size();
   std::vector<double> charges(N);
   Vector dipje;
-
+  
   for(unsigned i=0; i<N; ++i) {
-    charges[i]=getCharge(i);
-    ctot+=charges[i];
+      charges[i]=getCharge(i); ctot+=charges[i];
   }
   ctot/=(double)N;
 
   for(unsigned i=0; i<N; ++i) {
-    charges[i]-=ctot;
-    dipje += charges[i]*getPosition(i);
+      charges[i]-=ctot; dipje += charges[i]*pos[i];
   }
 
   if(!components) {
-    double dipole = dipje.modulo();
-    double idip = 1./dipole;
-
-    for(unsigned i=0; i<N; i++) {
-      double dfunc=charges[i]*idip;
-      setAtomsDerivatives(i,dfunc*dipje);
-    }
-    setBoxDerivativesNoPbc();
-    setValue(dipole);
+    double dipole = dipje.modulo(), idip = 1./dipole;
+    for(unsigned i=0; i<N; i++) addAtomsDerivatives(0, i, charges[i]*idip*dipje, myvals );
+    setBoxDerivativesNoPbc( 0, pos, myvals );
+    setValue(0, dipole, myvals );
   } else {
-    Value* valuex=getPntrToComponent("x");
-    Value* valuey=getPntrToComponent("y");
-    Value* valuez=getPntrToComponent("z");
     for(unsigned i=0; i<N; i++) {
-      setAtomsDerivatives(valuex,i,charges[i]*Vector(1.0,0.0,0.0));
-      setAtomsDerivatives(valuey,i,charges[i]*Vector(0.0,1.0,0.0));
-      setAtomsDerivatives(valuez,i,charges[i]*Vector(0.0,0.0,1.0));
+        addAtomsDerivatives(0, i, charges[i]*Vector(1.0,0.0,0.0), myvals );
+        addAtomsDerivatives(1, i, charges[i]*Vector(0.0,1.0,0.0), myvals );
+        addAtomsDerivatives(2, i, charges[i]*Vector(0.0,0.0,1.0), myvals );
     }
-    setBoxDerivativesNoPbc(valuex);
-    setBoxDerivativesNoPbc(valuey);
-    setBoxDerivativesNoPbc(valuez);
-    valuex->set(dipje[0]);
-    valuey->set(dipje[1]);
-    valuez->set(dipje[2]);
+    for(unsigned i=0;i<3;++i) {
+        setValue(i, dipje[i], myvals); setBoxDerivativesNoPbc( i, pos, myvals );
+    }
   }
 }
 
 }
 }
+
+
+
