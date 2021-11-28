2d4c9347a8be1107ccbd451855a51e283de0b1a5
Omar Valsson <omar.valsson@gmail.com>
2021-11-28 22:47:14 +0100

    update header

--- src/colvar/S2ContactModel.cpp
+++ src/colvar/S2ContactModel.cpp
@@ -1,23 +1,23 @@
 /* +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
-   Copyright (c) 2016-2017 The ves-code team
-   (see the PEOPLE-VES file at the root of this folder for a list of names)
+   Copyright (c) 2021 The plumed team
+   (see the PEOPLE file at the root of the distribution for a list of names)
 
-   See http://www.ves-code.org for more information.
+   See http://www.plumed.org for more information.
 
-   This file is part of ves-code module.
+   This file is part of plumed, version 2.
 
-   The ves-code module is free software: you can redistribute it and/or modify
+   plumed is free software: you can redistribute it and/or modify
    it under the terms of the GNU Lesser General Public License as published by
    the Free Software Foundation, either version 3 of the License, or
    (at your option) any later version.
 
-   The ves-code module is distributed in the hope that it will be useful,
+   plumed is distributed in the hope that it will be useful,
    but WITHOUT ANY WARRANTY; without even the implied warranty of
    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
    GNU Lesser General Public License for more details.
 
    You should have received a copy of the GNU Lesser General Public License
-   along with the ves-code module.  If not, see <http://www.gnu.org/licenses/>.
+   along with plumed.  If not, see <http://www.gnu.org/licenses/>.
 +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++ */
 
 #include "Colvar.h"
