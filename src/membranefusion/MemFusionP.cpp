4545597ef22b5d4e45f760d13d67ad4c5de2cb9b
Ary Lautaro Di Bartolo <lautaro@Arys-MacBook-Air.local>
2022-03-16 14:06:33 -0300

    Removing repeated files

--- src/membranefusion/MemFusionP.cpp
+++ src/membranefusion/MemFusionP.cpp
@@ -19,8 +19,8 @@
    You should have received a copy of the GNU Lesser General Public License
    along with plumed.  If not, see <http://www.gnu.org/licenses/>.
 +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++ */
-#include "Colvar.h"
-#include "ActionRegister.h"
+#include "colvar/Colvar.h"
+#include "core/ActionRegister.h"
 #include <cmath>
 #include <omp.h>
 
