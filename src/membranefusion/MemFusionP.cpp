ad0ac9a39340176771975953084732cbff4b8813
Giovanni Bussi <giovanni.bussi@gmail.com>
2022-05-01 22:22:48 +0200

    updated headers
    cc: @lautarodibartolo
    
    (nothing special to do, I just updated the headers)

--- src/membranefusion/MemFusionP.cpp
+++ src/membranefusion/MemFusionP.cpp
@@ -1,23 +1,22 @@
 /* +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
-   Copyright (c) 2017-2022 The plumed team
-   (see the PEOPLE file at the root of the distribution for a list of names)
+Copyright (c) 2022.
 
-   See http://www.plumed.org for more information.
+CVs originally developed by the Jochen Hub group from the University of Saarland (Germany)
+and adapted and implemented in PLUMED by Ary Lautaro Di Bartolo and Diego Masone from the 
+National University of Cuyo (Argentina).
 
-   This file is part of plumed, version 2.
+The membranefusion module is free software: you can redistribute it and/or modify
+it under the terms of the GNU Lesser General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
 
-   plumed is free software: you can redistribute it and/or modify
-   it under the terms of the GNU Lesser General Public License as published by
-   the Free Software Foundation, either version 3 of the License, or
-   (at your option) any later version.
+The membranefusion module is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU Lesser General Public License for more details.
 
-   plumed is distributed in the hope that it will be useful,
-   but WITHOUT ANY WARRANTY; without even the implied warranty of
-   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-   GNU Lesser General Public License for more details.
-
-   You should have received a copy of the GNU Lesser General Public License
-   along with plumed.  If not, see <http://www.gnu.org/licenses/>.
+You should have received a copy of the GNU Lesser General Public License
+along with plumed.  If not, see <http://www.gnu.org/licenses/>.
 +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++ */
 #include "colvar/Colvar.h"
 #include "core/ActionRegister.h"
