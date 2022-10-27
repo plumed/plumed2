plumed/plumed2

commit ae6ab5d367697d79f25f152ff7a87eb286857457

Added a warning when a plumed object has not been finalized correctly

src/core/PlumedMain.cpp
@@ -53,6 +53,8 @@
 #include <ios>
 #include <new>
 #include <typeinfo>
+#include <iostream>
+#include <algorithm>
 #ifdef __PLUMED_LIBCXX11
 #include <system_error>
 #include <future>
@@ -121,6 +123,30 @@ namespace PLMD {
   plumed_error() << "unknown exception " << what;
 }
 
+namespace {
+class Register {
+  std::vector<PlumedMain*> instances;
+public:
+  void add(PlumedMain* instance) {
+    instances.push_back(instance);
+  }
+  void remove(PlumedMain* instance) {
+    auto it = std::find(instances.begin(), instances.end(), instance);
+    if(it==instances.end()) {
+      std::cerr<<"WARNING: internal inconsistency in allocated PlumedMain instances\n";
+    } else {
+      instances.erase(it);
+    }
+  }
+  ~Register() {
+    if(instances.size()>0) std::cerr<<"PLUMED instances was not properly deallocated in your code: "<<instances.size()<<"\n";
+  }
+};
+
+static Register myregister;
+
+}
+
 PlumedMain::PlumedMain():
   initialized(false),
 // automatically write on log in destructor
@@ -142,10 +168,12 @@ PlumedMain::PlumedMain():
 {
   log.link(comm);
   log.setLinePrefix("PLUMED: ");
+  myregister.add(this);
 }
 
 // destructor needed to delete forward declarated objects
 PlumedMain::~PlumedMain() {
+  myregister.remove(this);
 }
 
 /////////////////////////////////////////////////////////////

commit 05476569fd97210138e9bd1777c885efc9e35f92

Fix in multithread calculations.
This fixes a bug introduced in ae6ab5d367697d79f25f152ff7a87eb286857457

Specifially, when running multiple threads with one Plumed instance
in each thread, the list gets corrupted.

src/core/PlumedMain.cpp
@@ -123,6 +123,9 @@ namespace PLMD {
   plumed_error() << "unknown exception " << what;
 }
 
+#if 0
+// this is temporarily commented out since it makes
+// multithread calculations fail
 namespace {
 class Register {
   std::vector<PlumedMain*> instances;
@@ -146,6 +149,7 @@ public:
 static Register myregister;
 
 }
+#endif
 
 PlumedMain::PlumedMain():
   initialized(false),
@@ -168,12 +172,12 @@ PlumedMain::PlumedMain():
 {
   log.link(comm);
   log.setLinePrefix("PLUMED: ");
-  myregister.add(this);
+  //myregister.add(this);
 }
 
 // destructor needed to delete forward declarated objects
 PlumedMain::~PlumedMain() {
-  myregister.remove(this);
+  //myregister.remove(this);
 }
 
 /////////////////////////////////////////////////////////////