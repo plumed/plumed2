e7a1a39a5884c1ee543d2410fd0711d8d638dd6b
Giovanni Bussi <giovanni.bussi@gmail.com>
2022-10-29 10:37:24 +0200

    better implementation of regtest

--- regtest/basic/rt-make-threads/main.cpp
+++ regtest/basic/rt-make-threads/main.cpp
@@ -1,90 +1,37 @@
 #include "plumed/wrapper/Plumed.h"
 #include <fstream>
 #include <thread>
+#include <functional>
+#include <vector>
 
 using namespace PLMD;
+
+void run(std::ostream & os, const std::string & name,std::function<void(void)> f,unsigned nthreads=4,unsigned nrepeats=10){
+  os<<"Test "<<name<<" with OpenMP...\n";
+  {
+    for(unsigned i=0;i<nrepeats;i++) {
+#pragma omp parallel num_threads(nthreads)
+      f();
+    }
+  }
+  os<<"OK\n";
+  os<<"Test "<<name<<" with C++11 threads...\n";
+  for(unsigned i=0;i<nrepeats;i++) {
+    std::vector<std::thread> threads;
+    for(unsigned j=0;j<nthreads;j++) threads.emplace_back(std::thread(f));
+    for(unsigned j=0;j<nthreads;j++) threads[j].join();
+  }
+  os<<"OK\n";
+}
 
 int main(){
 
   std::ofstream ofs("log");
 
-// test if constructor/destructor are thread safe
-  {
-    ofs<<"Testing if constructor and destructor are thread safe with OpenMP ..."<<std::endl;
-    unsigned nouter=10;
-    unsigned ninner=10000;
-    for(unsigned i=0;i<nouter;i++) {
-#pragma omp parallel
-      for(unsigned j=0;j<ninner;j++) PLMD::Plumed p;
-    }
-    ofs<<"OK"<<std::endl;
-  }
+  run(ofs,"constructor and destructor",[&](){for(unsigned j=0;j<10000;j++) PLMD::Plumed q;});
 
-// test if reference counter is thread safe
-  {
-    ofs<<"Testing if reference counter is thread safe with OpenMP ..."<<std::endl;
-    unsigned nouter=10;
-    unsigned ninner=100000;
-    PLMD::Plumed p;
-    for(unsigned i=0;i<nouter;i++) {
-#pragma omp parallel
-      for(unsigned j=0;j<ninner;j++) PLMD::Plumed q=p;
-    }
-    ofs<<"OK"<<std::endl;
-  }
-
-/// test if constructor/destructor are thread safe
-  {
-    ofs<<"Testing if constructor and destructor are thread safe with C++11 threads ..."<<std::endl;
-    unsigned nouter=10;
-    unsigned ninner=10000;
-    for(unsigned i=0;i<nouter;i++) {
-      std::thread thread1([&](){for(unsigned j=0;j<ninner;j++) PLMD::Plumed p;});
-      std::thread thread2([&](){for(unsigned j=0;j<ninner;j++) PLMD::Plumed p;});
-      std::thread thread3([&](){for(unsigned j=0;j<ninner;j++) PLMD::Plumed p;});
-      std::thread thread4([&](){for(unsigned j=0;j<ninner;j++) PLMD::Plumed p;});
-      std::thread thread5([&](){for(unsigned j=0;j<ninner;j++) PLMD::Plumed p;});
-      std::thread thread6([&](){for(unsigned j=0;j<ninner;j++) PLMD::Plumed p;});
-      std::thread thread7([&](){for(unsigned j=0;j<ninner;j++) PLMD::Plumed p;});
-      std::thread thread8([&](){for(unsigned j=0;j<ninner;j++) PLMD::Plumed p;});
-      thread1.join();
-      thread2.join();
-      thread3.join();
-      thread4.join();
-      thread5.join();
-      thread6.join();
-      thread7.join();
-      thread8.join();
-    }
-    ofs<<"OK"<<std::endl;
-  }
-
-// test if reference counter is thread safe
-  {
-    ofs<<"Testing if reference counter is thread safe with C++11 threads ..."<<std::endl;
-    unsigned nouter=10;
-    unsigned ninner=100000;
-    PLMD::Plumed p;
-    for(unsigned i=0;i<nouter;i++) {
-      std::thread thread1([&](){for(unsigned j=0;j<ninner;j++) PLMD::Plumed q=p;});
-      std::thread thread2([&](){for(unsigned j=0;j<ninner;j++) PLMD::Plumed q=p;});
-      std::thread thread3([&](){for(unsigned j=0;j<ninner;j++) PLMD::Plumed q=p;});
-      std::thread thread4([&](){for(unsigned j=0;j<ninner;j++) PLMD::Plumed q=p;});
-      std::thread thread5([&](){for(unsigned j=0;j<ninner;j++) PLMD::Plumed q=p;});
-      std::thread thread6([&](){for(unsigned j=0;j<ninner;j++) PLMD::Plumed q=p;});
-      std::thread thread7([&](){for(unsigned j=0;j<ninner;j++) PLMD::Plumed q=p;});
-      std::thread thread8([&](){for(unsigned j=0;j<ninner;j++) PLMD::Plumed q=p;});
-      thread1.join();
-      thread2.join();
-      thread3.join();
-      thread4.join();
-      thread5.join();
-      thread6.join();
-      thread7.join();
-      thread8.join();
-    }
-    ofs<<"OK"<<std::endl;
-  }
+  PLMD::Plumed p;
+  run(ofs,"reference counter",[&](){for(unsigned j=0;j<100000;j++) PLMD::Plumed q=p;});
 
   return 0;
 }
