e6e1f32c789b26d7c1f2bb8ee997a6490753421e
Giovanni Bussi <giovanni.bussi@gmail.com>
2023-02-21 21:30:14 +0100

    cppcheck fix

--- src/multicolvar/VolumeTetrapore.cpp
+++ src/multicolvar/VolumeTetrapore.cpp
@@ -327,7 +327,7 @@ void VolumeTetrapore::setupRegions() {
     perp=-perp; len_perp=-len_perp;
     for(unsigned i=0; i<3; ++i) dperp[i]*=-1.0;
   }
-  if( len_bi<=0 || len_cross<=0 || len_bi<=0 ) plumed_merror("Invalid box coordinates");
+  if( len_bi<=0 || len_cross<=0 || len_perp<=0 ) plumed_merror("Invalid box coordinates");
 
   // Now derivatives of lengths
   Tensor dd3( Tensor::identity() ); Vector ddb2=d1; if( lbi==2 ) ddb2=d2;
