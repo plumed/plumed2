ba30aa1a034228307c6e839cd8f696e1531dfa13
Daniele Rapetti <daniele.rapetti@sissa.it>
2023-04-27 16:44:22 +0200

    clearer comments in the tests

--- unittest/testWALKERS_MPI.cpp
+++ unittest/testWALKERS_MPI.cpp
@@ -3,6 +3,9 @@
 #include "plumed/wrapper/Plumed.h"
 #include "plumed/tools/Communicator.h"
 
+//When the user aks for a WALKERS_MPI in the METAD action, 
+//if MPI is not installed then the user must be informed
+//if MPI is installed then the comunications must be already set up
 
 int main(int, char**){
     //GIVEN an initialized Plumed interface 
@@ -21,19 +24,19 @@ int main(int, char**){
     plumed.cmd("init");
     plumed.cmd("readInputLine","d: DISTANCE ATOMS=1,2");
     plumed.cmd("readInputLine","d1: DISTANCE ATOMS={1 2}");
-
-    //WHEN the user give ask for a METAD with WALKERS_MPI keyword in it
-    
+   
     const std::string mokedLine=
     "METAD ARG=d,d1 SIGMA=0.1,0.2 HEIGHT=0.1 PACE=2 RESTART=YES WALKERS_MPI";
     
-    //THEN plumed should gracefully exit with a clear error message    
+    //WHEN PLUMED is not compiled with MPI or the MPI routines are not initialized
     std::string expectedMessage="WALKERS_MPI flag requires MPI compilation";
     if (PLMD::Communicator::PlumedHasMPI){
         expectedMessage="WALKERS_MPI needs the communicator correctly initialized";
     }
     try{
+        //WHEN the user give ask for a METAD with WALKERS_MPI keyword in it
         plumed.cmd("readInputLine",mokedLine.c_str());
+        //THEN plumed should gracefully exit with a clear error message 
     } catch(PLMD::Plumed::ExceptionError &e){//correct throw, we are happy
         std::string exceptionText{e.what()};
         
