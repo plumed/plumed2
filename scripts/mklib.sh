cd99f71d7040e6a0de38a8a8dbf56e6ee4fa1f3a
Daniele Rapetti <daniele.rapetti@sissa.it>
2023-06-08 12:51:52 +0200

    mklib refined with mktemp

--- scripts/mklib.sh
+++ scripts/mklib.sh
@@ -29,28 +29,28 @@ then
   echo "ERROR: I cannot find file $file"
   exit 1
 fi
+#adding a simple tmpfile, to preprocess "in place" the input file,
+#this assumes the user has write permission in the current directory
+#which should be true since we are going to compile and output something here
+tmpfile=$(mktemp ${file%.cpp}.XXXXXX.cpp)
+cp "${file}" "${tmpfile}"
 
-if grep -q '^#include "\(bias\|colvar\|function\|sasa\|vatom\)\/ActionRegister.h"' "$file"; then
+if grep -q '^#include "\(bias\|colvar\|function\|sasa\|vatom\)\/ActionRegister.h"' "${tmpfile}"; then
    >&2 echo 'WARNING: using a legacy ActionRegister.h include path, please use <<#include "core/ActionRegister.h">>'
-   sed 's%^#include ".*/ActionRegister.h"%#include "core/ActionRegister.h"%g' < "$file" > "tmp_${file}"
-   file="tmp_${file}"
+   sed 's%^#include ".*/ActionRegister.h"%#include "core/ActionRegister.h"%g' -i "${tmpfile}"
 fi
 
-if grep -q '^#include "\(cltools\)\/CLToolRegister.h"' "$file"; then
+if grep -q '^#include "\(cltools\)\/CLToolRegister.h"' "${tmpfile}"; then
    >&2  echo 'WARNING: using a legacy  CLToolRegister.h include path, please use <<#include "core/CLToolRegister.h">>'
-   sed 's%^#include ".*/CLToolRegister.h"%#include "core/CLToolRegister.h"%g' < "$file" > "tmp_${file}"
-   file="tmp_${file}"
+   sed 's%^#include ".*/CLToolRegister.h"%#include "core/CLToolRegister.h"%g' -i "${tmpfile}"
 fi
 
 rm -f "$obj" "$lib"
 
-echo "$compile" "$obj" "$file"
-echo "$link_installed" "$lib" "$obj"
 if test "$PLUMED_IS_INSTALLED" = yes ; then
-  eval "$compile" "$obj" "$file" && eval "$link_installed" "$lib" "$obj"
+  eval "$compile" "$obj" "$tmpfile" && eval "$link_installed" "$lib" "$obj"
 else
-  eval "$compile" "$obj" "$file" && eval "$link_uninstalled" "$lib" "$obj"
+  eval "$compile" "$obj" "$tmpfile" && eval "$link_uninstalled" "$lib" "$obj"
 fi
 
-
-
+rm -f ${tmpfile}
