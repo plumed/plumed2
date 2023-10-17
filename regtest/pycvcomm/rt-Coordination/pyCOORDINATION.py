5201992a9a1c7cc1f9cc070e97cef6595d1ae1a8
Daniele Rapetti <daniele.rapetti@sissa.it>
2023-10-17 14:25:02 +0200

    the COORDINATIOn works  (but no virial and devs)

--- regtest/pycvcomm/rt-Coordination/pyCOORDINATION.py
+++ regtest/pycvcomm/rt-Coordination/pyCOORDINATION.py
@@ -12,40 +12,44 @@ from sys import stderr as log
 # import plumedUtilities
 print("Imported pyCoord.", file=log)
 
-N:int=6
-M:int=12
-D0:float=0.0
-R0:float=2.0
-INVR0:float=1.0/R0
-DMAX=D0+R0*(0.00001**(1./(N-M)))
-STRETCH=1.0
-SHIFT=0.0
+N: int = 6
+M: int = 12
+D0: float = 0.0
+R0: float = 2.0
+INVR0: float = 1.0 / R0
+DMAX = D0 + R0 * (0.00001 ** (1.0 / (N - M)))
+STRETCH = 1.0
+SHIFT = 0.0
+
+
 def switch(d: np.ndarray) -> np.ndarray:
-    ret=np.zeros_like(d)
-    WhereToCalc=d<=DMAX
-    rdist=d[WhereToCalc]*INVR0
-    rNdist=(rdist)**(N-1)
-    ret[WhereToCalc]=(1.0/(1+rdist*rNdist))*STRETCH
-    ret[WhereToCalc]+=SHIFT
-    
-    return ret
+    ret = np.zeros_like(d)
+    dfunc = np.zeros_like(d)
+    WhereToCalc = d <= DMAX
+    rdist = d[WhereToCalc] * INVR0
+    rNdist = (rdist) ** (N - 1)
+    ret[WhereToCalc] = (1.0 / (1 + rdist * rNdist)) * STRETCH
+    ret[WhereToCalc] += SHIFT
+    dfunc[WhereToCalc] = -N * rNdist * ret[WhereToCalc] * ret[WhereToCalc]
+    dfunc *= STRETCH * INVR0
+    return ret, dfunc
+
 
 def stretchSwitch(mydmax):
-    s=switch(np.array([0.0,mydmax]))
-    # DMAX=tmp
-    print(s, file=log)
-    stretch=1/(s[0]-s[1])
-    return stretch,-s[1]*stretch
+    s, _ = switch(np.array([0.0, mydmax]))
+    stretch = 1 / (s[0] - s[1])
+    return stretch, -s[1] * stretch
 
-mydmax=DMAX
-DMAX+=0.1
-STRETCH,SHIFT=stretchSwitch(mydmax)
-DMAX=mydmax
+
+# some juggling for calculationg the stretch
+mydmax = DMAX
+DMAX += 0.1
+STRETCH, SHIFT = stretchSwitch(mydmax)
+DMAX = mydmax
+
 
 def pyCoord(action: plumedCommunications.PythonCVInterface):
-    # NB: This is not a realistic case of using the neigbour list!!!
-    # cvPY: PYCVINTERFACE GROUPA=1,4 IMPORT=pydistancegetAtPos CALCULATE=pydist
-
+    
     atoms = action.getPositions()
     nat = atoms.shape[0]
     nl = action.getNeighbourList()
@@ -55,8 +59,18 @@ def pyCoord(action: plumedCommunications.PythonCVInterface):
     couples = nl.getClosePairs()
     d = atoms[couples[:, 0]] - atoms[couples[:, 1]]
     d = pbc.apply(d)
-    d = np.linalg.norm(d, axis=1)
-    print(d, file=log)
-    sw=switch(d)
-    print(sw, file=log)
-    return np.sum(sw)
+    dist = np.linalg.norm(d, axis=1)
+
+    sw, dfunc = switch(dist)
+    dev = np.zeros_like(atoms)
+
+    predev = d * dfunc.reshape((-1,1))
+    
+    for atomID in range(nat):
+        # wherePlus =couples[:, 0]==atomID
+        # whereMinus=couples[:, 1]==atomID
+        dev[atomID] = np.sum(predev[couples[:, 0] == atomID], axis=0) - np.sum(
+            predev[couples[:, 1] == atomID], axis=0
+        )
+
+    return np.sum(sw), dev
