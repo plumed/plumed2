88d32c71c1055f2ea64ea4036c43cb9b0c03af43
Daniele Rapetti <daniele.rapetti@sissa.it>
2023-11-07 13:07:33 +0100

    some more explanation on the arbitrary stop

--- src/tools/NeighborList.cpp
+++ src/tools/NeighborList.cpp
@@ -105,6 +105,7 @@ void NeighborList::initialize() {
     //that is about 1296000000 pairs (36000 atoms)
     //36000 * 36000= 1296000000
     //each pairIDs occupies 64 bit (where unsigned are 32bit integers)
+    //4294967296 is max(uint32)+1 and is more than 34 GB (correspond to a system of 65536 atoms)
     if(nallpairs_ > 1296000000 )
       plumed_merror() << "An error happened while allocating the neighbor "
                       "list, please decrease the number of atoms used";
