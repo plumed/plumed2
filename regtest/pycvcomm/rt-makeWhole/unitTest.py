5bb6bd721bfa744022be354022dca166d9c45e3d
Daniele Rapetti <5535617+Iximiel@users.noreply.github.com>
2023-11-14 10:45:33 +0100

    set up the test for makeWhole

---
+++ regtest/pycvcomm/rt-makeWhole/unitTest.py
@@ -0,0 +1,19 @@
+import plumedCommunications as PLMD
+import numpy
+# import plumedUtilities
+log = open("pydist.log", "w")
+
+print("Imported my pydist+.", file=log)
+
+
+def mypytest(action: PLMD.PythonCVInterface):
+    atoms=action.getPositions()
+    d= atoms[0]-atoms[1]
+    ret = {
+        "dbefore": numpy.linalg.norm(d),
+    }
+    action.makeWhole()
+    atoms=action.getPositions()
+    d= atoms[0]-atoms[1]
+    ret["dafter"]=numpy.linalg.norm(d)
+    return ret
