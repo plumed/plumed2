8d726d18bcddbba7f0309728d99d05a2d46413f7
Daniele Rapetti <5535617+Iximiel@users.noreply.github.com>
2023-11-24 10:58:07 +0100

    added a test for func derivatives

--- pycv/regtest/pycvfunc/rt-derivative/unitTest.py
+++ pycv/regtest/pycvfunc/rt-derivative/unitTest.py
@@ -5,10 +5,10 @@ log = open("pydist.log", "w")
 
 print("Imported my pydist+.", file=log)
 
-initForF={"Value": PLMD.defaults.COMPONENT_NODEV}
+initForF={"Value": PLMD.defaults.COMPONENT}
 
 def function(action: PLMD.PythonFunction):
     arg = [action.argument(0),
            action.argument(1)
            ]
-    return arg[0]*arg[0]*arg[1]
+    return arg[0]*arg[1], [arg[1], arg[0]]
