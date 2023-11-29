863d00dd103ad2088ef83e080cfc982a7a67ca8f
Carlo Camilloni <carlo.camilloni@gmail.com>
2023-11-29 21:43:56 +0100

    improve nan comparison fortran intel compiler is not yet well configured

--- regtest/scripts/fixzeroes.sh
+++ regtest/scripts/fixzeroes.sh
@@ -6,7 +6,7 @@ grep -E '[^[:print:]]' $file 2> /dev/null && exit 0
 
 sed "s/-\(0\.0*0 \)/ \1/g; 
      s/-\(0\.0*0$\)/ \1/g" $file > $file.$$.tmp
-sed "s/-\(nan \)/ \1/g; 
-     s/-\(nan$\)/ \1/g" $file.$$.tmp > $file.$$.tmp2
+sed "s/-\(nan \)/\1/g; 
+     s/-\(nan$\)/\1/g" $file.$$.tmp > $file.$$.tmp2
 mv $file.$$.tmp2 $file
 
