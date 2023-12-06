0c5672ce2c67e9e055324712e4b2719567ff604d
Daniele Rapetti <5535617+Iximiel@users.noreply.github.com>
2023-12-06 10:54:26 +0100

    updated the standaloneCompile.sh

--- plugins/pycv/standaloneCompile.sh
+++ plugins/pycv/standaloneCompile.sh
@@ -13,11 +13,12 @@ if ! python3-config --embed >/dev/null 2>/dev/null; then
   exit 1
 fi
 
-export  PLUMED_MKLIB_CFLAGS="$(python3-config --cflags --embed) $(python -m pybind11 --includes)"
+#-fvisibility=hidden is needed to correct the warnings for the visibility of some pybind11 functionalities
+export  PLUMED_MKLIB_CFLAGS="$(python3-config --cflags --embed) $(python -m pybind11 --includes) -fvisibility=hidden"
 
 export PLUMED_MKLIB_LDFLAGS="$(python3-config --ldflags --embed) $conda_fixup"
 
 echo PLUMED_MKLIB_CFLAGS=$PLUMED_MKLIB_CFLAGS
 echo PLUMED_MKLIB_LDFLAGS=$PLUMED_MKLIB_LDFLAGS
 
-plumed mklib PythonCVInterface.cpp ActionWithPython.cpp PlumedPythonEmbeddedModule.cpp
+plumed mklib PythonCVInterface.cpp ActionWithPython.cpp PythonFunction.cpp PlumedPythonEmbeddedModule.cpp
