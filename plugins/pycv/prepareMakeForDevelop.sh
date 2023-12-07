fd2f6526c410c87b0e1a1cf5378237810460db7d
Daniele Rapetti <iximiel@gmail.com>
2023-12-07 15:35:47 +0100

    generaized pythonversion (see  https://github.com/plumed/plumed2/commit/69f9bfbf5afaf5a06677c6c0b39a25429cd9d700) Squashed commit of the following:
    commit 10c4d60d0274c423faab81306fb2354c7b07e75c
    Author: Daniele Rapetti <iximiel@gmail.com>
    Date:   Thu Dec 7 15:34:56 2023 +0100
    
        -Wl,--no-as-needed removed, added PYCV_EXTRA_LDFLAGS
    
    commit f1b7240219046d730e0d4f26e555f80bcb2ddfab
    Author: Daniele Rapetti <iximiel@gmail.com>
    Date:   Thu Dec 7 15:25:53 2023 +0100
    
        generaized pythonversion (see  https://github.com/plumed/plumed2/commit/69f9bfbf5afaf5a06677c6c0b39a25429cd9d700)

--- plugins/pycv/prepareMakeForDevelop.sh
+++ plugins/pycv/prepareMakeForDevelop.sh
@@ -1,12 +1,20 @@
 #! /usr/bin/env bash
 
+#assign a default value to python bin and plumed
+python_bin=${python_bin:-python}
+plumed_program_name=${plumed_program_name:-plumed}
+#python3-config may be any python3.* version, this should avoid contamination from other environments
+#and use pythonX.Y-config, as suggested in the manual
+pyver=$($python_bin -c "import sysconfig;print(sysconfig.get_config_var('VERSION'))")
+python_config=python${pyver}-config
+
 if [[ -z $PLUMED_KERNEL ]]; then
   echo "$(basename $0) can work only if \"PLUMED_KERNEL\" is defined"
   echo "either via module load or sourceme.sh, or manually exported"
   exit 1
 fi
 
-if ! python3-config --embed >/dev/null 2>/dev/null; then
+if ! ${python_config} --embed >/dev/null 2>/dev/null; then
 #TODO: verify that this does not give problems with conda
   echo "PyCV needs python to be built to be embedable"
   echo "(compiling python with --enable-shared should be enough)"
@@ -14,8 +22,8 @@ if ! python3-config --embed >/dev/null 2>/dev/null; then
 fi
 
 {
-  plumed --no-mpi config makefile_conf
+  ${plumed_program_name} --no-mpi config makefile_conf
   echo "PLUMED_KERNEL=${PLUMED_KERNEL}"
-  echo "ADDCPPFLAGS=$(python3-config --cflags --embed) $(python3 -m pybind11 --includes) -I$(plumed --no-mpi info --include-dir)/plumed"
-  echo "ADDCLDFLAGS=$(python3-config --ldflags --embed)"
+  echo "ADDCPPFLAGS=$(${python_config} --cflags --embed) $(${python_bin} -m pybind11 --includes) -I$(${plumed_program_name} --no-mpi info --include-dir)/plumed"
+  echo "ADDCLDFLAGS=$(PYCV_EXTRA_LDFLAGS) $(${python_config} --ldflags --embed)"
 } > Make.inc
--- plugins/pycv/standaloneCompile.sh
+++ plugins/pycv/standaloneCompile.sh
@@ -1,25 +1,35 @@
+#! /usr/bin/env bash
+#assign a default value to python bin and plumed
+python_bin=${python_bin:-python}
+plumed_program_name=${plumed_program_name:-plumed}
+#python3-config may be any python3.* version, this should avoid contamination from other environments
+#and use pythonX.Y-config, as suggested in the manual
+pyver=$($python_bin -c "import sysconfig;print(sysconfig.get_config_var('VERSION'))")
+python_config=python${pyver}-config
+
 # Note that in conda libpython3xx is not found in the path returned by ldflags. IMHO it is a bug.
 # The workaround is to -L appropriately. Will be fixed here.
-
 conda_fixup=${CONDA_PREFIX+-L$CONDA_PREFIX/lib}
-if [ ! -z "$conda_fixup" ]; then 
-  echo "CONDA_PREFIX is set. Assuming conda and enabling a workaround for missing -L in python3-config --ldflags --embed"
+if [ ! -z "$conda_fixup" ]; then
+  echo "CONDA_PREFIX is set. Assuming conda and enabling a workaround for missing -L in ${python_config} --ldflags --embed"
 fi
 
-if ! python3-config --embed >/dev/null 2>/dev/null; then
+if ! ${python_config} --embed >/dev/null 2>/dev/null; then
   #TODO: verify that this does not give problems with conda
   echo "PyCV needs python to be built to be embedable"
   echo "(compiling python with --enable-shared should be enough)"
   exit 1
 fi
 
+#for some reason, on the WSL I need to compile with `export PYCV_EXTRA_LDFLAGS="-Wl,--no-as-needed"`
 #-Wl,--no-as-needed forces the python library to be linked, without this in a WSL does not work
-#-fvisibility=hidden is needed to correct the warnings for the visibility of some pybind11 functionalities
-export  PLUMED_MKLIB_CFLAGS="$(python3-config --cflags --embed) $(python -m pybind11 --includes) -fvisibility=hidden"
 
-export PLUMED_MKLIB_LDFLAGS="-Wl,--no-as-needed $(python3-config --ldflags --embed) $conda_fixup"
+#-fvisibility=hidden is needed to correct the warnings for the visibility of some pybind11 functionalities
+export PLUMED_MKLIB_CFLAGS="$(${python_config} --cflags --embed) $(python -m pybind11 --includes) -fvisibility=hidden"
+
+export PLUMED_MKLIB_LDFLAGS="$(PYCV_EXTRA_LDFLAGS) $(${python_config} --ldflags --embed) $conda_fixup"
 
 echo PLUMED_MKLIB_CFLAGS=$PLUMED_MKLIB_CFLAGS
 echo PLUMED_MKLIB_LDFLAGS=$PLUMED_MKLIB_LDFLAGS
 
-plumed mklib PythonCVInterface.cpp ActionWithPython.cpp PythonFunction.cpp PlumedPythonEmbeddedModule.cpp
+${plumed_program_name} mklib PythonCVInterface.cpp ActionWithPython.cpp PythonFunction.cpp PlumedPythonEmbeddedModule.cpp
