534003bde29658e9a9dda7d78f7b40d76263f4c5
Daniele <Iximiel@users.noreply.github.com>
2023-12-19 10:13:41 +0100

    Added some aid-compile option in autoconf
    * Trying to setup pycv into config
    
    * forgot to commit configure before pushig
    
    * redesigning the compilation with the new ac
    
    * changed behaviour of not finding cython
    
    * now if no python package is found also python_bin is not set
    
    * now configure understands if python is embeddable
    
    * moved the variables and added some comments for clarity
    
    * rerun autoconf
    
    * set up  a cleaner instalation
    
    * forgot to push the compileconfiguration common sourced file
    
    * added some aid for maintainers
    
    ---------
    
    Co-authored-by: Daniele Rapetti <5535617+Iximiel@users.noreply.github.com>

--- plugins/pycv/astyle.sh
+++ plugins/pycv/astyle.sh
@@ -1,20 +1,23 @@
 #! /usr/bin/env bash
 
+#formatted with shfmt  https://github.com/mvdan/sh/releases
+#checked with shellcheck
+
 for file in *.c *.cpp *.h *.inc.in; do
 
   test -f "$file" || continue
 
   echo -n "astyle $file"
 
-  ../../astyle/astyle --options=../../.astyle.options <$file >$file.tmp && {
-    if cmp -s $file $file.tmp; then
+  ../../astyle/astyle --options=../../.astyle.options <"$file" >"${file}.tmp" && {
+    if cmp -s "$file" "${file}.tmp"; then
       echo
     else
-      cp $file.tmp $file
+      cp "${file}.tmp" "$file"
       echo " +++ PATCHED"
     fi
   }
 
-  rm $file.tmp
+  rm "${file}.tmp"
 
 done
--- plugins/pycv/prepareMakeForDevelop.sh
+++ plugins/pycv/prepareMakeForDevelop.sh
@@ -1,12 +1,11 @@
 #! /usr/bin/env bash
 
-#assign a default value to python bin and plumed
-python_bin=${python_bin:-python}
-plumed_program_name=${plumed_program_name:-plumed}
-#python3-config may be any python3.* version, this should avoid contamination from other environments
-#and use pythonX.Y-config, as suggested in the manual
-pyver=$($python_bin -c "import sysconfig;print(sysconfig.get_config_var('VERSION'))")
-python_config=python${pyver}-config
+#formatted with shfmt  https://github.com/mvdan/sh/releases
+#checked with shellcheck
+# the SC2154 warnings are variables present in the sourced file
+
+source compileConfiguration.sh
+
 
 if [[ -z $PLUMED_KERNEL ]]; then
   echo "$(basename $0) can work only if \"PLUMED_KERNEL\" is defined"
@@ -14,16 +13,15 @@ if [[ -z $PLUMED_KERNEL ]]; then
   exit 1
 fi
 
-if ! ${python_config} --embed >/dev/null 2>/dev/null; then
-#TODO: verify that this does not give problems with conda
-  echo "PyCV needs python to be built to be embedable"
-  echo "(compiling python with --enable-shared should be enough)"
-  exit 1
-fi
-
+plumed_include=$(${plumed_program_name} --no-mpi info --include-dir)
 {
   ${plumed_program_name} --no-mpi config makefile_conf
   echo "PLUMED_KERNEL=${PLUMED_KERNEL}"
-  echo "ADDCPPFLAGS=$(${python_config} --cflags --embed) $(${python_bin} -m pybind11 --includes) -I$(${plumed_program_name} --no-mpi info --include-dir)/plumed"
-  echo "ADDCLDFLAGS=${PYCV_EXTRA_LDFLAGS} $(${python_config} --ldflags --embed)"
-} > Make.inc
+  if [[ ! $plumed_canPyCV = yes ]]; then
+    echo "pybind11_cflags=$pybind11_cflags"
+    echo "python_cf_embedded=$python_cf_embedded"
+    echo "python_ld_embedded=$python_ld_embedded"
+  fi
+  echo "ADDCPPFLAGS=-I${plumed_include}/plumed"
+  echo "ADDCLDFLAGS=${PYCV_EXTRA_LDFLAGS} ${conda_fixup}"
+} > Makefile.conf
