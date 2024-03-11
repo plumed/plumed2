f840cd98833a5f924eb67f2837f2909686e18a5c
Giovanni Bussi <giovanni.bussi@gmail.com>
2024-03-11 09:31:53 +0100

    Removed hard fix for conda.
    I am removing a fix that I needed to introduce last
    year to make working conda builds of the python
    wrappers. Apparently, this is not needed anymore due to
    some updates in conda.
    
    See https://github.com/plumed/plumed2/issues/952

--- conda/py-plumed/build.sh
+++ conda/py-plumed/build.sh
@@ -3,7 +3,7 @@
 cd python
 make pip
 export plumed_default_kernel=$PREFIX/lib/libplumedKernel$SHLIB_EXT
-export plumed_disable_rtld_deepbind=yes
+#export plumed_disable_rtld_deepbind=yes
 
 $PYTHON -m pip install . --no-deps -vv
 
