e5b19e61d76b7743b32da1ddf371763d151e23d2
Gareth Aneurin Tribello <garethtribello@Gareths-MacBook-Pro.local>
2024-03-15 19:38:09 +0000

    Added more missing keywords to Bridge

--- src/adjmat/Bridge.cpp
+++ src/adjmat/Bridge.cpp
@@ -44,6 +44,9 @@ PLUMED_REGISTER_ACTION(Bridge,"BRIDGE")
 
 void Bridge::registerKeywords(Keywords& keys) {
   ActionShortcut::registerKeywords(keys);
+  keys.add("atoms","GROUP","the atoms for which you would like to calculate the adjacency matrix");
+  keys.add("atoms","GROUPA","");
+  keys.add("atoms","GROUPB","");
   keys.add("atoms","BRIDGING_ATOMS","The list of atoms that can form the bridge between the two interesting parts "
            "of the structure.");
   keys.add("optional","SWITCH","The parameters of the two \\ref switchingfunction in the above formula");
