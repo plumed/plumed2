364d188f5f2c41fc63e0c546e1da17b89f23477b
Gareth Aneurin Tribello <garethtribello@Gareths-MacBook-Pro.local>
2024-03-29 20:25:20 +0000

    Fixed a typo in FarthestPointSampling.cpp

--- src/analysis/FarthestPointSampling.cpp
+++ src/analysis/FarthestPointSampling.cpp
@@ -19,7 +19,7 @@
    You should have received a copy of the GNU Lesser General Public License
    along with plumed.  If not, see <http://www.gnu.org/licenses/>.
 +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++ */
-#include "adjmat/MatrixoperationBase.h"
+#include "adjmat/MatrixOperationBase.h"
 #include "core/ActionRegister.h"
 #include "tools/Random.h"
 
