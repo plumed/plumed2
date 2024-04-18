c0e2ddddbeffba321dc2a37a0fa7a684f7665ad2
Giovanni Bussi <giovanni.bussi@gmail.com>
2024-04-18 07:14:45 +0200

    Revert "Fixed a bug that appears when you try to sum all the elements in a matrix"
    This reverts commit 9b644057a06e98bf9fbf0831ba0666904e87d41c.
    
        See #1060
    
        @gtribello this is a temporary fix!
    
    This is the correct fix

--- src/adjmat/ActionWithMatrix.cpp
+++ src/adjmat/ActionWithMatrix.cpp
@@ -20,6 +20,7 @@
    along with plumed.  If not, see <http://www.gnu.org/licenses/>.
 +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++ */
 #include "ActionWithMatrix.h"
+#include "AdjacencyMatrixBase.h"
 
 namespace PLMD {
 namespace adjmat {
@@ -71,7 +72,7 @@ void ActionWithMatrix::finishChainBuild( ActionWithVector* act ) {
   else {
     next_action_in_chain=am;
     // Build the list of things we are going to loop over in runTask
-    if( am->isAdjacencyMatrix() || act->getName()=="VSTACK" ) return ;
+    AdjacencyMatrixBase* aa=dynamic_cast<AdjacencyMatrixBase*>(act); if( aa || act->getName()=="VSTACK" ) return ;
     plumed_massert( !matrix_to_do_after, "cannot add " + act->getLabel() + " in " + getLabel() + " as have already added " + matrix_to_do_after->getLabel() );
     matrix_to_do_after=am; am->matrix_to_do_before=this;
   }
@@ -133,7 +134,7 @@ void ActionWithMatrix::performTask( const unsigned& task_index, MultiValue& myva
 void ActionWithMatrix::runTask( const std::string& controller, const unsigned& current, const unsigned colno, MultiValue& myvals ) const {
   double outval=0; myvals.setTaskIndex(current); myvals.setSecondTaskIndex( colno );
   if( isActive() ) performTask( controller, current, colno, myvals );
-  bool hasval = !isAdjacencyMatrix();
+  bool hasval=false;
   for(int i=0; i<getNumberOfComponents(); ++i) {
     if( fabs(myvals.get( getConstPntrToComponent(i)->getPositionInStream()) )>0 ) { hasval=true; break; }
   }
