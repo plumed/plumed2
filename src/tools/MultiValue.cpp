23d932e7c65154de20d703a7c573b945d17047ff
Gareth Aneurin Tribello <garethtribello@Gareths-MacBook-Pro.local>
2024-05-08 22:08:48 +0100

    Fixed missing inclusion that is required if you compile with debug and added requirements to some regtests

--- src/tools/MultiValue.cpp
+++ src/tools/MultiValue.cpp
@@ -19,6 +19,7 @@
    You should have received a copy of the GNU Lesser General Public License
    along with plumed.  If not, see <http://www.gnu.org/licenses/>.
 +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++ */
+#include "Tools.h"
 #include "MultiValue.h"
 
 namespace PLMD {
