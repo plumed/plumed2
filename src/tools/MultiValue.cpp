c98951067d0fc3362b963ceeaafeed6c0eaa690c
Gareth Aneurin Tribello <garethtribello@Gareths-MacBook-Pro.local>
2024-05-09 06:03:48 +0100

    Fix cppcheck issue

--- src/tools/MultiValue.cpp
+++ src/tools/MultiValue.cpp
@@ -19,8 +19,8 @@
    You should have received a copy of the GNU Lesser General Public License
    along with plumed.  If not, see <http://www.gnu.org/licenses/>.
 +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++ */
-#include "Tools.h"
 #include "MultiValue.h"
+#include "Tools.h"
 
 namespace PLMD {
 
