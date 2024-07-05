plumed/plumed2

commit f1e636b5b2f549b164b39cdf45f104301ebe3a95

Fixed warning on stray \ with modern grep

regtest/scripts/fixzeroes.sh
@@ -2,7 +2,7 @@
 
 file=$1
 
-grep -q "\x00" $file  && exit 0
+grep -E '[^[:print:]]' $file 2> /dev/null && exit 0
 
 sed "s/-\(0\.0*0 \)/ \1/g; 
      s/-\(0\.0*0$\)/ \1/g" $file > $file.$$.tmp

commit eea84b7c4583ab90a0a446ca4179b521786f7c7e

fixed harmless bug
In the regtest suite, if a compared file contains a non printable
character, the corresponding line was shown in the output.
This was harmless but annoying (pollutes regtest output).
The bug was introduced in f1e636b5b2f549b164b39cdf45f104301ebe3a95

regtest/scripts/fixzeroes.sh
@@ -2,7 +2,7 @@
 
 file=$1
 
-grep -E '[^[:print:]]' $file 2> /dev/null && exit 0
+grep -q -E '[^[:print:]]' $file 2> /dev/null && exit 0
 
 sed "s/-\(0\.0*0 \)/ \1/g; 
      s/-\(0\.0*0$\)/ \1/g" $file > $file.$$.tmp