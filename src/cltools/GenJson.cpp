d59f49c8b8b274d2c0973d84436150b7262a8088
Gareth Aneurin Tribello <garethtribello@gareths-mbp.mp.qub.ac.uk>
2024-10-03 13:45:43 +0100

    Added default values for keywords to json syntax file

--- src/cltools/GenJson.cpp
+++ src/cltools/GenJson.cpp
@@ -115,14 +115,16 @@ int GenJson::main(FILE* in, FILE*out,Communicator& pc) {
     std::cout<<"    \"displayname\" : \""<<keys.getDisplayName()<<"\",\n";
     std::cout<<"    \"syntax\" : {"<<std::endl;
     for(unsigned j=0; j<keys.size(); ++j) {
-      std::string desc = keys.getKeywordDescription( keys.getKeyword(j) );
+      std::string defa = "", desc = keys.getKeywordDescription( keys.getKeyword(j) );
       if( desc.find("default=")!=std::string::npos ) {
-        std::size_t brac=desc.find_first_of(")"); desc = desc.substr(brac+1);
+        std::size_t defstart = desc.find_first_of("="), brac=desc.find_first_of(")");
+        defa = desc.substr(defstart+1,brac-defstart-2); desc = desc.substr(brac+1);
       }
       std::size_t dot=desc.find_first_of("."); std::string mydescrip = desc.substr(0,dot);
       if( mydescrip.find("\\")!=std::string::npos ) error("found invalid backslash character documentation for keyword " + keys.getKeyword(j) + " in action " + action_names[i] );
       std::string argtype = keys.getArgumentType( keys.getKeyword(j) );
       if( argtype.length()>0 ) std::cout<<"       \""<<keys.getKeyword(j)<<"\" : { \"type\": \""<<keys.getStyle(keys.getKeyword(j))<<"\", \"description\": \""<<mydescrip<<"\", \"multiple\": "<<keys.numbered( keys.getKeyword(j) )<<", \"argtype\": \""<<argtype<<"\"}";
+      else if( defa.length()>0 ) std::cout<<"       \""<<keys.getKeyword(j)<<"\" : { \"type\": \""<<keys.getStyle(keys.getKeyword(j))<<"\", \"description\": \""<<mydescrip<<"\", \"default\": \""<<defa<<"\"}";
       else std::cout<<"       \""<<keys.getKeyword(j)<<"\" : { \"type\": \""<<keys.getStyle(keys.getKeyword(j))<<"\", \"description\": \""<<mydescrip<<"\", \"multiple\": "<<keys.numbered( keys.getKeyword(j) )<<"}";
       if( j==keys.size()-1 && !keys.exists("HAS_VALUES") ) std::cout<<std::endl; else std::cout<<","<<std::endl;
     }
