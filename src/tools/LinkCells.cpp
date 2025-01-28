e4f71323b34da1f963b505ba6669b5805393912a
Gareth Aneurin Tribello <garethtribello@gareths-mbp.mp.qub.ac.uk>
2025-01-28 11:00:47 +0000

    More detailed output in error message to determine problem on github

--- src/tools/LinkCells.cpp
+++ src/tools/LinkCells.cpp
@@ -217,7 +217,7 @@ std::array<unsigned,3> LinkCells::findMyCell( const Vector& pos ) const {
     } else {
       celn[j] = std::floor( ( Tools::pbc(fpos[j]) + 0.5 ) * ncells[j] );
     }
-    plumed_assert( celn[j]>=0 && celn[j]<ncells[j] ) <<"in link cell "<<celn[j]<<" but should be between 0 and "<<ncells[j];
+    plumed_assert( celn[j]>=0 && celn[j]<ncells[j] ) <<"in link cell "<<celn[j]<<" but should be between 0 and "<<ncells[j]<<" link cell cutoff is "<<link_cutoff;
   }
   return celn;
 }
