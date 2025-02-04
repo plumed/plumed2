d80e004f93b0b8fd9b6f253fb62839e0ab76729d
Gareth Aneurin Tribello <garethtribello@dhcp-129-242.wifi.sissa.it>
2025-02-04 12:49:03 +0100

    Added ATOMS keyword in RDF so that input for PAIRENTROPY is equivalent to Pablo Piaggi's version

--- src/gridtools/PairEntropies.cpp
+++ src/gridtools/PairEntropies.cpp
@@ -48,7 +48,7 @@ void PairEntropies::registerKeywords( Keywords& keys ) {
   keys.remove("GROUP");
   keys.remove("GROUPA");
   keys.remove("GROUPB");
-  keys.add("atoms","ATOMS","the atoms that you would like to compute the entropies for");
+  keys.remove("ATOMS"); keys.add("atoms","ATOMS","the atoms that you would like to compute the entropies for");
   keys.setValueDescription("vector","the a vector containing the KL-entropy that is computed from the radial distribution function around each of the atoms in the input");
   keys.needsAction("PAIRENTROPY");
   keys.needsAction("INTERPOLATE_GRID");
