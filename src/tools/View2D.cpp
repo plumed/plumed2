42310dd8006e941bb77dd4ecd53da468c08e514b
Daniele Rapetti <5535617+Iximiel@users.noreply.github.com>
2025-02-24 14:18:24 +0100

    moving View2D

--- src/tools/View2D.cpp
+++ src/tools/View2D.cpp
@@ -19,4 +19,4 @@
    You should have received a copy of the GNU Lesser General Public License
    along with plumed.  If not, see <http://www.gnu.org/licenses/>.
 +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++ */
-#include "View.h"
+#include "View2D.h"
