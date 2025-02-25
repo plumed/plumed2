a66cc0c4ff7f18d0daac3f4542f87e02b44a71b1
Gareth Aneurin Tribello <garethtribello@Gareths-MacBook-Pro.local>
2025-02-25 15:46:02 +0000

    Using right module name in ifdef guard for ColvarOutput

--- src/tools/ColvarOutput.h
+++ src/tools/ColvarOutput.h
@@ -19,8 +19,8 @@
    You should have received a copy of the GNU Lesser General Public License
    along with plumed.  If not, see <http://www.gnu.org/licenses/>.
 +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++ */
-#ifndef __PLUMED_colvar_ColvarOutputh
-#define __PLUMED_colvar_ColvarOutputh
+#ifndef __PLUMED_tools_ColvarOutputh
+#define __PLUMED_tools_ColvarOutputh
 
 #include <vector>
 
