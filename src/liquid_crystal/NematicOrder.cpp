3aa4889bfdabef7204d40a19b1f7dc13740a4748
Alexander Humeniuk <alexander.humeniuk@merckgroup.com>
2025-05-16 13:57:13 +0200

    move nematic order parameter into module liquid_crystal

--- src/liquid_crystal/NematicOrder.cpp
+++ src/liquid_crystal/NematicOrder.cpp
@@ -1,17 +1,14 @@
 /* +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
-   Copyright (c) 2011-2023 The plumed team
-   (see the PEOPLE file at the root of the distribution for a list of names)
+   Copyright (c) 2025 of Alexander Humeniuk.
 
-   See http://www.plumed.org for more information.
+   This file is part of the liquid_crystal plumed module.
 
-   This file is part of plumed, version 2.
-
-   plumed is free software: you can redistribute it and/or modify
+   The liquid_crystal plumed module is free software: you can redistribute it and/or modify
    it under the terms of the GNU Lesser General Public License as published by
    the Free Software Foundation, either version 3 of the License, or
    (at your option) any later version.
 
-   plumed is distributed in the hope that it will be useful,
+   The liquid_crystal plumed module is distributed in the hope that it will be useful,
    but WITHOUT ANY WARRANTY; without even the implied warranty of
    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
    GNU Lesser General Public License for more details.
@@ -19,7 +16,7 @@
    You should have received a copy of the GNU Lesser General Public License
    along with plumed.  If not, see <http://www.gnu.org/licenses/>.
 +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++ */
-#include "Colvar.h"
+#include "colvar/Colvar.h"
 #include "core/ActionRegister.h"
 #include "tools/Matrix.h"
 
