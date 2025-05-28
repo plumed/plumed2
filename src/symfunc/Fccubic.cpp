274af9330ee0879263460e0337ee9c974e7c27b0
Gareth Aneurin Tribello <garethtribello@Gareths-MacBook-Pro.local>
2025-05-28 16:34:15 +0100

    Fix for regtests that use FCCUBIC

--- src/symfunc/Fccubic.cpp
+++ src/symfunc/Fccubic.cpp
@@ -93,7 +93,6 @@ typedef function::FunctionOfMatrix<Fccubic> MatrixFccubic;
 PLUMED_REGISTER_ACTION(MatrixFccubic,"FCCUBIC_FUNC_MATRIX")
 
 void Fccubic::registerKeywords( Keywords& keys ) {
-  keys.setDisplayName("FCCUBIC");
   keys.add("compulsory","ALPHA","3.0","The alpha parameter of the angular function");
   keys.setValueDescription("matrix","a function that measures the similarity with an fcc environment");
 }
