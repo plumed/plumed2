35489543a658cec7557f393b730528a165763d2c
Daniele Rapetti <5535617+Iximiel@users.noreply.github.com>
2025-09-19 17:30:26 +0200

    added custom check in switching functions

--- regtest/basic/rt-make-switch/main.cpp
+++ regtest/basic/rt-make-switch/main.cpp
@@ -32,9 +32,10 @@ struct callplain {
 };
 
 template<typename caller>
-void test(const std::string& name, std::string definition,bool stretch=true) {
+void test(const std::string& name, std::string definition,std::string custom,bool stretch=true) {
   if (!stretch) {
     definition += " NOSTRETCH ";
+    custom += " NOSTRETCH ";
   }
 
   std::ofstream os("out_"+std::string(caller::kind)+name+(stretch ? "" : "_nostretch"));
@@ -45,14 +46,21 @@ void test(const std::string& name, std::string definition,bool stretch=true) {
   if (!error.empty()) {
     std::cerr<<error<<"\n";
   }
-
   error.clear();
+
+  SwitchingFunction swc;
+  swc.set(custom,error);
+  if (!error.empty()) {
+    std::cerr<<error<<"\n";
+  }
+  error.clear();
+
   SwitchingFunctionAccelerable swa;
   swa.set(definition,error);
   if (!error.empty()) {
     std::cerr<<error<<"\n";
   }
-  os << "point :\tvalue deriv\tvalue_acc deriv_acc\tvalue_delta deriv_delta\n";
+  os << "point :\tvalue deriv\tvalue_acc deriv_acc\tvalue_delta deriv_delta\tvcustom_delta dcustom_delta\n";
   for (int i=0; i<10; i++) {
     double point=i/2.50;
     double deriv;
@@ -64,30 +72,68 @@ void test(const std::string& name, std::string definition,bool stretch=true) {
     double value_acc;
     std::tie(value_acc,deriv_acc) = caller::call(swa, point);
     os << value_acc << " " << deriv_acc <<"\t";
-    os << value-value_acc << " " << deriv-deriv_acc <<"\n";
+    os << value-value_acc << " " << deriv-deriv_acc <<"\t";
+
+    double deriv_custom;
+    double value_custom;
+    std::tie(value_custom,deriv_custom) = caller::call(swc, point);
+    os << value-value_custom << " " << deriv-deriv_custom <<"\n";
   }
 }
 
 
 int main() {
   for(const auto &x: {
-  std::pair<std::string,std::string> {"COSINUS R_0=2.6","cosinus"},
-{"EXP R_0=0.8 D_0=0.5 D_MAX=2.6","exp"},
-{"GAUSSIAN R_0=1.0 D_0=0.0 D_MAX=2.6","fastgaussian"},
-{"GAUSSIAN R_0=1.0 D_0=0.3 D_MAX=2.6","gaussian"},
-{"RATIONAL R_0=1.3 NN=6 MM=10 D_MAX=2.6","fastrational"},
-{"RATIONAL R_0=1.3 D_MAX=2.6","fastrational_MMeq2MM"},
-{"RATIONAL R_0=1.3 NN=5 MM=11 D_MAX=2.6","rational"},
-{"RATIONAL R_0=1.3 NN=5 D_MAX=2.6","rational_MMeq2MM"},
-{"Q R_0=1.0 D_0=0.3 BETA=1.0 LAMBDA=1.0 REF=1.3 D_MAX=2.6","q"},
-{"SMAP R_0=1.3 A=3 B=2 D_MAX=2.6","smap"},
-{"TANH R_0=1.3 D_MAX=2.6","tanh"}
+        std::tuple<std::string,std::string,std::string> {"COSINUS R_0=2.6",
+            "CUSTOM FUNC=0.5*(cos(x*pi)+1) R_0=2.6 D_MAX=2.6",
+            "cosinus"
+  }, {
+  "EXP R_0=0.8 D_0=0.5 D_MAX=2.6",
+  "CUSTOM FUNC=exp(-x) R_0=0.8 D_0=0.5 D_MAX=2.6",
+  "exp"
+}, {
+  "GAUSSIAN R_0=1.0 D_0=0.0 D_MAX=2.6",
+  "CUSTOM FUNC=exp(-x^2/2) R_0=1.0 D_0=0.0 D_MAX=2.6",
+  "fastgaussian"
+}, {
+  "GAUSSIAN R_0=1.0 D_0=0.3 D_MAX=2.6",
+  "CUSTOM FUNC=exp(-x^2/2) R_0=1.0 D_0=0.3 D_MAX=2.6",
+  "gaussian"
+}, {
+  "RATIONAL R_0=1.3 NN=6 MM=10 D_MAX=2.6",
+  "CUSTOM FUNC=(1-x^6)/(1-x^10) D_MAX=2.6 R_0=1.3",
+  "fastrational"
+}, {
+  "RATIONAL R_0=1.3 D_MAX=2.6",
+  "CUSTOM FUNC=(1-x^6)/(1-x^12) D_MAX=2.6 R_0=1.3",
+  "fastrational_NNeq2MM"
+}, {
+  "RATIONAL R_0=1.3 NN=5 MM=11 D_MAX=2.6",
+  "CUSTOM FUNC=(1-x^5)/(1-x^11) D_MAX=2.6 R_0=1.3",
+  "rational"
+}, {
+  "RATIONAL R_0=1.3 NN=5 D_MAX=2.6",
+  "CUSTOM FUNC=(1-x^5)/(1-x^10) D_MAX=2.6 R_0=1.3",
+  "rational_NNeq2MM"
+}, {
+  "Q R_0=1.0 D_0=0.3 BETA=5.0 LAMBDA=1.0 REF=1.3 D_MAX=2.6",
+  "CUSTOM FUNC=1/(1+exp(5.0*((x+0.3)-1.3))) R_0=1.0 D_0=0.3 D_MAX=2.6",
+  "q"
+}, {
+  "TANH R_0=1.3 D_MAX=2.6",
+  "CUSTOM FUNC=1-tanh(x) R_0=1.3 D_MAX=2.6",
+  "tanh"
+}, {
+  "SMAP R_0=1.3 A=3 B=2 D_MAX=2.6",
+  "CUSTOM FUNC=(1+(2^(3/2)-1)*x^3)^(-2/3) R_0=1.3 D_MAX=2.6",
+  "smap"
+}
     }) {
-    auto [definition, name] = x;
-    test<callplain> (name,definition);
-    test<callplain> (name,definition,false);
-    test<callSQR> (name,definition);
-    test<callSQR> (name,definition,false);
+    auto [definition, custom, name] = x;
+    test<callplain> (name,definition,custom);
+    test<callplain> (name,definition,custom,false);
+    test<callSQR> (name,definition,custom);
+    test<callSQR> (name,definition,custom,false);
 
   }
 
