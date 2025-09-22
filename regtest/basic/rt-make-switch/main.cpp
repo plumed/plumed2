0482e85b476db936635930b92f769ca8ad2a6997
Daniele Rapetti <5535617+Iximiel@users.noreply.github.com>
2025-09-22 14:24:33 +0200

    now the test is more clear and compact in its scope

--- regtest/basic/rt-make-switch/main.cpp
+++ regtest/basic/rt-make-switch/main.cpp
@@ -3,6 +3,7 @@
 #include <fstream>
 #include <iostream>
 #include <iomanip>
+#include <ostream>
 #include <utility>
 
 
@@ -11,7 +12,6 @@ using PLMD::SwitchingFunctionAccelerable;
 
 
 struct callSQR {
-  constexpr static auto kind="sqr_";
   template<typename T>
   static std::pair <double,double> call(const T& sw, double point) {
     double deriv;
@@ -22,23 +22,35 @@ struct callSQR {
 };
 
 struct callplain {
-  constexpr static auto kind="";
   template<typename T>
-  static std::pair <double,double> call(const T& sw, double point) {
+  static std::pair <double,double> call(const T& sw, const double point) {
     double deriv;
     double value = sw.calculate(point,deriv);
     return std::make_pair(value,deriv);
   }
 };
 
-template<typename caller>
+template<typename caller,typename SW>
+std::ostream& calculateAndSubtract(const SW& sw,
+                                   const double point,
+                                   const double value,
+                                   const double derivative,
+                                   std::ostream& out) {
+  double mydev;
+  double myval;
+  std::tie(myval,mydev) = caller::call(sw,point);
+  out << (myval -value)<< " " << (mydev - derivative)<<" " ;
+  return out;
+}
+
+
 void test(const std::string& name, std::string definition,std::string custom,bool stretch=true) {
   if (!stretch) {
     definition += " NOSTRETCH ";
     custom += " NOSTRETCH ";
   }
 
-  std::ofstream os("out_"+std::string(caller::kind)+name+(stretch ? "" : "_nostretch"));
+  std::ofstream os("out_"+name+(stretch ? "" : "_nostretch"));
   os <<std::fixed<<std::setprecision(6);
   SwitchingFunction sw;
   std::string error;
@@ -60,34 +72,37 @@ void test(const std::string& name, std::string definition,std::string custom,boo
   if (!error.empty()) {
     std::cerr<<error<<"\n";
   }
-  os << "point :\tvalue deriv\tvalue_acc deriv_acc\tvalue_delta deriv_delta\tvcustom_delta dcustom_delta\n";
+  os << "point : value deriv vsq_delta dsq_delta "
+     "vAcc_delta dAcc_delta "
+     "vAccsq_delta dAccsq_delta "
+     "vcus_delta dcus_delta "
+     "vsqcus_delta dsqcus_delta\n";
   for (int i=0; i<10; i++) {
     double point=i/2.50;
-    double deriv;
+    double derivative;
     double value;
-    os<< point <<" :\t";
-    std::tie(value,deriv) = caller::call(sw, point);
-    os << value << " " << deriv <<"\t";
-    double deriv_acc;
-    double value_acc;
-    std::tie(value_acc,deriv_acc) = caller::call(swa, point);
-    os << value_acc << " " << deriv_acc <<"\t";
-    os << value-value_acc << " " << deriv-deriv_acc <<"\t";
-
-    double deriv_custom;
-    double value_custom;
-    std::tie(value_custom,deriv_custom) = caller::call(swc, point);
-    os << value-value_custom << " " << deriv-deriv_custom <<"\n";
+    os<< point <<" : ";
+    std::tie(value,derivative) = callplain::call(sw, point);
+    os << value << " " << derivative <<" ";
+    calculateAndSubtract<callSQR>(sw,point, value, derivative,os) << " ";
+    calculateAndSubtract<callplain>(swa,point, value, derivative,os) << " ";
+    calculateAndSubtract<callSQR>(swa,point, value, derivative,os) << " ";
+    calculateAndSubtract<callplain>(swc,point, value, derivative,os) << " ";
+    calculateAndSubtract<callSQR>(swc,point, value, derivative,os) << "\n";
   }
 }
 
 
 int main() {
+//the test will output the value of the switch along with de difference of it and
+//the same calculation under different settings (with squared/non squared input,
+//with the "accelerable version and with custom)
   for(const auto &x: {
-        std::tuple<std::string,std::string,std::string> {"COSINUS R_0=2.6",
-            "CUSTOM FUNC=0.5*(cos(x*pi)+1) R_0=2.6 D_MAX=2.6",
-            "cosinus"
-  }, {
+  std::tuple<std::string,std::string,std::string> {
+  "COSINUS R_0=2.6",
+  "CUSTOM FUNC=0.5*(cos(x*pi)+1) R_0=2.6 D_MAX=2.6",
+  "cosinus"
+}, {
   "EXP R_0=0.8 D_0=0.5 D_MAX=2.6",
   "CUSTOM FUNC=exp(-x) R_0=0.8 D_0=0.5 D_MAX=2.6",
   "exp"
@@ -127,13 +142,16 @@ int main() {
   "SMAP R_0=1.3 A=3 B=2 D_MAX=2.6",
   "CUSTOM FUNC=(1+(2^(3/2)-1)*x^3)^(-2/3) R_0=1.3 D_MAX=2.6",
   "smap"
+}, {
+  "CUBIC D_MAX=2.6 D_0=0.6",
+//Here R_O = DMAX-D_0
+  "CUSTOM FUNC=(x-1)^2*(1+2*x) R_0=2.0 D_0=0.6 D_MAX=2.6",
+  "cubic"
 }
     }) {
     auto [definition, custom, name] = x;
-    test<callplain> (name,definition,custom);
-    test<callplain> (name,definition,custom,false);
-    test<callSQR> (name,definition,custom);
-    test<callSQR> (name,definition,custom,false);
+    test (name,definition,custom);
+    test (name,definition,custom,false);
 
   }
 
