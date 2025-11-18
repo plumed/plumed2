e5c99261c6cd4e68c2da8d1a9bd98a6433946fdd
Daniele Rapetti <5535617+Iximiel@users.noreply.github.com>
2025-11-18 16:34:29 +0100

    sorting for test stability

--- regtest/tools/rt-Neigbourlist/main.cpp
+++ regtest/tools/rt-Neigbourlist/main.cpp
@@ -26,8 +26,13 @@ void testResult(std::string name,
   for( unsigned i=0; i < indexes.size() ; ++i) {
     auto mynl=nl.getNeighbors(i);
     ofs <<name<< indexes[i].index() << ":";
+//sorting for test stability:
+    std::transform(mynl.begin(),mynl.end(),mynl.begin(),[&](unsigned ii) {
+      return indexes[ii].index();
+    });
+    std::sort(mynl.begin(),mynl.end());
     for (auto j: mynl) {
-      ofs << " " <<indexes[j].index();
+      ofs << " " <<j;
     }
     ofs << std::endl;
   }
