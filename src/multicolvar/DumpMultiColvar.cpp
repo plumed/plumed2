f913f767812b5c07e571928c399fcf9692f547e2
Gareth Tribello <garethtribello@Mac.lan>
2026-07-20 17:35:16 +0100

    Added missing documentation for DumpMultiColvar shortcut

--- src/multicolvar/DumpMultiColvar.cpp
+++ src/multicolvar/DumpMultiColvar.cpp
@@ -49,6 +49,7 @@ void DumpMultiColvar::registerKeywords(Keywords& keys) {
   ActionShortcut::registerKeywords( keys );
   keys.add("compulsory","DATA","the vector you wish to transform");
   keys.add("compulsory","FILE","the file that you would like to output the data to");
+  keys.add("compulsory","STRIDE","1","the frequency with which you would like to output the multicolvar data");
   keys.remove("HAS_VALUES");
   keys.needsAction("DUMPATOMS");
 }
