bf337bab73236ff5a110175d2c174faefa22d6d6
Gareth Tribello <garethtribello@Mac.lan>
2026-09-06 10:34:50 +0100

    PLUMED now errors if you try to use a lepton constant name in VAR for CUSTOM

--- src/function/Custom.cpp
+++ src/function/Custom.cpp
@@ -350,6 +350,9 @@ void Custom::read( ActionWithArguments* action ) {
   action->log.printf("  with variables :");
   for(unsigned i=0; i<var.size(); i++) {
     action->log.printf(" %s",var[i].c_str());
+    for(std::map<std::string,double>::const_iterator iter=lepton::Constants().begin(); iter!=lepton::Constants().end(); ++iter ) {
+        if( var[i]==iter->first ) action->error("variable " + var[i] + " clashes with lepton constant " + iter->first );
+    }
   }
   action->log.printf("\n");
   function.set( func, var, action );
